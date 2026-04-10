e226b16dc3c36e8a27ad243a1b137acf442a8e4e
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2026-04-10 16:00:27 -0700

    Ensure nested settings pages are added to Cmdk

--- static/app/components/commandPalette/ui/commandPaletteGlobalActions.tsx
+++ static/app/components/commandPalette/ui/commandPaletteGlobalActions.tsx
@@ -34,9 +34,11 @@ import {
   IconUser,
 } from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {ConfigStore} from 'sentry/stores/configStore';
 import {apiOptions} from 'sentry/utils/api/apiOptions';
 import {isActiveSuperuser} from 'sentry/utils/isActiveSuperuser';
 import {QUERY_API_CLIENT, useMutation} from 'sentry/utils/queryClient';
+import {replaceRouterParams} from 'sentry/utils/replaceRouterParams';
 import {useMutateUserOptions} from 'sentry/utils/useMutateUserOptions';
 import {useOrganization} from 'sentry/utils/useOrganization';
 import {useProjects} from 'sentry/utils/useProjects';
@@ -50,6 +52,8 @@ import {MOBILE_LANDING_SUB_PATH} from 'sentry/views/insights/pages/mobile/settin
 import {ISSUE_TAXONOMY_CONFIG} from 'sentry/views/issueList/taxonomies';
 import {useStarredIssueViews} from 'sentry/views/navigation/secondary/sections/issues/issueViews/useStarredIssueViews';
 import {getUserOrgNavigationConfiguration} from 'sentry/views/settings/organization/userOrgNavigationConfiguration';
+import {getNavigationConfiguration} from 'sentry/views/settings/project/navigationConfiguration';
+import type {NavigationGroupProps} from 'sentry/views/settings/types';
 
 import {CMDKAction} from './cmdk';
 import {CommandPaletteSlot} from './commandPaletteSlot';
@@ -202,11 +206,32 @@ export function GlobalCommandPaletteActions() {
         )}
 
         <CMDKAction display={{label: t('Settings'), icon: <IconSettings />}}>
-          {getUserOrgNavigationConfiguration().flatMap(section =>
-            section.items.map(item => (
-              <CMDKAction key={item.path} display={{label: item.title}} to={item.path} />
-            ))
-          )}
+          {getUserOrgNavigationConfiguration().map(section => {
+            const orgNavContext: NavigationGroupProps = {
+              ...section,
+              organization,
+              access: new Set(organization.access ?? []),
+              features: new Set(organization.features ?? []),
+              isSelfHosted: ConfigStore.get('isSelfHosted'),
+            };
+            return (
+              <CMDKAction key={section.name} display={{label: section.name}}>
+                {section.items
+                  .filter(item =>
+                    typeof item.show === 'function'
+                      ? item.show(orgNavContext)
+                      : item.show !== false
+                  )
+                  .map(item => (
+                    <CMDKAction
+                      key={item.path}
+                      display={{label: item.title, details: item.description}}
+                      to={replaceRouterParams(item.path, {orgId: organization.slug})}
+                    />
+                  ))}
+              </CMDKAction>
+            );
+          })}
         </CMDKAction>
 
         <CMDKAction
@@ -220,8 +245,33 @@ export function GlobalCommandPaletteActions() {
                 label: project.name,
                 icon: <ProjectAvatar project={project} size={16} />,
               }}
-              to={`/settings/${organization.slug}/projects/${project.slug}/`}
-            />
+            >
+              {getNavigationConfiguration({organization, project}).flatMap(section => {
+                const projectNavContext = {
+                  ...section,
+                  organization,
+                  project,
+                  access: new Set(organization.access ?? []),
+                  features: new Set(project.features ?? []),
+                };
+                return section.items
+                  .filter(item =>
+                    typeof item.show === 'function'
+                      ? item.show(projectNavContext)
+                      : item.show !== false
+                  )
+                  .map(item => (
+                    <CMDKAction
+                      key={item.path}
+                      display={{label: item.title, details: item.description}}
+                      to={replaceRouterParams(item.path, {
+                        orgId: organization.slug,
+                        projectId: project.slug,
+                      })}
+                    />
+                  ));
+              })}
+            </CMDKAction>
           ))}
         </CMDKAction>
       </CMDKAction>
