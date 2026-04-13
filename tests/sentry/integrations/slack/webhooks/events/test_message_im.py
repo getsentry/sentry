e0a553249559599fbc04bb7ab50df716211720cd
Alex Sohn <alexsohn1126@gmail.com>
2026-04-13 12:35:39 -0400

    ref(slack): Remove duplicate MessageIMDmAgentTest superseded by test_direct_message.py

--- tests/sentry/integrations/slack/webhooks/events/test_message_im.py
+++ tests/sentry/integrations/slack/webhooks/events/test_message_im.py
@@ -4,13 +4,12 @@ import orjson
 import pytest
 from slack_sdk.web import SlackResponse
 
-from sentry.integrations.messaging.metrics import SeerSlackHaltReason
 from sentry.integrations.types import EventLifecycleOutcome
-from sentry.testutils.asserts import assert_halt_metric, assert_slo_metric
+from sentry.testutils.asserts import assert_slo_metric
 from sentry.testutils.cases import IntegratedApiTestCase
 from sentry.testutils.helpers import get_response_text
 
-from . import SEER_EXPLORER_FEATURES, BaseEventTest
+from . import BaseEventTest
 
 MESSAGE_IM_EVENT = """{
     "type": "message",
@@ -154,123 +153,3 @@ class MessageIMEventTest(BaseEventTest, IntegratedApiTestCase):
         resp = self.post_webhook(event_data=orjson.loads(MESSAGE_IM_EVENT_NO_TEXT))
         assert resp.status_code == 200, resp.content
         assert not self.mock_post.called
-
-
-MESSAGE_IM_DM_EVENT = {
-    "type": "message",
-    "channel": "DOxxxxxx",
-    "user": "Uxxxxxxx",
-    "text": "What is causing errors in my project?",
-    "ts": "123456789.9875",
-}
-
-MESSAGE_IM_DM_EVENT_THREADED = {
-    **MESSAGE_IM_DM_EVENT,
-    "thread_ts": "123456789.0001",
-}
-
-AUTHORIZATIONS_DATA = {
-    "authorizations": [{"user_id": "U0BOT", "is_bot": True}],
-}
-
-
-class MessageIMDmAgentTest(BaseEventTest):
-    """Tests for DM messages triggering the Seer Explorer agentic workflow."""
-
-    @pytest.fixture(autouse=True)
-    def mock_chat_postMessage(self):
-        with patch(
-            "slack_sdk.web.client.WebClient.chat_postMessage",
-            return_value=SlackResponse(
-                client=None,
-                http_verb="POST",
-                api_url="https://slack.com/api/chat.postMessage",
-                req_args={},
-                data={"ok": True},
-                headers={},
-                status_code=200,
-            ),
-        ) as self.mock_post:
-            yield
-
-    @patch("sentry.seer.entrypoints.slack.tasks.process_mention_for_slack.apply_async")
-    def test_dm_dispatches_task(self, mock_apply_async):
-        with self.feature(SEER_EXPLORER_FEATURES):
-            resp = self.post_webhook(event_data=MESSAGE_IM_DM_EVENT, data=AUTHORIZATIONS_DATA)
-
-        assert resp.status_code == 200
-        mock_apply_async.assert_called_once()
-        kwargs = mock_apply_async.call_args[1]["kwargs"]
-        assert kwargs["integration_id"] == self.integration.id
-        assert kwargs["organization_id"] == self.organization.id
-        assert kwargs["channel_id"] == "DOxxxxxx"
-        assert kwargs["ts"] == "123456789.9875"
-        assert kwargs["thread_ts"] is None
-        assert kwargs["text"] == MESSAGE_IM_DM_EVENT["text"]
-        assert kwargs["slack_user_id"] == "Uxxxxxxx"
-        assert kwargs["bot_user_id"] == "U0BOT"
-
-    @patch("sentry.seer.entrypoints.slack.tasks.process_mention_for_slack.apply_async")
-    def test_dm_threaded_dispatches_task(self, mock_apply_async):
-        with self.feature(SEER_EXPLORER_FEATURES):
-            resp = self.post_webhook(
-                event_data=MESSAGE_IM_DM_EVENT_THREADED, data=AUTHORIZATIONS_DATA
-            )
-
-        assert resp.status_code == 200
-        mock_apply_async.assert_called_once()
-        kwargs = mock_apply_async.call_args[1]["kwargs"]
-        assert kwargs["ts"] == "123456789.9875"
-        assert kwargs["thread_ts"] == "123456789.0001"
-
-    @patch("sentry.seer.entrypoints.slack.tasks.process_mention_for_slack.apply_async")
-    def test_dm_no_authorizations(self, mock_apply_async):
-        with self.feature(SEER_EXPLORER_FEATURES):
-            resp = self.post_webhook(event_data=MESSAGE_IM_DM_EVENT)
-
-        assert resp.status_code == 200
-        mock_apply_async.assert_called_once()
-        kwargs = mock_apply_async.call_args[1]["kwargs"]
-        assert kwargs["bot_user_id"] == ""
-
-    @pytest.fixture(autouse=True)
-    def mock_set_thread_status(self):
-        with patch(
-            "sentry.integrations.slack.integration.SlackIntegration.set_thread_status",
-        ) as self.mock_status:
-            yield
-
-    @patch("sentry.integrations.utils.metrics.EventLifecycle.record_event")
-    @patch("sentry.seer.entrypoints.slack.tasks.process_mention_for_slack.apply_async")
-    def test_dm_feature_flag_disabled_falls_back_to_help(self, mock_apply_async, mock_record):
-        """When feature flag is off, DM should fall back to help message."""
-        resp = self.post_webhook(event_data=MESSAGE_IM_DM_EVENT)
-
-        assert resp.status_code == 200
-        mock_apply_async.assert_not_called()
-        assert_halt_metric(mock_record, SeerSlackHaltReason.NO_VALID_ORGANIZATION)
-
-    @patch("sentry.integrations.utils.metrics.EventLifecycle.record_event")
-    @patch("sentry.seer.entrypoints.slack.tasks.process_mention_for_slack.apply_async")
-    def test_dm_no_integration(self, mock_apply_async, mock_record):
-        with patch(
-            "sentry.integrations.slack.webhooks.event.integration_service.get_organization_integrations",
-            return_value=[],
-        ):
-            with self.feature(SEER_EXPLORER_FEATURES):
-                resp = self.post_webhook(event_data=MESSAGE_IM_DM_EVENT)
-
-        assert resp.status_code == 200
-        mock_apply_async.assert_not_called()
-        assert_halt_metric(mock_record, SeerSlackHaltReason.NO_VALID_INTEGRATION)
-
-    @patch("sentry.integrations.utils.metrics.EventLifecycle.record_event")
-    @patch("sentry.seer.entrypoints.slack.tasks.process_mention_for_slack.apply_async")
-    def test_dm_empty_text(self, mock_apply_async, mock_record):
-        event_data = {**MESSAGE_IM_DM_EVENT, "text": ""}
-        with self.feature(SEER_EXPLORER_FEATURES):
-            resp = self.post_webhook(event_data=event_data)
-
-        assert resp.status_code == 200
-        mock_apply_async.assert_not_called()
-        assert_halt_metric(mock_record, SeerSlackHaltReason.MISSING_EVENT_DATA)
