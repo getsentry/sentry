55b4d2baf1d35f452d8acec0438b61e8f6dffbd9
Colton Allen <colton.allen@sentry.io>
2026-04-15 08:16:50 -0500

    Fix typing

--- src/sentry_plugins/client.py
+++ src/sentry_plugins/client.py
@@ -59,6 +59,7 @@ class AuthApiClient(ApiClient):
         timeout: int | None = None,
         ignore_webhook_errors: bool = False,
         prepared_request: PreparedRequest | None = None,
+        stream: bool | None = None,
         raw_response: Literal[True] = ...,
     ) -> Response: ...
 
@@ -77,6 +78,7 @@ class AuthApiClient(ApiClient):
         timeout: int | None = None,
         ignore_webhook_errors: bool = False,
         prepared_request: PreparedRequest | None = None,
+        stream: bool | None = None,
         raw_response: bool = ...,
     ) -> Any: ...
 
