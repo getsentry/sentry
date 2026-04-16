f7bdadade983c041f2e11c0ac1c5d583acecd234
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-04-16 14:54:12 -0700

    use default fields model

--- src/sentry/models/organizationavatarreplica.py
+++ src/sentry/models/organizationavatarreplica.py
@@ -3,12 +3,12 @@ from __future__ import annotations
 from django.db import models
 
 from sentry.backup.scopes import RelocationScope
-from sentry.db.models import Model, control_silo_model, sane_repr
+from sentry.db.models import DefaultFieldsModel, control_silo_model, sane_repr
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
 
 
 @control_silo_model
-class OrganizationAvatarReplica(Model):
+class OrganizationAvatarReplica(DefaultFieldsModel):
     __relocation_scope__ = RelocationScope.Excluded
 
     organization_id = HybridCloudForeignKey("sentry.Organization", on_delete="CASCADE", unique=True)
