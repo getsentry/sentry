9e11037369f44e9847d86691f6f8cd8db4dbdd98
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2026-04-16 21:37:13 -0700

    ty fix

--- static/app/views/seerExplorer/components/blockComponents.tsx
+++ static/app/views/seerExplorer/components/blockComponents.tsx
@@ -472,7 +472,7 @@ export function BlockComponent({
                           )}
                         </ToolCallTextContainer>
                         {showTodoList && (
-                          <TodoListContent text={todosToMarkdown(block.todos)} />
+                          <TodoListContent text={todosToMarkdown(block.todos ?? [])} />
                         )}
                       </Stack>
                     );
