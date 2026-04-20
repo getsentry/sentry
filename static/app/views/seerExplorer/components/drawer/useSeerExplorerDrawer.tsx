b168d6ba7f8db779a43cab32467b6b76ecfcd701
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2026-04-20 15:38:15 -0700

    dont clear query param

--- static/app/views/seerExplorer/components/drawer/useSeerExplorerDrawer.tsx
+++ static/app/views/seerExplorer/components/drawer/useSeerExplorerDrawer.tsx
@@ -4,25 +4,17 @@ import {css} from '@emotion/react';
 import {useDrawer} from 'sentry/components/globalDrawer';
 import {t} from 'sentry/locale';
 import {trackAnalytics} from 'sentry/utils/analytics';
-import {useLocation} from 'sentry/utils/useLocation';
-import {useNavigate} from 'sentry/utils/useNavigate';
 import {useOrganization} from 'sentry/utils/useOrganization';
 import {ExplorerDrawerContent} from 'sentry/views/seerExplorer/components/drawer/explorerDrawerContent';
-import {
-  isSeerExplorerEnabled,
-  RUN_ID_QUERY_PARAM,
-  usePageReferrer,
-} from 'sentry/views/seerExplorer/utils';
+import {isSeerExplorerEnabled, usePageReferrer} from 'sentry/views/seerExplorer/utils';
 
 export const useSeerExplorerDrawer = () => {
-  const navigate = useNavigate();
-  const location = useLocation();
   const organization = useOrganization();
 
   const {openDrawer, closeDrawer} = useDrawer();
   const {getPageReferrer} = usePageReferrer();
 
-  // TODO: add effect that opens drawer and seeds run_id from URL
+  // TODO: add effect that opens drawer and seeds run_id from URL, remove from current URL onClose
   // (useSeerExplorer hook should no longer handle this)
 
   const [isOpen, setIsOpen] = useState(false); // for hook users
@@ -41,17 +33,7 @@ export const useSeerExplorerDrawer = () => {
   const onClose = useCallback(() => {
     isOpenRef.current = false;
     setIsOpen(false);
-    navigate(
-      {
-        pathname: location.pathname,
-        query: {
-          ...location.query,
-          [RUN_ID_QUERY_PARAM]: undefined,
-        },
-      },
-      {replace: true, preventScrollReset: true}
-    );
-  }, [location, navigate]);
+  }, []);
 
   const closeSeerExplorerDrawer = useCallback(() => {
     // Prevent closing the global drawer if another drawer (e.g. autofix) is open
