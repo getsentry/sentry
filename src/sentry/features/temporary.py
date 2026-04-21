f1b2104271c43bfff8241be6e54bd9d677b3965e
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-04-21 12:46:17 -0400

    fix

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -73,7 +73,7 @@ def register_temporary_features(manager: FeatureManager) -> None:
     # Enables custom editable dashboards
     manager.add("organizations:dashboards-edit", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True, default=True)
     # Enable unfurling of dashboard widgets in Slack
-    manager.add("organizations:dashboards-widget-unfurl", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True, default=True)
+    manager.add("organizations:dashboards-widget-unfurl", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enables import/export functionality for dashboards
     manager.add("organizations:dashboards-import", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable metrics enhanced performance for AM2+ customers as they transition from AM2 to AM3
