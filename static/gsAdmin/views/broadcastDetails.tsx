a0a1657f1b9eac288787b512957da91165b7dd5c
Rahul Chhabria <rahul.chhabria@sentry.io>
2026-04-22 11:16:02 -0700

    Revert "fix(broadcasts): Hide toggle action while editing"
    This reverts commit bacbebd8b3345df30596e550502c59a0c3b501fa.

--- static/gsAdmin/views/broadcastDetails.tsx
+++ static/gsAdmin/views/broadcastDetails.tsx
@@ -319,7 +319,7 @@ export function BroadcastDetails() {
       help: data.isActive
         ? 'Hide this broadcast from users.'
         : "Show this broadcast to users (if it hasn't expired).",
-      visible: isAdmin && !isEditing,
+      visible: isAdmin,
       onAction: () => onUpdate({isActive: !data.isActive}),
     },
     {
