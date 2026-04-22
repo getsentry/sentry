0892cca9f6bd1657806180b79b46e435fe42ee64
TkDodo <office@dorfmeister.cc>
2026-04-22 10:54:47 +0200

    fix: snapshot tests

--- static/app/components/core/button/button.snapshots.tsx
+++ static/app/components/core/button/button.snapshots.tsx
@@ -8,75 +8,72 @@ import {darkTheme, lightTheme} from 'sentry/utils/theme/theme';
 
 const themes = {light: lightTheme, dark: darkTheme};
 
-type Permutation = {
-  label: string;
-  icon?: ButtonProps['icon'];
-  priority?: ButtonProps['priority'];
-  size?: ButtonProps['size'];
-};
-
-const priorities: Permutation[] = [
-  {label: 'default', priority: 'default'},
-  {label: 'primary', priority: 'primary'},
-  {label: 'danger', priority: 'danger'},
-  {label: 'warning', priority: 'warning'},
-  {label: 'link', priority: 'link'},
-  {label: 'transparent', priority: 'transparent'},
+const allPriorities: Array<ButtonProps['priority']> = [
+  'default',
+  'primary',
+  'danger',
+  'warning',
+  'link',
+  'transparent',
 ];
 
-const sizes: Permutation[] = [
-  {label: 'size zero', size: 'zero'},
-  {label: 'size xs', size: 'xs'},
-  {label: 'size sm', size: 'sm'},
-  {label: 'size md', size: 'md'},
-];
-
-const sizesWithIcon: Permutation[] = sizes.map(s => ({
-  ...s,
-  label: `${s.label} with icon`,
-  icon: <IconEdit />,
-}));
-
-const sizesIconOnly: Permutation[] = sizes.map(s => ({
-  ...s,
-  label: `${s.label} icon-only`,
-  icon: <IconEdit />,
-}));
-
-const permutations: Permutation[] = [
-  ...priorities,
-  ...sizes,
-  ...sizesWithIcon,
-  ...sizesIconOnly,
-];
+const allSizes: Array<ButtonProps['size']> = ['zero', 'xs', 'sm', 'md'];
 
 describe('Button', () => {
   describe.each(['light', 'dark'] as const)('%s', themeName => {
-    it.snapshot.each<Permutation>(permutations)(
-      '$label',
-      ({priority, size, icon, label}) => (
+    function Wrapper({children}: {children: React.ReactNode}) {
+      return (
         <ThemeProvider theme={themes[themeName]}>
           {/* Buttons need a bit of padding as rootElement.screenshot() clips to the
             element's CSS border-box. For buttons, box-shadows/outlines/focus rings
             extending outside #root get cut off. */}
-
-          <div style={{padding: 8}}>
-            {label.includes('icon-only') ? (
-              <Button priority={priority} size={size} icon={icon} aria-label="Edit" />
-            ) : (
-              <Button priority={priority} size={size} icon={icon}>
-                {label}
-              </Button>
-            )}
-          </div>
+          <div style={{padding: 8}}>{children}</div>
         </ThemeProvider>
+      );
+    }
+
+    it.snapshot.each(allPriorities)(
+      '%s',
+      priority => (
+        <Wrapper>
+          <Button priority={priority}>{priority}</Button>
+        </Wrapper>
       ),
-      ({priority, size, icon}) => ({
-        theme: themeName,
-        ...(priority && {priority}),
-        ...(size && {size}),
-        icon: icon ? 'yes' : 'no',
-      })
+      priority => ({theme: themeName, priority: String(priority)})
     );
+
+    describe.each(allSizes)('size %s', size => {
+      it.snapshot(
+        'without icon',
+        () => (
+          <Wrapper>
+            <Button size={size}>Button</Button>
+          </Wrapper>
+        ),
+        {theme: themeName, size: String(size), icon: 'no'}
+      );
+
+      it.snapshot(
+        'with icon',
+        () => (
+          <Wrapper>
+            <Button size={size} icon={<IconEdit />}>
+              Button
+            </Button>
+          </Wrapper>
+        ),
+        {theme: themeName, size: String(size), icon: 'yes'}
+      );
+
+      it.snapshot(
+        'icon-only',
+        () => (
+          <Wrapper>
+            <Button size={size} icon={<IconEdit />} aria-label="Edit" />
+          </Wrapper>
+        ),
+        {theme: themeName, size: String(size), icon: 'only'}
+      );
+    });
   });
 });
