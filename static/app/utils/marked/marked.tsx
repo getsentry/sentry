e4b4f882392553e63581299f77c6f8dbeb78e548
Hector Dearman <hector.dearman@sentry.io>
2026-04-22 20:23:51 +0100

    fix(seer): Don't allow images in markdown
    I don't know of valid use cases for embedding images in markdown rendered in sentry.io - and it is a potential source
    of XSS.

--- static/app/utils/marked/marked.tsx
+++ static/app/utils/marked/marked.tsx
@@ -9,8 +9,6 @@ import {loadPrismLanguage} from 'sentry/utils/prism';
 // Only https and mailto, (e.g. no javascript, vbscript, data protocols)
 const safeLinkPattern = /^(https?:|mailto:)/i;
 
-const safeImagePattern = /^https?:\/\/./i;
-
 function isSafeHref(href: string, pattern: RegExp) {
   try {
     return pattern.test(decodeURIComponent(unescape(href)));
@@ -34,15 +32,6 @@ class SafeRenderer extends marked.Renderer {
       ALLOWED_TAGS,
       ALLOWED_ATTR,
     });
-  }
-
-  image(tokens: Tokens.Image) {
-    // For a bad image, return an empty string
-    if (!isSafeHref(tokens.href, safeImagePattern)) {
-      return '';
-    }
-
-    return super.image(tokens);
   }
 }
 
