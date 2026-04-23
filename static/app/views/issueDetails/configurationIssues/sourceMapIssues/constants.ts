f5d5f95cbb1d04c52a404f1e7f22b8e641623b25
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2026-04-23 23:05:01 +0600

    feat(source-map-config-issues): Addressing pr suggestions

--- static/app/views/issueDetails/configurationIssues/sourceMapIssues/constants.ts
+++ static/app/views/issueDetails/configurationIssues/sourceMapIssues/constants.ts
@@ -1,9 +1,11 @@
+import {JavascriptProcessingErrors} from 'sentry/constants/eventErrors';
+
 const SOURCE_MAP_ERROR_TYPES = [
-  'js_no_source',
-  'js_invalid_source',
-  'js_missing_sources_content',
-  'js_scraping_disabled',
-  'js_invalid_sourcemap_location',
+  JavascriptProcessingErrors.JS_MISSING_SOURCE,
+  JavascriptProcessingErrors.JS_INVALID_SOURCEMAP,
+  JavascriptProcessingErrors.JS_MISSING_SOURCES_CONTENT,
+  JavascriptProcessingErrors.JS_SCRAPING_DISABLED,
+  JavascriptProcessingErrors.JS_INVALID_SOURCEMAP_LOCATION,
 ];
 
 export const SOURCE_MAP_ERROR_TYPES_QUERY = `error_type:[${SOURCE_MAP_ERROR_TYPES.join(',')}]`;
