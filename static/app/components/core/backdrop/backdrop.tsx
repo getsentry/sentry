7cc506087dc6ac47e55fe6602be796b0b2c49e4d
Nate Moore <nate.moore@sentry.io>
2026-04-24 13:00:54 -0400

    ref(modal): use standard backdrop

--- static/app/components/core/backdrop/backdrop.tsx
+++ static/app/components/core/backdrop/backdrop.tsx
@@ -1,20 +1,28 @@
 import {useTheme} from '@emotion/react';
 import {motion} from 'framer-motion';
 
-export function Backdrop() {
+interface BackdropProps {
+  'data-test-id'?: string;
+  onClick?: (e: React.MouseEvent<HTMLDivElement>) => void;
+  zIndex?: 'drawer' | 'modal';
+}
+
+export function Backdrop({onClick, zIndex = 'drawer', ...rest}: BackdropProps) {
   const theme = useTheme();
   // TODO(design-engineering): These should be exposed as `theme.tokens`
   const background = theme.type === 'light' ? '#10082845' : '#10082080';
   return (
     <motion.div
       id="backdrop"
+      onClick={onClick}
+      data-test-id={rest['data-test-id']}
       transition={theme.motion.framer.smooth.slow}
       initial={{
         opacity: 0,
         background,
         position: 'fixed',
         inset: '0',
-        zIndex: theme.zIndex.drawer,
+        zIndex: theme.zIndex[zIndex],
       }}
       animate={{opacity: 1}}
       exit={{opacity: 0}}
