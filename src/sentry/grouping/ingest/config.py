7b47d9b2a9faf214d599ba6069bb5cb8a4943de0
Katie Byers <lobsterkatie@gmail.com>
2026-04-29 14:32:07 -0700

    fix docstring

--- src/sentry/grouping/ingest/config.py
+++ src/sentry/grouping/ingest/config.py
@@ -146,7 +146,9 @@ def update_or_set_grouping_config_if_needed(project: Project, source: str) -> st
 def is_in_transition(project: Project) -> bool:
     """
     Determine if a project is currently in a grouping transition, i.e., that it has a valid
-    secondary grouping config defined and that it's secondary grouping expiry date hasn't passed.
+    secondary grouping config defined and that its secondary grouping expiry date hasn't passed.
+
+    If out-dated secondary config options are found, clean them up.
     """
     primary_grouping_config = project.get_option("sentry:grouping_config")
     secondary_grouping_config = project.get_option("sentry:secondary_grouping_config")
