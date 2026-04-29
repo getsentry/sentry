29e3caa1528ae09c9e55727f8bbc38625308355f
George Gritsouk <989898+gggritso@users.noreply.github.com>
2026-04-29 16:45:07 -0400

    Delete useHasPlatformizedInsights and remaining orphaned exports
    All callers were removed by PRs 1-5. Clean up the leftovers:
    - Delete useHasPlatformizedInsights.tsx (last external callers gone)
    - Remove ModuleFilters, getDurationChartTitle, getThroughputChartTitle
    - Remove PAGE_SPAN_OPS, PageSpanOps, DEFAULT_SPAN_OP_SELECTION, SPAN_OP_QUERY_PARAM
    - Remove FIELD_ALIASES from queues settings

--- static/app/views/insights/common/utils/useHasPlatformizedInsights.tsx
+++
@@ -1,5 +0,0 @@
-import type {Organization} from 'sentry/types/organization';
-
-export function hasPlatformizedInsights(organization: Organization) {
-  return organization.features.includes('insights-prebuilt-dashboards');
-}
