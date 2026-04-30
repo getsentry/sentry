801c4c1d48a559b199faf7d39cbad239fdcfa504
Vjeran Grozdanic <vjeran.grozdanic@sentry.io>
2026-04-30 13:26:26 +0200

    fix comment

--- src/sentry/snuba/referrer.py
+++ src/sentry/snuba/referrer.py
@@ -677,7 +677,7 @@ class Referrer(StrEnum):
     INSIGHTS_MOBILE_HAS_TTFDCONFIGURED = "insights.mobile.hasTTFDConfigured"
     INSIGHTS_TIME_SPENT_TOTAL_TIME = "insights.time_spent.total_time"
 
-    # TODO(telex-team): temporary referrer, remove once low value spans job is stabilized
+    # TODO(telex-team): temporary referrer, remove once low value spans job is no longer needed
     LOW_VALUE_SPANS_JOB = "low_value_spans_job"
 
     METRIC_EXTRACTION_CARDINALITY_CHECK = "metric_extraction.cardinality_check"
