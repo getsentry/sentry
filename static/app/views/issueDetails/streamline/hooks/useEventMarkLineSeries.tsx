9311bfa950885414d06abf0cfa1d912dd63d06e0
TkDodo <dominik.dorfmeister@sentry.io>
2026-05-07 10:25:45 +0200

    fix: bring back tooltip-arrow

--- static/app/views/issueDetails/streamline/hooks/useEventMarkLineSeries.tsx
+++ static/app/views/issueDetails/streamline/hooks/useEventMarkLineSeries.tsx
@@ -202,6 +202,7 @@ export function useCurrentEventMarklineSeries({
                   {time}
                 </Text>
               </Flex>
+              <div className="tooltip-arrow" />
             </Stack>
           );
         },
