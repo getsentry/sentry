b051b62370f2cc5415d2e0c545d9a14c3e4abf2c
nsdeschenes <nicholas.deschenes@sentry.io>
2026-05-07 10:58:25 -0300

    Add in trace meta details

--- static/app/views/explore/metrics/metricInfoTabs/metricDetails.tsx
+++ static/app/views/explore/metrics/metricInfoTabs/metricDetails.tsx
@@ -1,6 +1,9 @@
 import {Fragment} from 'react';
 import {useTheme} from '@emotion/react';
 
+import {Container} from '@sentry/scraps/layout';
+import {Separator} from '@sentry/scraps/separator';
+
 import {EmptyStreamWrapper} from 'sentry/components/emptyStateWarning';
 import {LoadingIndicator} from 'sentry/components/loadingIndicator';
 import {IconWarning} from 'sentry/icons';
@@ -24,14 +27,23 @@ import {
   DetailsContent,
   MetricsDetailsWrapper,
 } from 'sentry/views/explore/metrics/metricInfoTabs/metricInfoTabStyles';
-import {TraceMetricKnownFieldKey} from 'sentry/views/explore/metrics/types';
+import {
+  TraceMetricKnownFieldKey,
+  type TraceMetricEventsResponseItem,
+} from 'sentry/views/explore/metrics/types';
+import {
+  isEAPTraceMeta,
+  useTraceMeta,
+} from 'sentry/views/performance/newTraceDetails/traceApi/useTraceMeta';
 
 export function MetricDetails({
   dataRow,
   ref,
+  showTelemetry,
 }: {
-  dataRow: any;
+  dataRow: TraceMetricEventsResponseItem;
   ref: React.RefObject<HTMLTableRowElement | null>;
+  showTelemetry: boolean;
 }) {
   const theme = useTheme();
   const location = useLocation();
@@ -42,17 +54,27 @@ export function MetricDetails({
   });
   const projectSlug = project?.slug ?? '';
 
-  const traceDetailResult = useMetricTraceDetail({
+  const enableQueries =
+    defined(dataRow[TraceMetricKnownFieldKey.ID]) &&
+    defined(dataRow[TraceMetricKnownFieldKey.PROJECT_ID]) &&
+    defined(dataRow[TraceMetricKnownFieldKey.TRACE]);
+
+  const {
+    data: traceDetailsData,
+    isPending,
+    isError,
+  } = useMetricTraceDetail({
     metricId: String(dataRow[TraceMetricKnownFieldKey.ID] ?? ''),
     projectId: String(dataRow[TraceMetricKnownFieldKey.PROJECT_ID] ?? ''),
     traceId: String(dataRow[TraceMetricKnownFieldKey.TRACE] ?? ''),
-    enabled:
-      defined(dataRow[TraceMetricKnownFieldKey.ID]) &&
-      defined(dataRow[TraceMetricKnownFieldKey.PROJECT_ID]) &&
-      defined(dataRow[TraceMetricKnownFieldKey.TRACE]),
+    enabled: enableQueries,
   });
 
-  const {data, isPending, isError} = traceDetailResult;
+  const traceSlug = String(dataRow[TraceMetricKnownFieldKey.TRACE] ?? '');
+  const timestamp = Number(dataRow[TraceMetricKnownFieldKey.TIMESTAMP] ?? 0);
+  const {data: traceMeta, isLoading: isTraceMetaLoading} = useTraceMeta(
+    enableQueries && showTelemetry ? [{traceSlug, timestamp}] : []
+  );
 
   if (isError) {
     return (
@@ -64,48 +86,63 @@ export function MetricDetails({
     );
   }
 
+  if (isPending || (isTraceMetaLoading && showTelemetry)) {
+    return (
+      <MetricsDetailsWrapper ref={ref}>
+        <LogDetailTableBodyCell colSpan={0}>
+          <LoadingIndicator />
+        </LogDetailTableBodyCell>
+      </MetricsDetailsWrapper>
+    );
+  }
+
+  const attributes: Record<string, TraceItemResponseAttribute['value']> = {};
+  const attributeTypes: Record<string, TraceItemResponseAttribute['type']> = {};
+  if (traceDetailsData) {
+    for (const attr of traceDetailsData.attributes) {
+      attributes[attr.name] = attr.value;
+      attributeTypes[attr.name] = attr.type;
+    }
+  }
+
   return (
-    <MetricsDetailsWrapper ref={isPending ? undefined : ref}>
+    <MetricsDetailsWrapper ref={ref}>
       <LogDetailTableBodyCell colSpan={0}>
-        {isPending && <LoadingIndicator />}
-        {!isPending && data && (
-          <Fragment>
-            <DetailsContent>
-              <LogAttributeTreeWrapper>
-                <AttributesTree
-                  attributes={data.attributes.filter(
-                    attribute => !HiddenTraceMetricDetailFields.includes(attribute.name)
-                  )}
-                  getCustomActions={getActions}
-                  renderers={LogAttributesRendererMap}
-                  rendererExtra={{
-                    attributeTypes: data.attributes.reduce<
-                      Record<string, TraceItemResponseAttribute['type']>
-                    >((acc, attr) => {
-                      acc[attr.name] = attr.type;
-                      return acc;
-                    }, {}),
-                    attributes: data.attributes.reduce<
-                      Record<string, TraceItemResponseAttribute['value']>
-                    >((acc, attr) => {
-                      acc[attr.name] = attr.value;
-                      return acc;
-                    }, {}),
-                    caseSensitiveHighlighting: false,
-                    highlightTerms: [],
-                    logColors: getLogColors(SeverityLevel.INFO, theme),
-                    location,
-                    organization,
-                    projectSlug,
-                    project,
-                    traceItemMeta: data.meta,
-                    theme,
-                  }}
-                />
-              </LogAttributeTreeWrapper>
-            </DetailsContent>
-          </Fragment>
-        )}
+        <DetailsContent>
+          {showTelemetry && isEAPTraceMeta(traceMeta) ? (
+            <Fragment>
+              <Container>
+                <pre>
+                  Logs: {traceMeta?.logs} Spans: {traceMeta?.span_count} Errors:{' '}
+                  {traceMeta?.errors}
+                </pre>
+              </Container>
+              <Separator padding="md" orientation="horizontal" />
+            </Fragment>
+          ) : null}
+          <LogAttributeTreeWrapper>
+            <AttributesTree
+              attributes={traceDetailsData.attributes.filter(
+                attribute => !HiddenTraceMetricDetailFields.includes(attribute.name)
+              )}
+              getCustomActions={getActions}
+              renderers={LogAttributesRendererMap}
+              rendererExtra={{
+                attributes,
+                attributeTypes,
+                caseSensitiveHighlighting: false,
+                highlightTerms: [],
+                logColors: getLogColors(SeverityLevel.INFO, theme),
+                location,
+                organization,
+                projectSlug,
+                project,
+                traceItemMeta: traceDetailsData.meta,
+                theme,
+              }}
+            />
+          </LogAttributeTreeWrapper>
+        </DetailsContent>
       </LogDetailTableBodyCell>
     </MetricsDetailsWrapper>
   );
