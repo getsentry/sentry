2e50fd94936e55506f97bdc1f2b1e72166fbe5ef
Lyn Nagara <1779792+lynnagara@users.noreply.github.com>
2026-05-08 12:18:52 -0700

    feat(cells): Remove legacy organization invite path (#115198)
    Any invites sent to the old path have now expired, since it has been 30
    days since the path in the invitation email was fixed.
    
    We can now safely remove the redirect that was in place.
    
    Now all organization invites only use the properly org-scoped path.

--- src/sentry/api/endpoints/accept_organization_invite.py
+++ src/sentry/api/endpoints/accept_organization_invite.py
@@ -1,10 +1,7 @@
 from __future__ import annotations
 
-import logging
-from collections.abc import Mapping
-
 from django.contrib.auth import logout
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpResponse
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.exceptions import ValidationError
@@ -20,78 +17,27 @@ from sentry.api.invite_helper import (
 )
 from sentry.demo_mode.utils import is_demo_user
 from sentry.models.authprovider import AuthProvider
-from sentry.models.organizationmapping import OrganizationMapping
-from sentry.models.organizationmembermapping import OrganizationMemberMapping
 from sentry.organizations.services.organization import RpcUserInviteContext, organization_service
-from sentry.types.cell import CellResolutionError, get_cell_by_name
 from sentry.utils import auth
-
-logger = logging.getLogger(__name__)
-
-
-def handle_empty_organization_id_or_slug(
-    member_id: int, user_id: int | None, request: HttpRequest
-) -> RpcUserInviteContext | None:
-    member_mapping: OrganizationMemberMapping | None = None
-    member_mappings: Mapping[int, OrganizationMemberMapping] = {
-        omm.organization_id: omm
-        for omm in OrganizationMemberMapping.objects.filter(organizationmember_id=member_id).all()
-    }
-    org_mappings = OrganizationMapping.objects.filter(
-        organization_id__in=list(member_mappings.keys())
-    )
-    for mapping in org_mappings:
-        try:
-            if get_cell_by_name(mapping.cell_name).is_historic_monolith_region():
-                member_mapping = member_mappings.get(mapping.organization_id)
-                break
-        except CellResolutionError:
-            pass
-
-    if member_mapping is None:
-        return None
-    invite_context = organization_service.get_invite_by_id(
-        organization_id=member_mapping.organization_id,
-        organization_member_id=member_id,
-        user_id=user_id,
-    )
-
-    logger.info(
-        "organization.member_invite.no_id_or_slug",
-        extra={
-            "member_id": member_id,
-            "org_id": member_mapping.organization_id,
-            "url": request.path,
-            "method": request.method,
-        },
-    )
-
-    return invite_context
 
 
 def get_invite_state(
     member_id: int,
-    # TODO(cells): Remove None once AcceptOrganizationInviteRedirectView is deleted
-    organization_id_or_slug: str | None,
+    organization_id_or_slug: str,
     user_id: int | None,
-    request: HttpRequest,
 ) -> RpcUserInviteContext | None:
-    if organization_id_or_slug is None:
-        return handle_empty_organization_id_or_slug(member_id, user_id, request)
-
+    if organization_id_or_slug.isdecimal():
+        invite_context = organization_service.get_invite_by_id(
+            organization_id=organization_id_or_slug,
+            organization_member_id=member_id,
+            user_id=user_id,
+        )
     else:
-        if organization_id_or_slug.isdecimal():
-            invite_context = organization_service.get_invite_by_id(
-                organization_id=organization_id_or_slug,
-                organization_member_id=member_id,
-                user_id=user_id,
-            )
-        else:
-            invite_context = organization_service.get_invite_by_slug(
-                organization_member_id=member_id,
-                slug=organization_id_or_slug,
-                user_id=user_id,
-            )
+        invite_context = organization_service.get_invite_by_slug(
+            organization_member_id=member_id,
+            slug=organization_id_or_slug,
+            user_id=user_id,
+        )
 
     return invite_context
 
@@ -126,7 +72,6 @@ class AcceptOrganizationInvite(Endpoint):
             member_id=int(member_id),
             organization_id_or_slug=organization_id_or_slug,
             user_id=user_id,
-            request=request,
         )
         if invite_context is None:
             raise ValidationError({"details": "Invalid invite code"})
