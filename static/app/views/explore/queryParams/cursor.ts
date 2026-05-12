a78c2fe17434341ab0896afa32f20022493cbbe8
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-05-12 14:26:48 -0700

    ref(explore): Deprecate getCursorFromLocation in favor of nuqs

--- static/app/views/explore/queryParams/cursor.ts
+++ static/app/views/explore/queryParams/cursor.ts
@@ -6,6 +6,7 @@ export function defaultCursor(): string {
   return '';
 }
 
+/** @deprecated Use nuqs to manage query params instead. */
 export function getCursorFromLocation(location: Location, key: string) {
   return decodeScalar(location.query?.[key], defaultCursor());
 }
