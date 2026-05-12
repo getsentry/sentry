5e6b3ca2841db8ad6ec3bc77d93bc4a8ed0d21a5
Josh Goldberg ✨ <joshua.goldberg@sentry.io>
2026-05-12 13:54:32 -0400

    fix: add default column widths

--- static/app/views/explore/logs/tables/useLogsTableColumnWidths.tsx
+++ static/app/views/explore/logs/tables/useLogsTableColumnWidths.tsx
@@ -3,6 +3,12 @@ import {useCallback, useEffect, useState, type RefObject} from 'react';
 import {useWindowSize} from 'sentry/utils/window/useWindowSize';
 import {OurLogKnownFieldKey} from 'sentry/views/explore/logs/types';
 
+type ColumnWidths = Record<string, number | string>;
+
+const DEFAULT_COLUMN_WIDTHS = {
+  [OurLogKnownFieldKey.MESSAGE]: 'minmax(90px,1fr)',
+};
+
 type LogsTableColumnWidthOptions = {
   dataLength: number;
   fields: readonly string[];
@@ -10,8 +16,6 @@ type LogsTableColumnWidthOptions = {
   isScrolling: boolean;
   tableRef: RefObject<HTMLElement | null>;
 };
-
-type ColumnWidths = Record<string, number | string>;
 
 export function useLogsTableColumnWidths({
   fields,
@@ -46,7 +50,7 @@ export function useLogsTableColumnWidths({
       Object.fromEntries(
         fields.map((field, i) => {
           if (field === OurLogKnownFieldKey.MESSAGE) {
-            return [field, 'minmax(90px, 99fr)'];
+            return [field, DEFAULT_COLUMN_WIDTHS[field]];
           }
           if (i === fields.length - 1) {
             return [field, 'minmax(0px, 1fr)'];
@@ -57,5 +61,5 @@ export function useLogsTableColumnWidths({
     );
   }, [isScrolling, isPending, dataLength, columnWidths, fields, tableRef]);
 
-  return [columnWidths, clearColumnWidths] as const;
+  return [columnWidths ?? DEFAULT_COLUMN_WIDTHS, clearColumnWidths] as const;
 }
