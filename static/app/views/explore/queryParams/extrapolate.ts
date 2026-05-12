bda369418330e7d88dd30217c272c313ea04750d
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-05-12 14:26:50 -0700

    ref(explore): Deprecate getExtrapolateFromLocation in favor of nuqs

--- static/app/views/explore/queryParams/extrapolate.ts
+++ static/app/views/explore/queryParams/extrapolate.ts
@@ -2,6 +2,7 @@ import type {Location} from 'history';
 
 import {decodeScalar} from 'sentry/utils/queryString';
 
+/** @deprecated Use nuqs to manage query params instead. */
 export function getExtrapolateFromLocation(location: Location, key: string) {
   return decodeScalar(location.query?.[key], '1') === '1';
 }
