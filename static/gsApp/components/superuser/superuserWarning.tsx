c95b106d0eb08609d61dabdb87c34b1b599a6873
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-05-12 08:02:17 +0200

    Update static/gsApp/components/superuser/superuserWarning.tsx
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- static/gsApp/components/superuser/superuserWarning.tsx
+++ static/gsApp/components/superuser/superuserWarning.tsx
@@ -19,7 +19,7 @@ import {useHasPageFrameFeature} from 'sentry/views/navigation/useHasPageFrameFea
 const POLICY_URL =
   'https://www.notion.so/sentry/Sentry-Rules-for-Handling-Customer-Data-9612532c37e14eeb943a6a584abbac99';
 
-const SUPERUSER_MESSAGE = 'You are in superuser mode · hover for exit option';
+const SUPERUSER_MESSAGE = 'You are in superuser mode / Hover for more information';
 const SUPERUSER_SEPARATOR = ' ///// ';
 const WARNING_MESSAGE = (
   <Fragment>
