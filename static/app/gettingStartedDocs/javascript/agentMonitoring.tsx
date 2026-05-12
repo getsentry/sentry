37ba6be8b487eb7488c692df86aea9931154e408
Ogi <86684834+obostjancic@users.noreply.github.com>
2026-05-12 14:26:58 +0200

    ref(onboarding): Remove redundant streamGenAiSpans comment

--- static/app/gettingStartedDocs/javascript/agentMonitoring.tsx
+++ static/app/gettingStartedDocs/javascript/agentMonitoring.tsx
@@ -80,7 +80,6 @@ Sentry.init({
   dsn: "${params.dsn.public}",
   // Tracing must be enabled for agent monitoring to work
   tracesSampleRate: 1.0,
-  // Stream GenAI spans for conversations
   streamGenAiSpans: true,
   // Add data like inputs and responses to/from LLMs and tools;
   // see https://docs.sentry.io/platforms/javascript/data-management/data-collected/ for more info
