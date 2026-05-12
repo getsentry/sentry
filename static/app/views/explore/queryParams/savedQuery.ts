5df79f1cb6d0a6d61a6f6ecf408bcc735ddf8bbd
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-05-12 14:26:26 -0700

    ref(explore): Deprecate getIdFromLocation in favor of nuqs

--- static/app/views/explore/queryParams/savedQuery.ts
+++ static/app/views/explore/queryParams/savedQuery.ts
@@ -5,6 +5,7 @@ import {decodeScalar} from 'sentry/utils/queryString';
 export const ID_KEY = 'id';
 export const TITLE_KEY = 'title';
 
+/** @deprecated Use nuqs to manage query params instead. */
 export function getIdFromLocation(location: Location, key: string): string | undefined {
   return decodeScalar(location.query?.[key]);
 }
