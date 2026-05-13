664e1b069d578fe180f8f496d78d5477df9eaa1c
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2026-05-13 12:45:55 -0400

    add more traces

--- src/sentry/tasks/llm_issue_detection/trace_data.py
+++ src/sentry/tasks/llm_issue_detection/trace_data.py
@@ -153,6 +153,7 @@ def _build_project_playlist(
 def get_project_top_transaction_traces_for_llm_detection(
     project_id: int,
     limit: int,
+    sample_multiplier: int,
     start_time_delta_minutes: int,
 ) -> list[TraceMetadataWithSpanCount]:
     """
@@ -199,7 +200,7 @@ def get_project_top_transaction_traces_for_llm_detection(
 
     transaction_rows = transactions_result.get("data", [])
     random.shuffle(transaction_rows)
-    transaction_rows = transaction_rows[:TRACE_SAMPLE_SIZE]
+    transaction_rows = transaction_rows[: TRACE_SAMPLE_SIZE * sample_multiplier]
 
     trace_ids: list[str] = []
     seen_names: set[str] = set()
