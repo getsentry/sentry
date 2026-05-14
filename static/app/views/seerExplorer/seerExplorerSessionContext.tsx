50534fc4e38243a0657821e6c04a98a7edf73406
JonasBa <jonas@badalic.com>
2026-05-13 19:08:56 -0700

    ref(seer) bump limit back to 20

--- static/app/views/seerExplorer/seerExplorerSessionContext.tsx
+++ static/app/views/seerExplorer/seerExplorerSessionContext.tsx
@@ -46,7 +46,7 @@ export function SeerExplorerSessionsProvider(props: SeerExplorerSessionsProvider
   const organization = useOrganization({allowNull: true});
 
   const query = useSeerExplorerSessionsQuery({
-    limit: 10,
+    limit: 20,
     enabled: isSeerExplorerEnabled(organization),
   });
 
