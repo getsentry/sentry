708ce92039fa730f64272d29b254a5bd8c55ff8e
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-05-19 11:37:30 -0700

    update test

--- static/app/views/settings/account/notifications/notificationSettings.spec.tsx
+++ static/app/views/settings/account/notifications/notificationSettings.spec.tsx
@@ -1,9 +1,5 @@
-import {OrganizationFixture} from 'sentry-fixture/organization';
-
-import {initializeOrg} from 'sentry-test/initializeOrg';
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
-import {OrganizationsStore} from 'sentry/stores/organizationsStore';
 import {NOTIFICATION_SETTING_FIELDS} from 'sentry/views/settings/account/notifications/fields';
 import {NotificationSettings} from 'sentry/views/settings/account/notifications/notificationSettings';
 
@@ -20,14 +16,10 @@ function renderMockRequests() {
 
 describe('NotificationSettings', () => {
   it('should render', async () => {
-    const {organization} = initializeOrg();
-    OrganizationsStore.load([organization]);
-
     renderMockRequests();
 
     render(<NotificationSettings />);
 
-    // There are 8 notification setting Selects/Toggles.
     for (const field of [
       'alerts',
       'workflow',
@@ -37,63 +29,13 @@ describe('NotificationSettings', () => {
       'email',
       'personalActivityNotifications',
       'selfAssignOnResolve',
-    ] as const) {
-      expect(
-        await screen.findByText(String(NOTIFICATION_SETTING_FIELDS[field].label))
-      ).toBeInTheDocument();
-    }
-    expect(screen.getByText('Issue Alerts')).toBeInTheDocument();
-  });
-
-  it('renders quota section with feature flag', async () => {
-    const {organization} = initializeOrg({
-      organization: {
-        features: ['user-spend-notifications-settings'],
-      },
-    });
-    OrganizationsStore.load([organization]);
-
-    renderMockRequests();
-
-    render(<NotificationSettings />);
-
-    // There are 9 notification setting Selects/Toggles.
-
-    for (const field of [
-      'alerts',
-      'workflow',
-      'deploy',
-      'approval',
-      'reports',
-      'email',
       'quota',
-      'personalActivityNotifications',
-      'selfAssignOnResolve',
+      'spikeProtection',
     ] as const) {
       expect(
         await screen.findByText(String(NOTIFICATION_SETTING_FIELDS[field].label))
       ).toBeInTheDocument();
     }
     expect(screen.getByText('Issue Alerts')).toBeInTheDocument();
-  });
-
-  it('renders spend section instead of quota section with feature flag', async () => {
-    const {organization} = initializeOrg({
-      organization: {
-        features: ['user-spend-notifications-settings', 'spend-visibility-notifications'],
-      },
-    });
-
-    const organizationNoFlag = OrganizationFixture();
-    organizationNoFlag.features.push('user-spend-notifications-settings');
-    OrganizationsStore.load([organization, organizationNoFlag]);
-
-    renderMockRequests();
-
-    render(<NotificationSettings />);
-
-    expect(await screen.findByText('Spend')).toBeInTheDocument();
-
-    expect(screen.queryByText('Quota')).not.toBeInTheDocument();
   });
 });
