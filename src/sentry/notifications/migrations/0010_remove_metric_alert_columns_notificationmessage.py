34b7ae28468f59f1bcdded7dbb76ad6e5d06023e
Colleen O'Rourke <colleen@sentry.io>
2026-05-20 12:19:43 -0700

    update sql

--- src/sentry/notifications/migrations/0010_remove_metric_alert_columns_notificationmessage.py
+++ src/sentry/notifications/migrations/0010_remove_metric_alert_columns_notificationmessage.py
@@ -40,7 +40,12 @@ class Migration(CheckedMigration):
         migrations.SeparateDatabaseAndState(
             database_operations=[
                 SafeRunSQL(
-                    sql='ALTER TABLE "sentry_notificationmessage" ALTER COLUMN "group_id" SET NOT NULL',
+                    sql="""
+                    ALTER TABLE "sentry_notificationmessage" ADD CONSTRAINT "sentry_notificationmessage_group_id_notnull" CHECK ("group_id" IS NOT NULL) NOT VALID;
+                    ALTER TABLE "sentry_notificationmessage" VALIDATE CONSTRAINT "sentry_notificationmessage_group_id_notnull";
+                    ALTER TABLE "sentry_notificationmessage" ALTER COLUMN "group_id" SET NOT NULL;
+                    ALTER TABLE "sentry_notificationmessage" DROP CONSTRAINT "sentry_notificationmessage_group_id_notnull";
+                    """,
                     reverse_sql='ALTER TABLE "sentry_notificationmessage" ALTER COLUMN "group_id" DROP NOT NULL',
                     hints={"tables": ["sentry_notificationmessage"]},
                 ),
