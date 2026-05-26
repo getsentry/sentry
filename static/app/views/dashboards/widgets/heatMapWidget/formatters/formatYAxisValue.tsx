f37db701a987d8fe9a8168b86be933d43bab09b6
nikkikapadia <nikki.kapadia@sentry.io>
2026-05-26 16:34:34 -0400

    use number formatting recommendations

--- static/app/views/dashboards/widgets/heatMapWidget/formatters/formatYAxisValue.tsx
+++ static/app/views/dashboards/widgets/heatMapWidget/formatters/formatYAxisValue.tsx
@@ -8,43 +8,32 @@ import {
   SizeUnit,
 } from 'sentry/utils/discover/fields';
 import {formatAbbreviatedNumber, formatDollars} from 'sentry/utils/formatters';
+import {formatNumberWithDynamicDecimalPoints} from 'sentry/utils/number/formatNumberWithDynamicDecimalPoints';
 import {formatPercentage} from 'sentry/utils/number/formatPercentage';
 import {convertDuration} from 'sentry/utils/unitConversion/convertDuration';
 import {convertSize} from 'sentry/utils/unitConversion/convertSize';
+import {
+  NUMBER_MIN_VALUE,
+  NUMBER_MAX_FRACTION_DIGITS,
+} from 'sentry/views/dashboards/widgets/common/settings';
 import {
   isADurationUnit,
   isARateUnit,
   isASizeUnit,
 } from 'sentry/views/dashboards/widgets/common/typePredicates';
-
-import {formatYAxisDuration} from './formatYAxisDuration';
+import {formatYAxisDuration} from 'sentry/views/dashboards/widgets/timeSeriesWidget/formatters/formatYAxisDuration';
 
 /**
- * Format a value for the Y axis on an ECharts graph.
+ * Format a value for the Y axis on an ECharts heat map graph.
  *
  * The values on the Y axis are chosen by ECharts. ECharts will automatically
- * select, when possible, nice round values. We always format the chosen value
- * at _full precision_ and trust EChart's choices. e.g., if it chooses "100", we
- * should show "100", because it probably chose a Y axis scale like "0, 100,
- * 200, 300". If it chooses "17.22" we should render "17.22" and not truncate
- * the value, since ECharts is probably using a scale like "17.20, 17.21, 17.22"
- * because the Y axis range is narrow. Similarly, a value like "0.00006"
- * probably means the range was narrow starting from 0, "0.00000, 0.00002
- * 0.00004 0.00006" and so on.
+ * select, when possible, nice round values. With heat maps this is not the case.
+ * Since the Y axis in heat maps are considered categories to ECharts,
+ * We need to format the values ourselves to the precision we'd like to see,
+ * especially with floating point numbers.
  *
- * This concept does not apply to:
- * 1. Integers. There are no fractional values!
- * 2. Durations. Durations have multiplier prefixes all the way down to nanoseconds, which should be enough. If needed, we can introduce smaller multipliers.
- * 3. Sizes. Sizes are effectively integers, and we have "byte" is already
- * small. It's an extremely rare case that we have fractional bytes with high
- * precision.
- * Rates and percentages would benefit from more precision, but it's not as critical there.
- *
- * The downside of this approach is that if the precision varies on the scale
- * (e.g., "0, 0.5, 1, 1.5") the Y axis labels are not well-aligned. This is a
- * limitation of ECharts, since it doesn't provide information about the entire
- * scale to each number. The ideal solution here would be to coordinate the
- * formatting of all the values.
+ * The rest of the logic is the same as the time series widget Y axis formatter
+ * (static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatYAxisValue.tsx).
  */
 export function formatYAxisValue(value: number, type: string, unit?: string): string {
   if (value === 0) {
@@ -58,9 +47,12 @@ export function formatYAxisValue(value: number, type: string, unit?: string): st
       if (Number.isInteger(value)) {
         return formatAbbreviatedNumber(value);
       }
-      return value.toLocaleString(undefined, {
-        maximumFractionDigits: 20,
-      });
+      if (value > 0 && value < NUMBER_MIN_VALUE) {
+        return value.toLocaleString(undefined, {
+          maximumFractionDigits: NUMBER_MAX_FRACTION_DIGITS,
+        });
+      }
+      return formatNumberWithDynamicDecimalPoints(value, NUMBER_MAX_FRACTION_DIGITS);
     case 'percentage':
       return formatPercentage(value, 3);
     case 'duration': {
