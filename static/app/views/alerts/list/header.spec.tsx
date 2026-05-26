32f21bd0a542983fddb0d8f48974f6c464ddfaf8
Nate Moore <nate.moore@sentry.io>
2026-05-26 13:38:21 -0400

    fix(tests): update tests for page-frame cleanup

--- static/app/views/alerts/list/header.spec.tsx
+++ static/app/views/alerts/list/header.spec.tsx
@@ -1,38 +1,16 @@
-import {ProjectFixture} from 'sentry-fixture/project';
-
-import {initializeOrg} from 'sentry-test/initializeOrg';
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
-import {PageFiltersStore} from 'sentry/components/pageFilters/store';
-import {ProjectsStore} from 'sentry/stores/projectsStore';
 import {AlertHeader} from 'sentry/views/alerts/list/header';
 
 describe('AlertHeader', () => {
-  const project = ProjectFixture();
-  const {organization} = initializeOrg();
-  beforeEach(() => {
-    PageFiltersStore.init();
-    PageFiltersStore.onInitializeUrlState({
-      projects: [parseInt(project.id, 10)],
-      environments: [],
-      datetime: {
-        period: '7d',
-        start: null,
-        end: null,
-        utc: null,
-      },
-    });
-    ProjectsStore.init();
-    ProjectsStore.loadInitialData([project]);
+  it('renders header actions in TopBar.Slot', () => {
+    render(<AlertHeader activeTab="stream" />);
+    expect(screen.getByRole('button', {name: 'Settings'})).toBeInTheDocument();
   });
 
-  it('should pass global selection project to create alert button', () => {
-    render(<AlertHeader activeTab="stream" />, {
-      organization,
-    });
-    expect(screen.getByRole('button', {name: 'Create Alert'})).toHaveAttribute(
-      'href',
-      '/organizations/org-slug/issues/alerts/wizard/?referrer=alert_stream&project=project-slug'
-    );
+  it('renders tabs with correct links', () => {
+    render(<AlertHeader activeTab="stream" />);
+    expect(screen.getByRole('tab', {name: 'Alert Rules'})).toBeInTheDocument();
+    expect(screen.getByRole('tab', {name: 'History'})).toBeInTheDocument();
   });
 });
