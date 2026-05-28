cc0aea89539cccb2eef004de274b0ec10169a96e
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-05-28 14:07:30 -0700

    fix the rest of the setup wizard types

--- static/app/views/setupWizard/types.tsx
+++ static/app/views/setupWizard/types.tsx
@@ -1,10 +1,6 @@
-import type {Organization, OrganizationSummary} from 'sentry/types/organization';
+import type {OrganizationSummary} from 'sentry/types/organization';
 import type {Region} from 'sentry/types/system';
 
 export type OrganizationSummaryWithRegion = OrganizationSummary & {
-  region: Region;
-};
-
-export type OrganizationWithRegion = Organization & {
   region: Region;
 };
