50eb3b51015f530b7b0695faf071019293339b24
Mark Story <mark@mark-story.com>
2026-06-01 14:58:13 -0400

    chore(cells) Remove OrganizationSlugReservation replication
    This is the first step to remove OrganizationSlugReservationReplica.
    
    Currently we replicate every write to OrganizationSlugReservation into
    the organization's cell but we never read those records. Removing this
    replication saves a outbox during provisioning which helps reduce
    complexity, and latency.
    
    Refs INFRENG-317

--- src/sentry/models/organizationslugreservation.py
+++ src/sentry/models/organizationslugreservation.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections.abc import Collection, Mapping
 from enum import IntEnum
 from typing import Any
 
@@ -8,11 +7,10 @@ from django.db import models
 from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
+from sentry.db.models import Model
 from sentry.db.models.base import control_silo_model, sane_repr
 from sentry.db.models.fields import BoundedBigIntegerField
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
-from sentry.hybridcloud.outbox.base import ReplicatedControlModel
-from sentry.hybridcloud.outbox.category import OutboxCategory
 from sentry.hybridcloud.rpc import CELL_NAME_LENGTH
 
 
@@ -27,10 +25,8 @@ class OrganizationSlugReservationType(IntEnum):
 
 
 @control_silo_model
-class OrganizationSlugReservation(ReplicatedControlModel):
+class OrganizationSlugReservation(Model):
     __relocation_scope__ = RelocationScope.Excluded
-    category = OutboxCategory.ORGANIZATION_SLUG_RESERVATION_UPDATE
-    replication_version = 1
 
     slug = models.SlugField(unique=True, null=False)
     organization_id = HybridCloudForeignKey("sentry.organization", null=False, on_delete="CASCADE")
@@ -65,32 +61,3 @@ class OrganizationSlugReservation(ReplicatedControlModel):
 
         kwds.pop("unsafe_write")
         return super().update(*args, **kwds)
-
-    def outbox_cell_names(self) -> Collection[str]:
-        return [self.cell_name]
-
-    def handle_async_replication(self, cell_name: str, shard_identifier: int) -> None:
-        from sentry.hybridcloud.services.control_organization_provisioning.serial import (
-            serialize_slug_reservation,
-        )
-        from sentry.hybridcloud.services.replica import cell_replica_service
-
-        serialized = serialize_slug_reservation(self)
-        cell_replica_service.upsert_replicated_org_slug_reservation(
-            slug_reservation=serialized, cell_name=self.cell_name
-        )
-
-    @classmethod
-    def handle_async_deletion(
-        cls,
-        identifier: int,
-        cell_name: str,
-        shard_identifier: int,
-        payload: Mapping[str, Any] | None,
-    ) -> None:
-        from sentry.hybridcloud.services.replica import cell_replica_service
-
-        cell_replica_service.delete_replicated_org_slug_reservation(
-            cell_name=cell_name,
-            organization_slug_reservation_id=identifier,
-        )
