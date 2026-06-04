ce744865dcd16c261df2357309f3fa56a4294c68
Suejung Shin <suejung.shin@sentry.io>
2026-06-04 09:19:19 -0700

    move log up

--- src/sentry/seer/code_review/webhooks/logging.py
+++ src/sentry/seer/code_review/webhooks/logging.py
@@ -5,6 +5,7 @@ from collections.abc import Mapping
 from typing import TYPE_CHECKING
 
 from sentry import features
+from sentry.models.organization import Organization as Org
 
 if TYPE_CHECKING:
     from sentry.models.organization import Organization
@@ -22,8 +23,6 @@ def debug_log(
     level: int = logging.INFO,
     exc_info: bool = False,
 ) -> None:
-    from sentry.models.organization import Organization as Org
-
     if isinstance(organization, int):
         try:
             organization = Org.objects.get_from_cache(id=organization)
