b533bd47c5403b8fef9bbc98746e7767063ad4a4
Josh Goldberg ✨ <joshua.goldberg@sentry.io>
2026-06-05 12:11:14 -0400

    more padding

--- static/app/views/explore/logs/styles.tsx
+++ static/app/views/explore/logs/styles.tsx
@@ -498,7 +498,10 @@ export const FloatingBackToTopContainer = styled('div')<{
   position: ${p => p.position};
   z-index: 1;
   opacity: ${p => (p.inReplay ? 1 : 0.9)};
-  top: ${p => (p.inReplay ? p.theme.space.md : `${p.topOffset ?? 65}px`)};
+  top: ${p =>
+    p.inReplay
+      ? p.theme.space.md
+      : `calc(${p.topOffset ?? 65}px + ${p.theme.space.xl})`};
   width: var(--floatingWidth);
   display: flex;
   justify-content: center;
