2f86e2150ac43bd47ddf255e3262e16c313bae25
Gabe Villalobos <gabriel.villalobos@sentry.io>
2026-06-05 15:09:09 -0700

    chore(cells): Adds Error Embed pages to metric allowed paths

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -2807,6 +2807,7 @@ SENTRY_REQUEST_METRIC_ALLOWED_PATHS = (
     "sentry.sentry_apps.api.endpoints",
     "sentry.preprod.api.endpoints",
     "sentry.workflow_engine.endpoints",
+    "sentry.feedback.endpoints",
 )
 SENTRY_MAIL_ADAPTER_BACKEND = "sentry.mail.adapter.MailAdapter"
 
