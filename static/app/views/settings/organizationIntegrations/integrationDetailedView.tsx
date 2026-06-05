18025368b322bffbfc2140ebc184dc754ee35e6d
Christinarlong <christina.long@sentry.io>
2026-06-05 15:04:11 -0700

    ref(webhooks): Add dedicated /integrations/webhooks/ route, revert integrationDetailedView shim

--- static/app/views/settings/organizationIntegrations/integrationDetailedView.tsx
+++ static/app/views/settings/organizationIntegrations/integrationDetailedView.tsx
@@ -47,7 +47,6 @@ import {IntegrationLayout} from 'sentry/views/settings/organizationIntegrations/
 import {InstalledIntegration} from 'sentry/views/settings/organizationIntegrations/installedIntegration';
 import {IntegrationButton} from 'sentry/views/settings/organizationIntegrations/integrationButton';
 import {IntegrationContext} from 'sentry/views/settings/organizationIntegrations/integrationContext';
-import {WebhookDetailedView} from 'sentry/views/settings/organizationIntegrations/webhookDetailedView';
 
 // Show the features tab if the org has features for the integration
 const integrationFeatures = ['slack'];
@@ -104,7 +103,7 @@ function makeIntegrationQueryKey({
 
 const tabs: IntegrationTab[] = ['overview', 'configurations', 'features'];
 
-function DefaultView() {
+export default function IntegrationDetailedView() {
   const api = useApi({persistInFlight: true});
   const queryClient = useQueryClient();
   const [activeTab, setActiveTab] = useQueryState(
@@ -556,18 +555,4 @@ function DefaultView() {
       />
     </SentryDocumentTitle>
   );
-}
-
-export default function IntegrationDetailedView() {
-  const {integrationSlug} = useParams<{integrationSlug: string}>();
-  const organization = useOrganization();
-
-  if (
-    integrationSlug === 'webhooks' &&
-    organization.features.includes('legacy-webhook-ui')
-  ) {
-    return <WebhookDetailedView />;
-  }
-
-  return <DefaultView />;
 }
--- static/app/views/settings/organizationIntegrations/webhookDetailedView.tsx
+++ static/app/views/settings/organizationIntegrations/webhookDetailedView.tsx
@@ -196,3 +196,5 @@ export function WebhookDetailedView() {
     />
   );
 }
+
+export default WebhookDetailedView;
