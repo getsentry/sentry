089cb519f49d4c3947586836c82093eafac7481d
Dan Fuller <dfuller@sentry.io>
2026-06-09 14:11:06 -0700

    ref(flags): Graduate organizations:processing-errors-eap (#117224)
    - Removes the `organizations:processing-errors-eap` feature flag, which
    has been rolled out to all organizations via flagpole
    - Processing errors are now unconditionally stored in EAP

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -214,7 +214,6 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:preprod-snapshots", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enables the playstation ingestion in relay
     manager.add("organizations:relay-playstation-ingestion", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
-    manager.add("organizations:processing-errors-eap", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     manager.add("organizations:sourcemap-issue-detection", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable profiling
     manager.add("organizations:profiling", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
