aea25466aa44a9c22ce7bd34609174ea587e54e2
William Mak <william@wmak.io>
2026-06-10 17:27:04 -0400

    ref: cleanup unused code

--- src/sentry/api/endpoints/organization_spans_fields.py
+++ src/sentry/api/endpoints/organization_spans_fields.py
@@ -1,15 +1,12 @@
 import logging
-from abc import ABC, abstractmethod
 from datetime import timedelta
 from typing import Literal
 
-from google.protobuf.timestamp_pb2 import Timestamp
 from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 from sentry_protos.snuba.v1.endpoint_trace_item_attributes_pb2 import (
     TraceItemAttributeNamesRequest,
-    TraceItemAttributeValuesRequest,
 )
 from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey
 
@@ -18,7 +15,6 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import cell_silo_endpoint
 from sentry.api.bases import NoProjects, OrganizationEventsEndpointBase
-from sentry.api.event_search import translate_escape_sequences
 from sentry.api.paginator import ChainPaginator
 from sentry.api.serializers import serialize
 from sentry.api.utils import handle_query_errors
@@ -30,9 +26,7 @@ from sentry.search.eap.resolver import SearchResolver
 from sentry.search.eap.spans.definitions import SPAN_DEFINITIONS
 from sentry.search.eap.types import SearchResolverConfig, SupportedTraceItemType
 from sentry.search.eap.utils import can_expose_attribute_to_api, translate_internal_to_public_alias
-from sentry.search.events.types import SnubaParams
 from sentry.snuba.referrer import Referrer
-from sentry.tagstore.types import TagValue
 from sentry.utils import snuba_rpc
 
 logger = logging.getLogger(__name__)
@@ -144,142 +138,3 @@ class OrganizationSpansFieldsEndpoint(OrganizationSpansFieldsEndpointBase):
             default_per_page=max_span_tags,
             max_per_page=max_span_tags,
         )
-
-
-class BaseSpanFieldValuesAutocompletionExecutor(ABC):
-    PROJECT_SLUG_KEYS = {"project", "project.name"}
-    PROJECT_ID_KEYS = {"project.id"}
-
-    def __init__(
-        self,
-        organization: Organization,
-        snuba_params: SnubaParams,
-        key: str,
-        query: str | None,
-        max_span_tag_values: int,
-    ):
-        self.organization = organization
-        self.snuba_params = snuba_params
-        self.key = key
-        self.query = query or ""
-        self.max_span_tag_values = max_span_tag_values
-
-    @abstractmethod
-    def execute(self) -> list[TagValue]:
-        raise NotImplementedError
-
-    def project_id_autocomplete_function(self) -> list[TagValue]:
-        return [
-            TagValue(
-                key=self.key,
-                value=str(project.id),
-                times_seen=None,
-                first_seen=None,
-                last_seen=None,
-            )
-            for project in self.snuba_params.projects
-            if not self.query or self.query in str(project.id)
-        ]
-
-    def project_slug_autocomplete_function(self) -> list[TagValue]:
-        return [
-            TagValue(
-                key=self.key,
-                value=project.slug,
-                times_seen=None,
-                first_seen=None,
-                last_seen=None,
-            )
-            for project in self.snuba_params.projects
-            if not self.query or self.query in project.slug
-        ]
-
-
-class EAPSpanFieldValuesAutocompletionExecutor(BaseSpanFieldValuesAutocompletionExecutor):
-    def __init__(
-        self,
-        organization: Organization,
-        snuba_params: SnubaParams,
-        key: str,
-        query: str | None,
-        max_span_tag_values: int,
-    ):
-        super().__init__(organization, snuba_params, key, query, max_span_tag_values)
-        self.resolver = SearchResolver(
-            params=snuba_params, config=SearchResolverConfig(), definitions=SPAN_DEFINITIONS
-        )
-        self.search_type, self.attribute_key = self.resolve_attribute_key(key, snuba_params)
-
-    def resolve_attribute_key(
-        self, key: str, snuba_params: SnubaParams
-    ) -> tuple[constants.SearchType, AttributeKey]:
-        resolved, _ = self.resolver.resolve_attribute(key)
-        return resolved.search_type, resolved.proto_definition
-
-    def execute(self) -> list[TagValue]:
-        if self.key in self.PROJECT_ID_KEYS:
-            return self.project_id_autocomplete_function()
-
-        if self.key in self.PROJECT_SLUG_KEYS:
-            return self.project_slug_autocomplete_function()
-
-        if self.search_type == "boolean":
-            return self.boolean_autocomplete_function()
-
-        if self.search_type == "string":
-            return self.string_autocomplete_function()
-
-        return []
-
-    def boolean_autocomplete_function(self) -> list[TagValue]:
-        return [
-            TagValue(
-                key=self.key,
-                value="false",
-                times_seen=None,
-                first_seen=None,
-                last_seen=None,
-            ),
-            TagValue(
-                key=self.key,
-                value="true",
-                times_seen=None,
-                first_seen=None,
-                last_seen=None,
-            ),
-        ]
-
-    def string_autocomplete_function(self) -> list[TagValue]:
-        start_timestamp = Timestamp()
-        start_timestamp.FromDatetime(
-            self.snuba_params.start_date.replace(hour=0, minute=0, second=0, microsecond=0)
-        )
-
-        end_timestamp = Timestamp()
-        end_timestamp.FromDatetime(
-            self.snuba_params.end_date.replace(hour=0, minute=0, second=0, microsecond=0)
-            + timedelta(days=1)
-        )
-
-        query = translate_escape_sequences(self.query)
-
-        meta = self.resolver.resolve_meta(referrer=Referrer.API_SPANS_TAG_VALUES_RPC.value)
-        rpc_request = TraceItemAttributeValuesRequest(
-            meta=meta,
-            key=self.attribute_key,
-            value_substring_match=query,
-            limit=self.max_span_tag_values,
-        )
-        rpc_response = snuba_rpc.attribute_values_rpc(rpc_request)
-
-        return [
-            TagValue(
-                key=self.key,
-                value=value,
-                times_seen=None,
-                first_seen=None,
-                last_seen=None,
-            )
-            for value in rpc_response.values
-            if value
-        ]
