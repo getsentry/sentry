fe9a73cd8ab3b9d3984e09258df1b60f740bb0ea
Malachi Willey <malwilley@gmail.com>
2026-06-11 10:35:40 -0700

    Fix mypy

--- tests/sentry/issues/test_progress.py
+++ tests/sentry/issues/test_progress.py
@@ -9,27 +9,27 @@ from sentry.types.activity import ActivityType
 
 
 class GetGroupProgressStatesTest(TestCase):
-    def test_empty_group_list(self):
+    def test_empty_group_list(self) -> None:
         assert get_group_progress_states([]) == {}
 
-    def test_no_activities_unassigned(self):
+    def test_no_activities_unassigned(self) -> None:
         group = self.create_group()
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.IDENTIFIED
 
-    def test_no_activities_assigned(self):
+    def test_no_activities_assigned(self) -> None:
         group = self.create_group()
         GroupAssignee.objects.assign(group, self.user)
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.TRIAGED
 
-    def test_diagnosed_state(self):
+    def test_diagnosed_state(self) -> None:
         group = self.create_group()
         self.create_group_activity(group=group, type=ActivityType.SEER_RCA_COMPLETED.value)
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.DIAGNOSED
 
-    def test_fix_proposed_state(self):
+    def test_fix_proposed_state(self) -> None:
         group = self.create_group()
         self.create_group_activity(
             group=group, type=ActivityType.SET_RESOLVED_IN_PULL_REQUEST.value
@@ -37,31 +37,31 @@ class GetGroupProgressStatesTest(TestCase):
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.FIX_PROPOSED
 
-    def test_fix_proposed_seer_pr(self):
+    def test_fix_proposed_seer_pr(self) -> None:
         group = self.create_group()
         self.create_group_activity(group=group, type=ActivityType.SEER_PR_CREATED.value)
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.FIX_PROPOSED
 
-    def test_fix_applied_state(self):
+    def test_fix_applied_state(self) -> None:
         group = self.create_group()
         self.create_group_activity(group=group, type=ActivityType.SET_RESOLVED.value)
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.FIX_APPLIED
 
-    def test_fix_applied_in_commit(self):
+    def test_fix_applied_in_commit(self) -> None:
         group = self.create_group()
         self.create_group_activity(group=group, type=ActivityType.SET_RESOLVED_IN_COMMIT.value)
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.FIX_APPLIED
 
-    def test_fix_applied_in_release(self):
+    def test_fix_applied_in_release(self) -> None:
         group = self.create_group()
         self.create_group_activity(group=group, type=ActivityType.SET_RESOLVED_IN_RELEASE.value)
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.FIX_APPLIED
 
-    def test_highest_state_wins(self):
+    def test_highest_state_wins(self) -> None:
         group = self.create_group()
         now = timezone.now()
         self.create_group_activity(
@@ -77,7 +77,7 @@ class GetGroupProgressStatesTest(TestCase):
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.FIX_APPLIED
 
-    def test_regression_resets_progress(self):
+    def test_regression_resets_progress(self) -> None:
         group = self.create_group()
         now = timezone.now()
         self.create_group_activity(
@@ -93,7 +93,7 @@ class GetGroupProgressStatesTest(TestCase):
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.IDENTIFIED
 
-    def test_regression_resets_to_triaged_when_assigned(self):
+    def test_regression_resets_to_triaged_when_assigned(self) -> None:
         group = self.create_group()
         GroupAssignee.objects.assign(group, self.user)
         now = timezone.now()
@@ -110,7 +110,7 @@ class GetGroupProgressStatesTest(TestCase):
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.TRIAGED
 
-    def test_activity_after_regression_counts(self):
+    def test_activity_after_regression_counts(self) -> None:
         group = self.create_group()
         now = timezone.now()
         self.create_group_activity(
@@ -131,7 +131,7 @@ class GetGroupProgressStatesTest(TestCase):
         result = get_group_progress_states([group.id])
         assert result[group.id] == IssueProgressState.DIAGNOSED
 
-    def test_bulk_multiple_groups(self):
+    def test_bulk_multiple_groups(self) -> None:
         group1 = self.create_group()
         group2 = self.create_group()
         group3 = self.create_group()
