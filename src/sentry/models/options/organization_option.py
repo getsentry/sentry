ca3417abb9334559c45bc17c48b7deb442488299
Chris Klochek <christopher.klochek@sentry.io>
2026-06-15 14:07:47 -0400

    just clear the local cache if the shared cache is empty

--- src/sentry/models/options/organization_option.py
+++ src/sentry/models/options/organization_option.py
@@ -76,11 +76,10 @@ class OrganizationOptionManager(OptionManager["OrganizationOption"]):
     def reload_task_local_cache(self, organization_id: int):
         cache_key = self._make_key(organization_id)
 
+        # Reload the local cache with what's in the shared cache.  If the shared cache is
+        # not populated, then subsequent accesses (via get_all_values) will re-generate the
+        # value with an actual query.
         result = cache.get(cache_key)
-        if result is None:
-            result = {i.key: i.value for i in self.filter(organization=organization_id)}
-            cache.set(cache_key, result)
-
         self._option_cache[cache_key] = result
 
     def reload_cache(self, organization_id: int, update_reason: str) -> Mapping[str, Any]:
