f9d254d06d7f349347491d0e0ea3e3095de4fad0
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-06-15 13:42:12 +0200

    json.dumps dict

--- src/sentry/data_export/tasks.py
+++ src/sentry/data_export/tasks.py
@@ -40,7 +40,7 @@ from sentry.models.files.utils import DEFAULT_BLOB_SIZE, MAX_FILE_SIZE, Assemble
 from sentry.silo.base import SiloMode
 from sentry.tasks.base import instrumented_task
 from sentry.taskworker.namespaces import export_tasks
-from sentry.utils import metrics
+from sentry.utils import json, metrics
 from sentry.utils.db import atomic_transaction
 
 logger = logging.getLogger(__name__)
@@ -873,4 +873,4 @@ def _set_data_on_scope(data_export: ExportedData) -> None:
         sentry_sdk.set_tag("export.dataset", str(qi.get("dataset")))
         sentry_sdk.set_attribute("export.dataset", str(qi.get("dataset")))
     sentry_sdk.set_extra("export.query", data_export.query_info)
-    sentry_sdk.set_attribute("export.query", data_export.query_info)
+    sentry_sdk.set_attribute("export.query", json.dumps(data_export.query_info))
