e22c12e7f9476f1af967d6c46229adcfc9b0b605
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-06-15 09:12:46 +0200

    ref(o11y): Add set_attribute calls in Ingest files

--- src/sentry/reprocessing2.py
+++ src/sentry/reprocessing2.py
@@ -394,8 +394,12 @@ def buffered_delete_old_primary_hash(
 
     scope = sentry_sdk.get_isolation_scope()
     scope.set_tag("project_id", project_id)
+    scope.set_attribute("project_id", project_id)
     scope.set_tag("old_group_id", group_id)
+    scope.set_attribute("old_group_id", group_id)
     scope.set_tag("old_primary_hash", old_primary_hash)
+    if old_primary_hash is not None:
+        scope.set_attribute("old_primary_hash", old_primary_hash)
 
     with sentry_sdk.start_span(
         op="sentry.reprocessing2.buffered_delete_old_primary_hash.flush_events"
@@ -678,6 +682,7 @@ def start_group_reprocessing(
         )
 
     sentry_sdk.set_extra("event_count", event_count)
+    sentry_sdk.set_attribute("event_count", event_count)
 
     if max_events is not None:
         event_count = min(max_events, event_count)
