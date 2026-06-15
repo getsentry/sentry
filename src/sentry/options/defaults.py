f9e613ddb1cfc76bc20ae7c56445e304bb9a8624
Colton Allen <colton.allen@sentry.io>
2026-06-15 09:07:29 -0500

    Exclusively use msgspec span decoder

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3015,11 +3015,6 @@ register(
     default=[],
     flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
 )
-register(
-    "spans.buffer.use-msgspec-decoder",
-    default=0.0,
-    flags=FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
-)
 # Segments consumer
 register(
     "spans.process-segments.consumer.enable",
