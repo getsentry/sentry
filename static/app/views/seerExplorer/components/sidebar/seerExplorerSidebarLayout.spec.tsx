e6a8339cf9148b10f3e84f78a4f79b47675828ff
TkDodo <dominik.dorfmeister@sentry.io>
2026-06-17 14:32:52 +0200

    remove test timeout

--- static/app/views/seerExplorer/components/sidebar/seerExplorerSidebarLayout.spec.tsx
+++ static/app/views/seerExplorer/components/sidebar/seerExplorerSidebarLayout.spec.tsx
@@ -189,9 +189,7 @@ describe('SeerExplorerSidebarLayout', () => {
     expect(dividerDirection()).toBe('updown');
 
     await userEvent.click(screen.getByRole('button', {name: 'Dock position'}));
-    await userEvent.click(
-      await screen.findByRole('menuitemradio', {name: 'Right'}, {timeout: 5000})
-    );
+    await userEvent.click(await screen.findByRole('menuitemradio', {name: 'Right'}));
 
     await waitFor(() => expect(dividerDirection()).toBe('leftright'));
     await waitFor(() =>
