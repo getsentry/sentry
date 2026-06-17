728376b7612e7a6506d3672c699e169792070ccf
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-06-17 11:25:27 +0200

    add source and custom_sampling_context parameters

--- src/sentry/utils/tracing.py
+++ src/sentry/utils/tracing.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING, Any
 
 import sentry_sdk
+from sentry_sdk.scope import Scope
 from sentry_sdk.traces import NoOpStreamedSpan, StreamedSpan
 from sentry_sdk.tracing import NoOpSpan, Span, Transaction
 from sentry_sdk.tracing_utils import has_span_streaming_enabled
@@ -13,7 +14,9 @@ def start_span(
     *,
     name: str,
     op: str | None = None,
+    source: str | None = None,
     sampled: bool | None = None,
+    custom_sampling_context: dict[str, Any] | None = None,
     transaction: bool = False,
 ) -> Transaction | NoOpSpan | StreamedSpan | Span:
     """
@@ -22,18 +25,30 @@ def start_span(
     Accepts the minimum set of arguments currently used by this repo's `sentry_sdk.start_span()` and `sentry_sdk.start_transaction()` call sites.
     """
     span_streaming = has_span_streaming_enabled(sentry_sdk.get_client().options)
-    if span_streaming and sampled is not False and transaction:
-        return sentry_sdk.traces.start_span(
-            name=name,
-            attributes={} if op is None else {"sentry.op": op},
-            parent_span=None,
-        )
-    elif span_streaming and sampled is not False:
-        return sentry_sdk.traces.start_span(
-            name=name,
-            attributes={} if op is None else {"sentry.op": op},
-        )
-    elif span_streaming:
+    if span_streaming:
+        if custom_sampling_context is not None:
+            Scope.set_custom_sampling_context(custom_sampling_context)
+
+        attributes = {}
+        if op is not None:
+            attributes["sentry.op"] = op
+
+        if source is not None:
+            attributes["sentry.span.source"] = source
+
+        if sampled is not False and transaction:
+            return sentry_sdk.traces.start_span(
+                name=name,
+                attributes=attributes,
+                parent_span=None,
+            )
+
+        if sampled is not False:
+            return sentry_sdk.traces.start_span(
+                name=name,
+                attributes=attributes,
+            )
+
         return NoOpStreamedSpan(
             scope=sentry_sdk.get_current_scope(),
             unsampled_reason="sample_rate",
@@ -47,7 +62,12 @@ def start_span(
         if sampled is not None:
             kwargs["sampled"] = sampled
 
-        return sentry_sdk.start_transaction(**kwargs)
+        return sentry_sdk.start_transaction(
+            name=name,
+            op=op,
+            sampled=sampled,
+            custom_sampling_context=custom_sampling_context,
+        )
 
     return sentry_sdk.start_span(
         name=name,
