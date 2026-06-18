64aa1f8a57adfcfaa5a8389b71c9f45f218b7f22
Christinarlong <christina.long@sentry.io>
2026-06-18 11:31:22 -0700

    ref(plugins): remove dead tag derivation plugins
    The `sentry_urls`, `sentry_interface_types`, and `sentry_useragents`
    plugins have been fully replaced by the `TagDeriver` protocol in
    `event_manager_auto_tags.py`. Remove the plugin directories, the
    `TagPlugin` base class, INSTALLED_APPS entries, mypy exclusions,
    tests, and codeowners references.

--- src/sentry/plugins/bases/__init__.py
+++ src/sentry/plugins/bases/__init__.py
@@ -2,4 +2,3 @@ from .issue import IssueTrackingPlugin  # NOQA
 from .issue2 import IssueTrackingPlugin2  # NOQA
 from .notify import NotificationPlugin  # NOQA
 from .releasetracking import ReleaseTrackingPlugin  # NOQA
-from .tag import TagPlugin  # NOQA
