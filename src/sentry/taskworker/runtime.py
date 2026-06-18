bbd23f9d7be9f7355ef4c7dca833aa0d469bf8c5
Mark Story <mark@mark-story.com>
2026-06-18 13:58:06 -0400

    feat(tasks) Add taskbroker-client metrics for tasks
    We want to standardize metrics prefixes and tags for taskworkers. Having
    standardized metrics makes building dashboards and alerting easier for
    platform teams, and makes documenting metrics for others simpler.
    
    I've put usage of the new metrics backend behind an env var so that
    I can incrementally enable and validate the new metrics.
    
    Refs STREAM-1174

--- src/sentry/taskworker/runtime.py
+++ src/sentry/taskworker/runtime.py
@@ -1,6 +1,9 @@
+import os
+
 from django.conf import settings
 from django.core.cache import cache
 from taskbroker_client.app import TaskbrokerApp
+from taskbroker_client.metrics import DatadogMetrics
 
 from sentry.taskworker.adapters import (
     DjangoCacheAtMostOnceStore,
@@ -10,10 +13,23 @@ from sentry.taskworker.adapters import (
     make_producer,
 )
 
+metrics_class = SentryMetricsBackend()
+if os.getenv("USE_TASKWORKER_METRICS", None):
+    # Metrics created by this interface will not
+    # have `sentry.` prefix, and will not have
+    # K8S_LABEL applied.
+    metrics_class = DatadogMetrics(
+        application="sentry",
+        statsd_host=os.getenv("HOST_IP", "127.0.0.1"),
+        statsd_port=os.getenv("SENTRY_STATSD_PORT", 8126),
+        sample_rate=settings.SENTRY_METRICS_SAMPLE_RATE,
+        enable_prefixed_metrics=True,
+    )
+
 app = TaskbrokerApp(
     name="sentry",
     producer_factory=make_producer,
-    metrics_class=SentryMetricsBackend(),
+    metrics_class=metrics_class,
     router_class=SentryRouter(),
     at_most_once_store=DjangoCacheAtMostOnceStore(cache),
     context_hooks=[ViewerContextHook()],
