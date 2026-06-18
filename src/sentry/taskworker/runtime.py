6a04c4e8d1ee2d0775b0875b7e8c69749fb40217
Mark Story <mark@mark-story.com>
2026-06-18 16:12:49 -0400

    Address feedback and infer metrics config from settings

--- src/sentry/taskworker/runtime.py
+++ src/sentry/taskworker/runtime.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from django.conf import settings
@@ -13,18 +14,41 @@ from sentry.taskworker.adapters import (
     make_producer,
 )
 
+logger = logging.getLogger(__name__)
+
+
+def _extract_metrics_config() -> tuple[str | None, int | None]:
+    host, port = None, None
+    metric_options = settings.SENTRY_METRICS_OPTIONS
+    try:
+        # Use the metrics settings options to infer the host/port.
+        # The metrics options have different structures depending on which backend is used.
+        if settings.SENTRY_METRICS_BACKEND == "sentry.metrics.dualwrite.DualWriteMetricsBackend":
+            metric_options = settings.SENTRY_METRICS_OPTIONS["primary_backend_args"]
+
+        # Some backends use `host` and others use `statsd_host`
+        host = metric_options.get("host", None) or metric_options.get("statsd_host", None)
+        port = metric_options.get("port", None) or metric_options.get("statsd_port", None)
+    except Exception as e:
+        logger.warning("Could not extract metrics settings", extra={"error": str(e)})
+    return host, port
+
+
 metrics_class = SentryMetricsBackend()
-if os.getenv("USE_TASKWORKER_METRICS", None):
-    # Metrics created by this interface will not
-    # have `sentry.` prefix, and will not have
-    # K8S_LABEL applied.
-    metrics_class = DatadogMetrics(
-        application="sentry",
-        statsd_host=os.getenv("HOST_IP", "127.0.0.1"),
-        statsd_port=os.getenv("SENTRY_STATSD_PORT", 8126),
-        sample_rate=settings.SENTRY_METRICS_SAMPLE_RATE,
-        enable_prefixed_metrics=True,
-    )
+
+if os.getenv("USE_TASKWORKER_METRICS", None) == "1":
+    host, port = _extract_metrics_config()
+    if host and port:
+        # Metrics created by this interface will not
+        # have `sentry.` prefix, and will not have
+        # K8S_LABEL applied.
+        metrics_class = DatadogMetrics(
+            application="sentry",
+            statsd_host=host,
+            statsd_port=port,
+            sample_rate=settings.SENTRY_METRICS_SAMPLE_RATE,
+            enable_prefixed_metrics=True,
+        )
 
 app = TaskbrokerApp(
     name="sentry",
