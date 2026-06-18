56b556fbffe8466b6d2119fd4ec9ac022c4051bc
Mark Story <mark@mark-story.com>
2026-06-18 16:29:04 -0400

    Fix int cast

--- src/sentry/taskworker/runtime.py
+++ src/sentry/taskworker/runtime.py
@@ -29,6 +29,7 @@ def _extract_metrics_config() -> tuple[str | None, int | None]:
         # Some backends use `host` and others use `statsd_host`
         host = metric_options.get("statsd_host", None) or metric_options.get("host", None)
         port = metric_options.get("statsd_port", None) or metric_options.get("port", None)
+        port = int(port)
     except Exception as e:
         logger.warning("Could not extract metrics settings", extra={"error": str(e)})
     return host, port
