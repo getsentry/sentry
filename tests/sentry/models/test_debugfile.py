968cd964108abbf2b073f2b0d5355e326276e0af
lcian <17258265+lcian@users.noreply.github.com>
2026-06-19 11:25:07 +0200

    ruff

--- tests/sentry/models/test_debugfile.py
+++ tests/sentry/models/test_debugfile.py
@@ -321,7 +321,6 @@ class DebugFileObjectstoreTest(TestCase):
             self._get_session().get(storage_path)
 
 
-
 class CreateDebugFileTest(APITestCase):
     @property
     def file_path(self):
