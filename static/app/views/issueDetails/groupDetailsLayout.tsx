849ab25a7519df069942bec2ca2b186f8508c377
Malachi Willey <malwilley@gmail.com>
2026-06-23 14:13:03 -0700

    Move the sample error check down to where the group is available

--- static/app/views/issueDetails/groupDetailsLayout.tsx
+++ static/app/views/issueDetails/groupDetailsLayout.tsx
@@ -9,6 +9,7 @@ import type {Group} from 'sentry/types/group';
 import type {Project} from 'sentry/types/project';
 import {DemoTourStep, SharedTourElement} from 'sentry/utils/demoMode/demoTours';
 import {getConfigForIssueType} from 'sentry/utils/issueTypeConfig';
+import {useOrganization} from 'sentry/utils/useOrganization';
 import {
   IssueDetailsContextProvider,
   useIssueDetails,
@@ -20,8 +21,10 @@ import {
   IssueDetailsTour,
   IssueDetailsTourContext,
 } from 'sentry/views/issueDetails/issueDetailsTour';
+import {SampleEventAlert} from 'sentry/views/issueDetails/sampleEventAlert';
 import {IssueDetailsSidebar} from 'sentry/views/issueDetails/sidebar/sidebar';
 import {ToggleSidebar} from 'sentry/views/issueDetails/sidebar/toggleSidebar';
+import {useIsSampleEvent} from 'sentry/views/issueDetails/utils';
 import {
   getGroupReprocessingStatus,
   ReprocessingStatus,
@@ -55,9 +58,14 @@ export function GroupDetailsLayout({
   const groupReprocessingStatus = getGroupReprocessingStatus(group);
   const hasPageFrameFeature = useHasPageFrameFeature();
   const theme = useTheme();
+  const organization = useOrganization();
+  const isSampleError = useIsSampleEvent();
 
   return (
     <IssueDetailsContextProvider>
+      {isSampleError && (
+        <SampleEventAlert project={group.project} organization={organization} />
+      )}
       <Container
         display="contents"
         style={
