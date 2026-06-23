b9f36af7a52c44024a7dbc38e3139c7346654787
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-06-23 10:49:29 -0700

    pull in the real matchesToRoutes for tests

--- static/app/utils/__mocks__/recreateRoute.tsx
+++ static/app/utils/__mocks__/recreateRoute.tsx
@@ -1,1 +1,5 @@
+const actual = jest.requireActual('sentry/utils/recreateRoute');
+
+export const matchesToRoutes = actual.matchesToRoutes;
+
 export const recreateRoute = jest.fn((name: any) => name);
