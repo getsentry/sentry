38b63570c7ee716c3f85b56b69a5f2792beaf785
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-06-24 10:42:26 -0700

    cleanup types

--- static/app/components/core/trackingContext.tsx
+++ static/app/components/core/trackingContext.tsx
@@ -2,10 +2,10 @@ import {createContext, useContext} from 'react';
 
 import type {DO_NOT_USE_ButtonProps as ButtonProps} from './button/types';
 
-const TrackingContext = createContext<(props: ButtonProps) => void>(() => {});
+const TrackingContext = createContext<() => (props: ButtonProps) => void>(() => () => {});
 
 export const TrackingContextProvider = TrackingContext.Provider;
 
 export const useButtonTracking = () => {
-  return useContext(TrackingContext);
+  return useContext(TrackingContext)();
 };
--- static/app/scrapsProviders/tracking.tsx
+++ static/app/scrapsProviders/tracking.tsx
@@ -3,26 +3,30 @@ import {TrackingContextProvider} from '@sentry/scraps/trackingContext';
 
 import {getOverride} from 'sentry/overrideRegistry';
 
-function defaultButtonTracking(props: ButtonProps) {
-  const hasAnalyticsDebug = window.localStorage?.getItem('DEBUG_ANALYTICS') === '1';
-  const hasCustomAnalytics =
-    props.analyticsEventName || props.analyticsEventKey || props.analyticsParams;
-  if (hasCustomAnalytics && hasAnalyticsDebug) {
+function useDefaultButtonTracking() {
+  return (props: ButtonProps) => {
+    const hasAnalyticsDebug = window.localStorage?.getItem('DEBUG_ANALYTICS') === '1';
+    const hasCustomAnalytics =
+      props.analyticsEventName || props.analyticsEventKey || props.analyticsParams;
     // eslint-disable-next-line no-console
-    console.log('buttonAnalyticsEvent', {
-      eventKey: props.analyticsEventKey,
-      eventName: props.analyticsEventName,
-      variant: props.variant,
-      href: 'href' in props ? props.href : undefined,
-      ...props.analyticsParams,
-    });
-  }
+    console.log('hasCustomAnalytics', hasCustomAnalytics);
+    if (hasCustomAnalytics && hasAnalyticsDebug) {
+      // eslint-disable-next-line no-console
+      console.log('buttonAnalyticsEvent', {
+        eventKey: props.analyticsEventKey,
+        eventName: props.analyticsEventName,
+        variant: props.variant,
+        href: 'href' in props ? props.href : undefined,
+        ...props.analyticsParams,
+      });
+    }
+  };
 }
 
 export function SentryTrackingProvider({children}: {children: React.ReactNode}) {
   return (
     <TrackingContextProvider
-      value={getOverride('react-hook:use-button-tracking') ?? defaultButtonTracking}
+      value={getOverride('react-hook:use-button-tracking') ?? useDefaultButtonTracking}
     >
       {children}
     </TrackingContextProvider>
