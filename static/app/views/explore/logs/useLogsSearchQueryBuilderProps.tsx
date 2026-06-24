b1f85ca454cdbf8add425a331a8771540c1a7204
nsdeschenes <nicholas.deschenes@sentry.io>
2026-06-24 10:59:12 -0300

    feat(logs): validate logs search builder fields

--- static/app/views/explore/logs/useLogsSearchQueryBuilderProps.tsx
+++ static/app/views/explore/logs/useLogsSearchQueryBuilderProps.tsx
@@ -2,8 +2,10 @@ import {useCallback, useMemo} from 'react';
 
 import {useCaseInsensitivity} from 'sentry/components/searchQueryBuilder/hooks';
 import type {TagCollection} from 'sentry/types/group';
+import {FieldKind} from 'sentry/utils/fields';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {usePrevious} from 'sentry/utils/usePrevious';
+import {prettifyAttributeName} from 'sentry/views/explore/components/traceItemAttributes/utils';
 import {
   useTraceItemSearchQueryBuilderProps,
   type TraceItemSearchQueryBuilderProps,
@@ -16,6 +18,7 @@ import {
 } from 'sentry/views/explore/queryParams/context';
 import {TraceItemDataset} from 'sentry/views/explore/types';
 import {findSuggestedColumns} from 'sentry/views/explore/utils';
+import type {EventValidationData} from 'sentry/views/explore/utils/validateEventParamsOptions';
 
 export function useLogsSearchQueryBuilderProps({
   attributeQuery,
@@ -25,6 +28,7 @@ export function useLogsSearchQueryBuilderProps({
   stringAttributes,
   numberSecondaryAliases,
   stringSecondaryAliases,
+  validatedSearchQueryData,
 }: {
   booleanAttributes: TagCollection;
   booleanSecondaryAliases: TagCollection;
@@ -33,6 +37,7 @@ export function useLogsSearchQueryBuilderProps({
   stringAttributes: TagCollection;
   stringSecondaryAliases: TagCollection;
   attributeQuery?: string;
+  validatedSearchQueryData?: EventValidationData;
 }) {
   const logsSearch = useQueryParamsSearch();
   const oldLogsSearch = usePrevious(logsSearch);
@@ -40,13 +45,73 @@ export function useLogsSearchQueryBuilderProps({
   const setQueryParams = useSetQueryParams();
   const [caseInsensitive, setCaseInsensitive] = useCaseInsensitivity();
 
+  const {
+    validatedBooleanAttributes,
+    validatedNumberAttributes,
+    validatedStringAttributes,
+    invalidFilterKeys,
+  } = useMemo(() => {
+    const localInvalidFilterKeys: string[] = [];
+    const localBooleanAttributes = {...booleanAttributes};
+    const localNumberAttributes = {...numberAttributes};
+    const localStringAttributes = {...stringAttributes};
+
+    if (validatedSearchQueryData?.query.fields.length) {
+      for (const item of validatedSearchQueryData.query.fields) {
+        if (item.valid) {
+          if (item.attrType === 'boolean' && item.name) {
+            localBooleanAttributes[item.name] ??= {
+              key: item.name,
+              name: prettifyAttributeName(item.name),
+              kind: FieldKind.BOOLEAN,
+            };
+          }
+
+          if (item.attrType === 'number' && item.name) {
+            localNumberAttributes[item.name] ??= {
+              key: item.name,
+              name: prettifyAttributeName(item.name),
+              kind: FieldKind.MEASUREMENT,
+            };
+          }
+
+          if (item.attrType === 'string' && item.name) {
+            localStringAttributes[item.name] ??= {
+              key: item.name,
+              name: prettifyAttributeName(item.name),
+              kind: FieldKind.TAG,
+            };
+          }
+
+          continue;
+        }
+
+        if (item.name) {
+          localInvalidFilterKeys.push(item.name);
+        }
+      }
+    }
+
+    return {
+      validatedBooleanAttributes: localBooleanAttributes,
+      validatedNumberAttributes: localNumberAttributes,
+      validatedStringAttributes: localStringAttributes,
+      invalidFilterKeys: localInvalidFilterKeys,
+    };
+  }, [
+    booleanAttributes,
+    numberAttributes,
+    stringAttributes,
+    validatedSearchQueryData?.query.fields,
+  ]);
+
   const onSearch = useCallback(
     (newQuery: string) => {
       const newSearch = new MutableSearch(newQuery);
       const suggestedColumns = findSuggestedColumns(newSearch, oldLogsSearch, {
-        numberAttributes,
-        stringAttributes,
-        booleanAttributes,
+        numberAttributes: validatedNumberAttributes,
+        stringAttributes: validatedStringAttributes,
+        booleanAttributes: validatedBooleanAttributes,
       });
 
       const existingFields = new Set(fields);
@@ -58,12 +123,12 @@ export function useLogsSearchQueryBuilderProps({
       });
     },
     [
-      booleanAttributes,
       fields,
-      numberAttributes,
       oldLogsSearch,
       setQueryParams,
-      stringAttributes,
+      validatedBooleanAttributes,
+      validatedNumberAttributes,
+      validatedStringAttributes,
     ]
   );
 
@@ -73,9 +138,9 @@ export function useLogsSearchQueryBuilderProps({
       initialQuery,
       searchSource: 'ourlogs',
       onSearch,
-      booleanAttributes,
-      numberAttributes,
-      stringAttributes,
+      booleanAttributes: validatedBooleanAttributes,
+      numberAttributes: validatedNumberAttributes,
+      stringAttributes: validatedStringAttributes,
       itemType: TraceItemDataset.LOGS as TraceItemDataset.LOGS,
       booleanSecondaryAliases,
       numberSecondaryAliases,
@@ -86,19 +151,21 @@ export function useLogsSearchQueryBuilderProps({
       matchKeySuggestions: [{key: 'trace', valuePattern: /^[0-9a-fA-F]{32}$/}],
       hiddenAttributeKeys: HiddenLogSearchFields,
       attributeQuery,
+      invalidFilterKeys,
     }),
     [
       attributeQuery,
-      booleanAttributes,
       booleanSecondaryAliases,
       caseInsensitive,
       initialQuery,
-      numberAttributes,
       numberSecondaryAliases,
       onSearch,
       setCaseInsensitive,
-      stringAttributes,
       stringSecondaryAliases,
+      invalidFilterKeys,
+      validatedBooleanAttributes,
+      validatedNumberAttributes,
+      validatedStringAttributes,
     ]
   );
 
