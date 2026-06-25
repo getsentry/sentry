dbc4e5d75c0536e914f2672965fb860268201ae9
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-06-25 20:47:52 +0000

    ref(sessions): migrate ReleasesAdoptionChart to useSessionsRequest hook
    Co-Authored-By: sentry-junior[bot] <264270552+sentry-junior[bot]@users.noreply.github.com>

--- static/app/views/explore/releases/list/releasesAdoptionChart.tsx
+++ static/app/views/explore/releases/list/releasesAdoptionChart.tsx
@@ -11,7 +11,7 @@ import {Flex} from '@sentry/scraps/layout';
 
 import ChartZoom from 'sentry/components/charts/chartZoom';
 import {LineChart} from 'sentry/components/charts/lineChart';
-import {SessionsRequest} from 'sentry/components/charts/sessionsRequest';
+import {useSessionsRequest} from 'sentry/components/charts/useSessionsRequest';
 import {
   HeaderTitleLegend,
   InlineContainer,
@@ -153,154 +153,152 @@ export function ReleasesAdoptionChart({
 
   const {start, end, period, utc} = selection.datetime;
   const field = sessionDisplayToField(activeDisplay);
+  const {data: response, isRefetching} = useSessionsRequest({
+    interval,
+    groupBy: ['release'],
+    field: [field],
+    ...normalizeDateTimeParams(pick(location.query, Object.values(URL_PARAM))),
+  });
+
+  const loading = response === null;
+  const reloading = response !== null && isRefetching;
+  const totalCount = getCount(response?.groups, field);
+  const releasesSeries = getReleasesSeries(response);
+
+  if (loading) {
+    return renderEmpty();
+  }
+
+  if (!releasesSeries?.length) {
+    return null;
+  }
+
+  const numDataPoints = releasesSeries[0]!.data.length;
+  const xAxisData = releasesSeries[0]!.data.map(point => point.name);
+  const hideLastPoint = !releasesSeries.some(
+    series => series.data[numDataPoints - 1]!.value > 0
+  );
 
   return (
-    <SessionsRequest
-      interval={interval}
-      groupBy={['release']}
-      field={[field]}
-      {...normalizeDateTimeParams(pick(location.query, Object.values(URL_PARAM)))}
-    >
-      {({response, loading, reloading}) => {
-        const totalCount = getCount(response?.groups, field);
-        const releasesSeries = getReleasesSeries(response);
-        if (loading) {
-          return renderEmpty();
-        }
+    <Panel>
+      <PanelBody withPadding>
+        <ChartHeader>
+          <Flex as="header">{t('Release Adoption')}</Flex>
+        </ChartHeader>
+        <TransitionChart loading={loading} reloading={reloading}>
+          <TransparentLoadingMask visible={reloading} />
+          <ChartZoom period={period} utc={utc} start={start} end={end}>
+            {zoomRenderProps => (
+              <LineChart
+                {...zoomRenderProps}
+                grid={{left: '10px', right: '10px', top: '40px', bottom: '0px'}}
+                series={releasesSeries.map(series => ({
+                  ...series,
+                  data: hideLastPoint ? series.data.slice(0, -1) : series.data,
+                }))}
+                yAxis={{
+                  min: 0,
+                  max: 100,
+                  type: 'value',
+                  interval: 10,
+                  splitNumber: 10,
+                  axisLabel: {
+                    formatter: '{value}%',
+                  },
+                }}
+                xAxis={{
+                  show: true,
+                  min: xAxisData[0],
+                  max: xAxisData[numDataPoints - 1],
+                  type: 'time',
+                }}
+                tooltip={{
+                  formatter: seriesParams => {
+                    const series = Array.isArray(seriesParams)
+                      ? seriesParams
+                      : [seriesParams];
+                    // @ts-expect-error TS(2532): Object is possibly 'undefined'.
+                    const timestamp = series[0].data[0];
+                    const [first, second, third, ...rest] = series
+                      // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+                      .filter(s => s.data[1] > 0)
+                      // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+                      .sort((a, b) => b.data[1] - a.data[1]);
 
-        if (!releasesSeries?.length) {
-          return null;
-        }
+                    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+                    const restSum = rest.reduce((acc, s) => acc + s.data[1], 0);
 
-        const numDataPoints = releasesSeries[0]!.data.length;
-        const xAxisData = releasesSeries[0]!.data.map(point => point.name);
-        const hideLastPoint = !releasesSeries.some(
-          series => series.data[numDataPoints - 1]!.value > 0
-        );
+                    const seriesToRender = compact([first, second, third]);
 
-        return (
-          <Panel>
-            <PanelBody withPadding>
-              <ChartHeader>
-                <Flex as="header">{t('Release Adoption')}</Flex>
-              </ChartHeader>
-              <TransitionChart loading={loading} reloading={reloading}>
-                <TransparentLoadingMask visible={reloading} />
-                <ChartZoom period={period} utc={utc} start={start} end={end}>
-                  {zoomRenderProps => (
-                    <LineChart
-                      {...zoomRenderProps}
-                      grid={{left: '10px', right: '10px', top: '40px', bottom: '0px'}}
-                      series={releasesSeries.map(series => ({
-                        ...series,
-                        data: hideLastPoint ? series.data.slice(0, -1) : series.data,
-                      }))}
-                      yAxis={{
-                        min: 0,
-                        max: 100,
-                        type: 'value',
-                        interval: 10,
-                        splitNumber: 10,
-                        axisLabel: {
-                          formatter: '{value}%',
-                        },
-                      }}
-                      xAxis={{
-                        show: true,
-                        min: xAxisData[0],
-                        max: xAxisData[numDataPoints - 1],
-                        type: 'time',
-                      }}
-                      tooltip={{
-                        formatter: seriesParams => {
-                          const series = Array.isArray(seriesParams)
-                            ? seriesParams
-                            : [seriesParams];
-                          // @ts-expect-error TS(2532): Object is possibly 'undefined'.
-                          const timestamp = series[0].data[0];
-                          const [first, second, third, ...rest] = series
-                            // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-                            .filter(s => s.data[1] > 0)
-                            // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-                            .sort((a, b) => b.data[1] - a.data[1]);
+                    if (rest.length) {
+                      // @ts-expect-error TS(2345): Argument of type '{ seriesName: string; data: any[... Remove this comment to see the full error message
+                      seriesToRender.push({
+                        seriesName: tn('%s Other', '%s Others', rest.length),
+                        data: [timestamp, restSum],
+                        marker:
+                          '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;"></span>',
+                      });
+                    }
 
-                          // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-                          const restSum = rest.reduce((acc, s) => acc + s.data[1], 0);
+                    if (!seriesToRender.length) {
+                      return '<div/>';
+                    }
 
-                          const seriesToRender = compact([first, second, third]);
+                    const periodObj = parseStatsPeriod(interval) || {
+                      periodLength: 'd',
+                      period: '1',
+                    };
+                    const intervalStart = moment(timestamp).format('MMM D LT');
+                    const intervalEnd = (
+                      series[0]?.dataIndex === numDataPoints - 1
+                        ? moment(response?.end)
+                        : moment(timestamp).add(
+                            parseInt(periodObj.period!, 10),
+                            periodObj.periodLength
+                          )
+                    ).format('MMM D LT');
 
-                          if (rest.length) {
-                            // @ts-expect-error TS(2345): Argument of type '{ seriesName: string; data: any[... Remove this comment to see the full error message
-                            seriesToRender.push({
-                              seriesName: tn('%s Other', '%s Others', rest.length),
-                              data: [timestamp, restSum],
-                              marker:
-                                '<span style="display:inline-block;margin-right:5px;border-radius:10px;width:10px;height:10px;"></span>',
-                            });
-                          }
-
-                          if (!seriesToRender.length) {
-                            return '<div/>';
-                          }
-
-                          const periodObj = parseStatsPeriod(interval) || {
-                            periodLength: 'd',
-                            period: '1',
-                          };
-                          const intervalStart = moment(timestamp).format('MMM D LT');
-                          const intervalEnd = (
-                            series[0]?.dataIndex === numDataPoints - 1
-                              ? moment(response?.end)
-                              : moment(timestamp).add(
-                                  parseInt(periodObj.period!, 10),
-                                  periodObj.periodLength
-                                )
-                          ).format('MMM D LT');
-
-                          return [
-                            '<div class="tooltip-series">',
-                            seriesToRender
-                              .map(
-                                s =>
-                                  `<div><span class="tooltip-label">${
-                                    s.marker as string
-                                  }<strong>${
-                                    s.seriesName && truncationFormatter(s.seriesName, 32)
-                                    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-                                  }</strong></span>${s.data[1].toFixed(2)}%</div>`
-                              )
-                              .join(''),
-                            '</div>',
-                            `<div class="tooltip-footer">${intervalStart} &mdash; ${intervalEnd}</div>`,
-                            '<div class="tooltip-arrow"></div>',
-                          ].join('');
-                        },
-                      }}
-                      onClick={handleClick}
-                    />
-                  )}
-                </ChartZoom>
-              </TransitionChart>
-            </PanelBody>
-            <ChartFooter>
-              <InlineContainer>
-                <SectionHeading>
-                  {tct('Total [display]', {
-                    display:
-                      activeDisplay === ReleasesDisplayOption.USERS
-                        ? 'Users'
-                        : 'Sessions',
-                  })}
-                </SectionHeading>
-                <SectionValue>
-                  <Count value={totalCount || 0} />
-                </SectionValue>
-              </InlineContainer>
-            </ChartFooter>
-          </Panel>
-        );
-      }}
-    </SessionsRequest>
+                    return [
+                      '<div class="tooltip-series">',
+                      seriesToRender
+                        .map(
+                          s =>
+                            `<div><span class="tooltip-label">${
+                              s.marker as string
+                            }<strong>${
+                              s.seriesName && truncationFormatter(s.seriesName, 32)
+                              // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+                            }</strong></span>${s.data[1].toFixed(2)}%</div>`
+                        )
+                        .join(''),
+                      '</div>',
+                      `<div class="tooltip-footer">${intervalStart} &mdash; ${intervalEnd}</div>`,
+                      '<div class="tooltip-arrow"></div>',
+                    ].join('');
+                  },
+                }}
+                onClick={handleClick}
+              />
+            )}
+          </ChartZoom>
+        </TransitionChart>
+      </PanelBody>
+      <ChartFooter>
+        <InlineContainer>
+          <SectionHeading>
+            {tct('Total [display]', {
+              display:
+                activeDisplay === ReleasesDisplayOption.USERS
+                  ? 'Users'
+                  : 'Sessions',
+            })}
+          </SectionHeading>
+          <SectionValue>
+            <Count value={totalCount || 0} />
+          </SectionValue>
+        </InlineContainer>
+      </ChartFooter>
+    </Panel>
   );
 }
 
