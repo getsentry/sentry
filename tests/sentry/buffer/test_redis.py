8f1678c91f6cf2465383b9b3c581ca93727f55d8
Colton Allen <colton.allen@sentry.io>
2026-06-25 09:43:58 -0500

    Fix tests

--- tests/sentry/buffer/test_redis.py
+++ tests/sentry/buffer/test_redis.py
@@ -78,9 +78,9 @@ class TestRedisBuffer:
     def test_process_does_bubble_up_json(self, process) -> None:
         client = get_cluster_routing_client(self.buf.cluster, self.buf.is_redis_cluster)
 
-        client.hmset(
+        client.hset(
             "foo",
-            {
+            mapping={
                 "e+foo": '["s","bar"]',
                 "e+datetime": '["dt","1493791566.000000"]',
                 "f": '{"pk": ["i","1"]}',
@@ -103,9 +103,9 @@ class TestRedisBuffer:
     def test_process_does_bubble_up_pickle(self, process) -> None:
         client = get_cluster_routing_client(self.buf.cluster, self.buf.is_redis_cluster)
 
-        client.hmset(
+        client.hset(
             "foo",
-            {
+            mapping={
                 "e+foo": "S'bar'\np1\n.",
                 "f": "(dp1\nS'pk'\np2\nI1\ns.",
                 "i+times_seen": "2",
@@ -206,9 +206,9 @@ class TestRedisBuffer:
     def test_process_uses_signal_only(self, process) -> None:
         client = get_cluster_routing_client(self.buf.cluster, self.buf.is_redis_cluster)
 
-        client.hmset(
+        client.hset(
             "foo",
-            {
+            mapping={
                 "f": '{"pk": ["i","1"]}',
                 "i+times_seen": "1",
                 "m": "unittest.mock.Mock",
