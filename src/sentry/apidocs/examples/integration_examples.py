e1dc365f88a3da3a900013e6e8902a8605114b76
Leander Rodrigues <me@leander.xyz>
2026-07-06 13:07:03 -0400

    ✨ also remove this alert rule action stuff from slack/pd

--- src/sentry/apidocs/examples/integration_examples.py
+++ src/sentry/apidocs/examples/integration_examples.py
@@ -34,14 +34,7 @@ class IntegrationExamples:
                         "canAdd": True,
                         "canDisable": False,
                         "features": ["alert-rule", "chat-unfurl"],
-                        "aspects": {
-                            "alerts": [
-                                {
-                                    "type": "info",
-                                    "text": "The Slack integration adds a new Alert Rule action to all projects. To enable automatic notifications sent to Slack you must create a rule using the slack workspace action in your project settings.",
-                                }
-                            ]
-                        },
+                        "aspects": {},
                     },
                     "configOrganization": [],
                     "configData": {"installationType": "born_as_bot"},
@@ -87,14 +80,7 @@ class IntegrationExamples:
                     "canAdd": True,
                     "canDisable": False,
                     "features": ["alert-rule", "chat-unfurl"],
-                    "aspects": {
-                        "alerts": [
-                            {
-                                "type": "info",
-                                "text": "The Slack integration adds a new Alert Rule action to all projects. To enable automatic notifications sent to Slack you must create a rule using the slack workspace action in your project settings.",
-                            }
-                        ]
-                    },
+                    "aspects": {},
                 },
                 "configOrganization": [],
                 "configData": {"installationType": "born_as_bot"},
@@ -550,14 +536,7 @@ class IntegrationExamples:
                             "noun": "Installation",
                             "issue_url": "https://github.com/getsentry/sentry/issues/new?assignees=&labels=Component:%20Integrations&template=bug.yml&title=PagerDuty%20Integration%20Problem",
                             "source_url": "https://github.com/getsentry/sentry/tree/master/src/sentry/integrations/pagerduty",
-                            "aspects": {
-                                "alerts": [
-                                    {
-                                        "type": "info",
-                                        "text": "The PagerDuty integration adds a new Alert Rule action to all projects. To enable automatic notifications sent to PagerDuty you must create a rule using the PagerDuty action in your project settings.",
-                                    }
-                                ]
-                            },
+                            "aspects": {},
                         },
                         "canAdd": True,
                         "canDisable": False,
@@ -583,14 +562,7 @@ class IntegrationExamples:
                             "noun": "Workspace",
                             "issue_url": "https://github.com/getsentry/sentry/issues/new?assignees=&labels=Component:%20Integrations&template=bug.yml&title=Slack%20Integration%20Problem",
                             "source_url": "https://github.com/getsentry/sentry/tree/master/src/sentry/integrations/slack",
-                            "aspects": {
-                                "alerts": [
-                                    {
-                                        "type": "info",
-                                        "text": "The Slack integration adds a new Alert Rule action to all projects. To enable automatic notifications sent to Slack you must create a rule using the slack workspace action in your project settings.",
-                                    }
-                                ]
-                            },
+                            "aspects": {},
                         },
                         "canAdd": True,
                         "canDisable": False,
