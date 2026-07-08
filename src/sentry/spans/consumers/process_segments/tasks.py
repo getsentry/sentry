f9215111c5eaa0de6fcdfd9af719831faa88622e
Ben McKerry <110857332+bmckerry@users.noreply.github.com>
2026-07-08 14:09:15 -0400

    ref(producers): completely replace TaskProducer with FutureTrackingProducer

--- src/sentry/spans/consumers/process_segments/tasks.py
+++ src/sentry/spans/consumers/process_segments/tasks.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
-from functools import partial
-
 from arroyo import Topic as ArroyoTopic
-from arroyo.backends.kafka import KafkaProducer
+from arroyo.backends.kafka import FutureTrackingProducer, KafkaProducer
 from redis.exceptions import TimeoutError as RedisTimeoutError
 from taskbroker_client.constants import CompressionType
 from taskbroker_client.retry import Retry
@@ -14,24 +12,20 @@ from sentry.silo.base import SiloMode
 from sentry.spans.consumers.process_segments.factory import _process_segment_bytes
 from sentry.tasks.base import instrumented_task
 from sentry.taskworker.namespaces import spans_process_segments_tasks
-from sentry.taskworker.producer import get_task_producer
 from sentry.utils.arroyo_producer import get_arroyo_producer
 from sentry.utils.kafka_config import get_topic_definition
 
 
-def _get_snuba_items_producer(
-    name: str = "sentry.spans.process_segments.snuba_items",
-) -> KafkaProducer:
+def _get_snuba_items_producer() -> KafkaProducer:
     return get_arroyo_producer(
-        name,
+        "sentry.spans.process_segments.snuba_items",
         Topic.SNUBA_ITEMS,
     )
 
 
-_snuba_items_task_producer_name = "sentry.spans.process_segments.snuba_items_taskproducer"
-_snuba_items_task_producer = get_task_producer(
-    producer_name=_snuba_items_task_producer_name,
-    producer_factory=partial(_get_snuba_items_producer, name=_snuba_items_task_producer_name),
+_snuba_items_producer = FutureTrackingProducer(
+    name="sentry.spans.process_segments.snuba_items",
+    producer_factory=_get_snuba_items_producer,
 )
 _snuba_items_topic = ArroyoTopic(get_topic_definition(Topic.SNUBA_ITEMS)["real_topic_name"])
 
@@ -46,4 +40,4 @@ _snuba_items_topic = ArroyoTopic(get_topic_definition(Topic.SNUBA_ITEMS)["real_t
 )
 def process_segment_task(segment_bytes: bytes) -> None:
     for payload in _process_segment_bytes(segment_bytes):
-        _snuba_items_task_producer.produce(_snuba_items_topic, payload)
+        _snuba_items_producer.produce(_snuba_items_topic, payload)
