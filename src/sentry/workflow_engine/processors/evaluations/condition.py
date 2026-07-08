fad9afeb11dfddcd9b4bec86e04f7b1a3eeab463
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-07-07 21:36:12 -0700

    Make an ABC for the evaluations, a consistent shape for all the evaluations would be nice.

--- src/sentry/workflow_engine/processors/evaluations/condition.py
+++ src/sentry/workflow_engine/processors/evaluations/condition.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from typing import Any
 
+from sentry.workflow_engine.processors.evaluations.base import BaseWorkflowEngineEvaluation
 from sentry.workflow_engine.types import ConditionError, DataConditionResult
 
 
@@ -9,21 +10,19 @@ class DataConditionEvaluationException(Exception):
 
 
 @dataclass(frozen=True)
-class DataConditionEvaluation:
+class DataConditionEvaluation(BaseWorkflowEngineEvaluation):
     """
     This class is used to track the evaluation of a DataCondition's logic.
 
-    This is generally evaluated by DataCondition.evaluate_value(value), and
-    then used to create the DataConditionGroupEvaluation in `evaluate_data_conditions`.
-
-    Each of these attributes are eventually used to ensure detector/workflow conditions
-    are evaluating as expected.
+    This is created by DataCondition.evaluate_value(value) as a result.
 
     Attributes
     - value: Any - this is the value that was evaluated against.
     - evaluation: bool - this tracks the logical evaluation of the condition
-    - result: DataConditionResult - this is the value that is expected to be the result of the
-              evaluation, in general this is the `DataCondition.condition_result`
+    - result: DataConditionResult - this is the value that is expected to be the result of the evaluation, in general this is the `DataCondition.condition_result`
+
+    TODO
+    - Use this Evaluation to build DataConditionGroupEvaluation
     """
 
     value: Any
