d9ac66dabd52144ed0cb30b9eedf404756cd3dce
Alex Alderman Webb <alexander.webb@sentry.io>
2026-07-09 13:09:50 +0200

    ref(o11y): Adapt gate for setting `used_default_cache` (#119286)
    Make the check compatible with the streaming trace lifecycle.

--- src/sentry/cache/base.py
+++ src/sentry/cache/base.py
@@ -4,6 +4,8 @@ from typing import Any
 import sentry_sdk
 from django.conf import settings
 
+from sentry.utils.tracing import get_current_span
+
 
 def wrap_key(prefix: str, version: Any, key: Any) -> str:
     return f"{prefix}:{version}:{key}"
@@ -46,10 +48,9 @@ class BaseCache(local):
         if not self.is_default_cache:
             return
 
-        scope = sentry_sdk.get_current_scope()
         # Do not set this tag if we're in the global scope (which roughly
         # equates to having a transaction).
-        if scope.transaction:
+        if get_current_span() is not None:
             sentry_sdk.set_tag(f"{op}_default_cache", "true")
             sentry_sdk.set_attribute(f"{op}_default_cache", "true")
             sentry_sdk.set_tag("used_default_cache", "true")
