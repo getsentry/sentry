a6722720db03c0054659700477316c7396262484
srest2021 <sofia.rest@sentry.io>
2026-07-13 11:32:00 -0700

    fix tests

--- tests/sentry/integrations/github/test_webhook.py
+++ tests/sentry/integrations/github/test_webhook.py
@@ -1679,7 +1679,11 @@ class PullRequestEventWebhookTest(APITestCase):
                 organization=self.organization,
                 external_id="12345",
                 provider="github",
-                metadata={"access_token": "1234", "expires_at": future_expires.isoformat()},
+                metadata={
+                    "access_token": "1234",
+                    "expires_at": future_expires.isoformat(),
+                    "domain_name": "github.com/example",
+                },
             )
             integration.add_organization(self.project.organization.id, self.user)
 
@@ -1726,7 +1730,11 @@ class PullRequestEventWebhookTest(APITestCase):
                 organization=self.organization,
                 external_id="12345",
                 provider="github",
-                metadata={"access_token": "1234", "expires_at": future_expires.isoformat()},
+                metadata={
+                    "access_token": "1234",
+                    "expires_at": future_expires.isoformat(),
+                    "domain_name": "github.com/example",
+                },
             )
             integration.add_organization(self.project.organization.id, self.user)
 
