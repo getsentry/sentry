64c2dbc16ea7b31180a8126550a7e22074de7980
Kyle Consalus <kyle.consalus@sentry.io>
2026-07-13 11:05:13 -0700

    type

--- tests/sentry/issues/action_log/test_types.py
+++ tests/sentry/issues/action_log/test_types.py
@@ -3,7 +3,7 @@ from sentry.testutils.cases import TestCase
 
 
 class GroupActionRegistrationTest(TestCase):
-    def test_all_types_are_registered(self):
+    def test_all_types_are_registered(self) -> None:
         missing = [member for member in GroupActionType if GroupAction.by_type(member) is None]
         assert missing == [], (
             f"GroupActionType members without a registered GroupAction subclass: "
