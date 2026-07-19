8c700defc1c31ca87f707caf7e8bb8022fdbddb5
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-07-19 19:55:27 +0000

    fix(coding-conventions): Ensure stable component identity for InlineEventAttachment

--- static/app/views/issueDetails/groupEventAttachments/inlineEventAttachment.tsx
+++ static/app/views/issueDetails/groupEventAttachments/inlineEventAttachment.tsx
@@ -1,6 +1,15 @@
 import styled from '@emotion/styled';
 
-import {getInlineAttachmentRenderer} from 'sentry/components/events/attachmentViewers/previewAttachmentTypes';
+import {ImageViewer} from 'sentry/components/events/attachmentViewers/imageViewer';
+import {JsonViewer} from 'sentry/components/events/attachmentViewers/jsonViewer';
+import {LogFileViewer} from 'sentry/components/events/attachmentViewers/logFileViewer';
+import {
+  getInlineAttachmentRenderer,
+  imageMimeTypes,
+  webmMimeTypes,
+} from 'sentry/components/events/attachmentViewers/previewAttachmentTypes';
+import {RRWebJsonViewer} from 'sentry/components/events/attachmentViewers/rrwebJsonViewer';
+import {VideoViewer} from 'sentry/components/events/attachmentViewers/videoViewer';
 import {PanelItem} from 'sentry/components/panels/panelItem';
 import type {Event} from 'sentry/types/event';
 import type {IssueAttachment} from 'sentry/types/group';
@@ -12,21 +21,51 @@ interface InlineAttachmentsProps {
   projectSlug: string;
 }
 
+interface AttachmentViewerProps {
+  attachment: IssueAttachment;
+  eventId: string;
+  orgSlug: string;
+  projectSlug: string;
+}
+
+const logFileMimeTypes = ['text/css', 'text/csv', 'text/html', 'text/javascript', 'text/plain'];
+const jsonMimeTypes = ['application/json', 'application/ld+json', 'text/json', 'text/x-json'];
+
+function AttachmentViewer({attachment, orgSlug, projectSlug, eventId}: AttachmentViewerProps) {
+  const commonProps = {attachment, orgSlug, projectSlug, eventId};
+
+  if (imageMimeTypes.includes(attachment.mimetype)) {
+    return <ImageViewer {...commonProps} />;
+  }
+  if (webmMimeTypes.includes(attachment.mimetype)) {
+    return <VideoViewer {...commonProps} />;
+  }
+  if (logFileMimeTypes.includes(attachment.mimetype)) {
+    return <LogFileViewer {...commonProps} />;
+  }
+  if (jsonMimeTypes.includes(attachment.mimetype)) {
+    if (attachment.name === 'rrweb.json' || attachment.name.startsWith('rrweb-')) {
+      return <RRWebJsonViewer {...commonProps} />;
+    }
+    return <JsonViewer {...commonProps} />;
+  }
+  return null;
+}
+
 export function InlineEventAttachment({
   attachment,
   projectSlug,
   eventId,
 }: InlineAttachmentsProps) {
   const organization = useOrganization();
-  const AttachmentComponent = getInlineAttachmentRenderer(attachment);
 
-  if (!AttachmentComponent) {
+  if (!getInlineAttachmentRenderer(attachment)) {
     return null;
   }
 
   return (
     <AttachmentPreviewWrapper>
-      <AttachmentComponent
+      <AttachmentViewer
         orgSlug={organization.slug}
         projectSlug={projectSlug}
         eventId={eventId}
