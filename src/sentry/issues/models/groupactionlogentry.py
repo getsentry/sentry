0fc5d34954b24779f94c967753fcba0a993df7ba
Kyle Consalus <kyle.consalus@sentry.io>
2026-07-21 10:26:47 -0700

    use BaseManager

--- src/sentry/issues/models/groupactionlogentry.py
+++ src/sentry/issues/models/groupactionlogentry.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import functools
+from typing import ClassVar
 
 from django.db import models
 from django.db.models.functions import Now
@@ -13,10 +14,11 @@ from sentry.db.models import (
     cell_silo_model,
     sane_repr,
 )
+from sentry.db.models.manager.base import BaseManager
 from sentry.issues.action_log.types import GroupAction, GroupActionType, GroupActorType
 
 
-class GroupActionLogEntryManager(models.Manager["GroupActionLogEntry"]):
+class GroupActionLogEntryManager(BaseManager["GroupActionLogEntry"]):
     def user_visible(self) -> models.QuerySet[GroupActionLogEntry]:
         return self.filter(type__in=GroupAction.get_user_visible_types())
 
@@ -33,7 +35,7 @@ class GroupActionLogEntry(Model):
     ``sentry.issues.action_log`` instead.
     """
 
-    objects: GroupActionLogEntryManager = GroupActionLogEntryManager()
+    objects: ClassVar[GroupActionLogEntryManager] = GroupActionLogEntryManager()
 
     __relocation_scope__ = RelocationScope.Excluded
 
