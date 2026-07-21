74481f6b9fde5ff73f4930555c93d532411d2361
Anuj Raja <anujrajaceo@gmail.com>
2026-07-21 12:59:53 -0400

    test: cover deep JVM view hierarchy round trip

--- tests/sentry/lang/java/test_view_hierarchies.py
+++ tests/sentry/lang/java/test_view_hierarchies.py
@@ -1,5 +1,8 @@
 from typing import Any
 
+import orjson
+import pytest
+
 from sentry.lang.java.view_hierarchies import _serialize_view_hierarchy
 from sentry.utils import json
 
@@ -13,4 +16,10 @@ def test_serialize_deep_view_hierarchy() -> None:
         window["children"] = [child]
         window = child
 
-    assert json.loads(_serialize_view_hierarchy(view_hierarchy)) == view_hierarchy
+    raw_attachment = json.dumps(view_hierarchy).encode()
+    parsed_view_hierarchy = orjson.loads(raw_attachment)
+
+    with pytest.raises(orjson.JSONEncodeError):
+        orjson.dumps(parsed_view_hierarchy)
+
+    assert json.loads(_serialize_view_hierarchy(parsed_view_hierarchy)) == view_hierarchy
