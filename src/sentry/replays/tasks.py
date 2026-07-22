7ff6e8de6d28d9d8687a4a2370a2f5997758daa4
nikkikapadia <nikki.kapadia@sentry.io>
2026-07-22 12:22:11 -0400

    wip(inc-2344): route replay bulk delete job to long pool

--- src/sentry/replays/tasks.py
+++ src/sentry/replays/tasks.py
@@ -27,7 +27,7 @@ from sentry.replays.usecases.ingest.types import ProcessorContext
 from sentry.replays.usecases.reader import fetch_segments_metadata
 from sentry.silo.base import SiloMode
 from sentry.tasks.base import instrumented_task
-from sentry.taskworker.namespaces import replays_raw_tasks, replays_tasks
+from sentry.taskworker.namespaces import replays_long_tasks, replays_raw_tasks, replays_tasks
 from sentry.utils import metrics
 from sentry.utils.concurrent import ContextPropagatingThreadPoolExecutor
 
@@ -192,7 +192,7 @@ def _delete_if_exists(filename: str) -> None:
 
 @instrumented_task(
     name="sentry.replays.tasks.run_bulk_replay_delete_job",
-    namespace=replays_tasks,
+    namespace=replays_long_tasks,
     retry=Retry(times=5),
     processing_deadline_duration=300,
     silo_mode=SiloMode.CELL,
