f77f14d1fad1f9c23a0a216efc6b6563764b281f
Colleen O'Rourke <colleen@sentry.io>
2026-07-23 10:52:11 -0700

    woops

--- src/sentry/issues/endpoints/group_notes_details.py
+++ src/sentry/issues/endpoints/group_notes_details.py
@@ -159,7 +159,7 @@ class GroupNotesDetailsEndpoint(GroupEndpoint):
                 publish_action(
                     CommentEditAction(
                         comment_id=original_comment_log_action.id,
-                        text=payload.data.get("text"),
+                        text=payload.get("text"),
                         mentions=mentions,
                     ),
                     source=resolve_action_source(request),
