4625d639f9d1baa41730282dc2d55997d3be096c
TkDodo <office@dorfmeister.cc>
2026-07-29 17:19:59 +0200

    fix: alignContent

--- static/app/components/layouts/thirds.tsx
+++ static/app/components/layouts/thirds.tsx
@@ -101,7 +101,7 @@ export function Body({noRowGap, ...props}: BodyProps) {
       flexGrow={1}
       display="grid"
       columns={{zero: 'minmax(0, 1fr)', '4xl': 'minmax(100px, auto) 325px'}}
-      alignContent={{zero: 'stretch', '4xl': 'start'}}
+      alignContent={{zero: 'start', '4xl': 'start'}}
       gap={{zero: '0', '4xl': noRowGap ? '0 2xl' : '2xl'}}
       background="primary"
       padding="lg xl"
