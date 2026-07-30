e1241a3c1d1f1a71b44eccb320896a12317812c8
Leander Rodrigues <me@leander.xyz>
2026-07-30 15:11:02 -0400

    🏷️ fix mypy stuff and add init files

--- tests/sentry/workflow_engine/endpoints/validators/test_utils.py
+++ tests/sentry/workflow_engine/endpoints/validators/test_utils.py
@@ -1,8 +1,9 @@
 import pytest
 from rest_framework.exceptions import PermissionDenied
+from rest_framework.request import Request
 from rest_framework.serializers import ValidationError
 
-from sentry.auth.access import NoAccess, SystemAccess
+from sentry.auth.access import Access, NoAccess, SystemAccess
 from sentry.testutils.cases import TestCase
 from sentry.workflow_engine.defaults.detectors import ensure_default_all_projects_detector
 from sentry.workflow_engine.endpoints.validators.utils import (
@@ -11,12 +12,12 @@ from sentry.workflow_engine.endpoints.validators.utils import (
 
 
 class TestValidateDetectorsExistAndHavePermissions(TestCase):
-    def _make_request_with_access(self, access):
-        request = self.make_request(user=self.user)
+    def _make_request_with_access(self, access: Access) -> Request:
+        request: Request = self.make_request(user=self.user)  # type: ignore[assignment]
         request.access = access
         return request
 
-    def test_project_scoped_detector(self):
+    def test_project_scoped_detector(self) -> None:
         detector = self.create_detector(project=self.project)
         request = self._make_request_with_access(SystemAccess())
 
@@ -25,7 +26,7 @@ class TestValidateDetectorsExistAndHavePermissions(TestCase):
         )
         assert detector in result
 
-    def test_null_project_detector(self):
+    def test_null_project_detector(self) -> None:
         detector = ensure_default_all_projects_detector(self.organization.id)
         request = self._make_request_with_access(SystemAccess())
 
@@ -34,7 +35,7 @@ class TestValidateDetectorsExistAndHavePermissions(TestCase):
         )
         assert detector in result
 
-    def test_mix_of_project_and_null_project_detectors(self):
+    def test_mix_of_project_and_null_project_detectors(self) -> None:
         project_detector = self.create_detector(project=self.project)
         all_projects_detector = ensure_default_all_projects_detector(self.organization.id)
         request = self._make_request_with_access(SystemAccess())
@@ -47,13 +48,13 @@ class TestValidateDetectorsExistAndHavePermissions(TestCase):
             all_projects_detector.id,
         }
 
-    def test_nonexistent_detector_raises(self):
+    def test_nonexistent_detector_raises(self) -> None:
         request = self._make_request_with_access(SystemAccess())
 
         with pytest.raises(ValidationError, match="do not exist"):
             validate_detectors_exist_and_have_permissions([999999], self.organization, request)
 
-    def test_detector_from_other_org_not_found(self):
+    def test_detector_from_other_org_not_found(self) -> None:
         other_org = self.create_organization()
         other_project = self.create_project(organization=other_org)
         other_detector = self.create_detector(project=other_project)
@@ -64,7 +65,7 @@ class TestValidateDetectorsExistAndHavePermissions(TestCase):
                 [other_detector.id], self.organization, request
             )
 
-    def test_null_project_detector_from_other_org_permission_denied(self):
+    def test_null_project_detector_from_other_org_permission_denied(self) -> None:
         """
         A null-project detector belonging to another org should be rejected
         by the permission check since the user won't have access.
@@ -78,14 +79,14 @@ class TestValidateDetectorsExistAndHavePermissions(TestCase):
                 [other_all_projects_detector.id], self.organization, request
             )
 
-    def test_permission_denied_when_no_access(self):
+    def test_permission_denied_when_no_access(self) -> None:
         detector = self.create_detector(project=self.project)
         request = self._make_request_with_access(NoAccess())
 
         with pytest.raises(PermissionDenied):
             validate_detectors_exist_and_have_permissions([detector.id], self.organization, request)
 
-    def test_null_project_detector_permission_denied_when_no_access(self):
+    def test_null_project_detector_permission_denied_when_no_access(self) -> None:
         detector = ensure_default_all_projects_detector(self.organization.id)
         request = self._make_request_with_access(NoAccess())
 
