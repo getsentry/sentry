26e4ad99f9f8e0923d594031c17e009821160e1c
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-07-31 11:01:19 +0200

    ref(replays): Use primitives for onboarding panel layout
    Refs #120315

--- static/app/views/explore/replays/list/replayPanel.tsx
+++ static/app/views/explore/replays/list/replayPanel.tsx
@@ -1,4 +1,4 @@
-import styled from '@emotion/styled';
+import {Container, Flex} from '@sentry/scraps/layout';
 
 import {Panel} from 'sentry/components/panels/panel';
 
@@ -11,57 +11,42 @@ interface Props extends React.ComponentProps<typeof Panel> {
 export function ReplayPanel({image, noCenter, children, ...props}: Props) {
   return (
     <Panel {...props}>
-      <Container>
-        {image ? <IlloBox>{image}</IlloBox> : null}
-        <StyledBox centered={!image && !noCenter}>{children}</StyledBox>
-      </Container>
+      <Flex
+        align="start"
+        direction="row"
+        display={{zero: 'block', xl: 'flex'}}
+        justify="center"
+        margin={{zero: '0', xl: '0 auto'}}
+        maxWidth={{xl: '1000px'}}
+        minHeight={{xl: '300px', '3xl': '350px'}}
+        padding="2xl"
+        position="relative"
+        wrap="wrap"
+      >
+        {image ? (
+          <Container
+            flex={{xl: 1}}
+            margin={{zero: 'xl auto', xl: '120px 2xl 2xl'}}
+            maxWidth="300px"
+            minHeight="100px"
+            minWidth="150px"
+            position="relative"
+          >
+            {image}
+          </Container>
+        ) : null}
+        <Container
+          flex={{xl: 2}}
+          minWidth="0"
+          style={
+            !image && !noCenter
+              ? {zIndex: 1, textAlign: 'center', maxWidth: '600px'}
+              : {zIndex: 1}
+          }
+        >
+          {children}
+        </Container>
+      </Flex>
     </Panel>
   );
 }
-
-const Container = styled('div')`
-  padding: ${p => p.theme.space['2xl']};
-  position: relative;
-
-  @container (min-width: ${p => p.theme.container.xl}) {
-    display: flex;
-    align-items: flex-start;
-    flex-direction: row;
-    justify-content: center;
-    flex-wrap: wrap;
-    min-height: 300px;
-    max-width: 1000px;
-    margin: 0 auto;
-  }
-
-  @container (min-width: ${p => p.theme.container['3xl']}) {
-    min-height: 350px;
-  }
-`;
-
-const StyledBox = styled('div')<{centered?: boolean}>`
-  min-width: 0;
-  z-index: 1;
-
-  ${p => (p.centered ? 'text-align: center;' : '')}
-  ${p => (p.centered ? 'max-width: 600px;' : '')}
-
-  @container (min-width: ${p => p.theme.container.xl}) {
-    flex: 2;
-  }
-`;
-
-const IlloBox = styled(StyledBox)`
-  position: relative;
-  min-height: 100px;
-  max-width: 300px;
-  min-width: 150px;
-  margin: ${p => p.theme.space.xl} auto;
-
-  @container (min-width: ${p => p.theme.container.xl}) {
-    flex: 1;
-    margin: 120px ${p => p.theme.space['2xl']} ${p => p.theme.space['2xl']}
-      ${p => p.theme.space['2xl']};
-    max-width: auto;
-  }
-`;
