a8a8ab4c6c76f09be98ca16f6f74fa874dc58bc1
Malachi Willey <malwilley@gmail.com>
2026-07-31 12:45:32 -0700

    fix(autofix): Expand the first issue preview section

--- static/app/views/issueDetails/issuePreview/issuePreview.tsx
+++ static/app/views/issueDetails/issuePreview/issuePreview.tsx
@@ -212,7 +212,9 @@ function IssuePreviewContent() {
       ) : (
         <Dividers>
           <LinkedPullRequests group={group} showEmptyState={false} />
-          {hasAutofix ? <IssuePreviewAutofixSummary runState={autofix.runState} /> : null}
+          {hasAutofix ? (
+            <IssuePreviewAutofixSummary key={groupId} runState={autofix.runState} />
+          ) : null}
           <Container>
             <ErrorBoundary mini>
               <FoldSection
