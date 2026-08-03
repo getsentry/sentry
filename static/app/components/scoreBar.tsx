45312eaaff2ece9be74f4509edd156213c3565ee
TkDodo <dominik.dorfmeister@sentry.io>
2026-08-03 17:56:01 +0200

    fix: duplicate Array.from

--- static/app/components/scoreBar.tsx
+++ static/app/components/scoreBar.tsx
@@ -41,10 +41,10 @@ function BaseScoreBar({
 
   return (
     <div className={className} {...props}>
-      {Array.from(Array.from({length: scoreInBounds}), (_j, i) => (
+      {Array.from({length: scoreInBounds}, (_, i) => (
         <Bar {...barProps} key={i} color={palette[paletteIndex]} />
       ))}
-      {Array.from(Array.from({length: maxScore - scoreInBounds}), (_j, i) => (
+      {Array.from({length: maxScore - scoreInBounds}, (_, i) => (
         <Bar key={`empty-${i}`} {...barProps} empty />
       ))}
     </div>
