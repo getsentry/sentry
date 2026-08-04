eb6788eddf01b39c630e63c14e589777ed52e0ee
Rudra Patel <rudra.patel@sentry.io>
2026-08-04 13:25:37 -0700

    feat(issues): Add copy to clipboard button for http requests

--- static/app/components/events/interfaces/request/index.tsx
+++ static/app/components/events/interfaces/request/index.tsx
@@ -7,6 +7,7 @@ import {ExternalLink} from '@sentry/scraps/link';
 import {SegmentedControl} from '@sentry/scraps/segmentedControl';
 import {Text} from '@sentry/scraps/text';
 
+import {CopyAsDropdown} from 'sentry/components/copyAsDropdown';
 import {ErrorBoundary} from 'sentry/components/errorBoundary';
 import {KeyValueList} from 'sentry/components/events/interfaces/keyValueList';
 import {GraphQlRequestBody} from 'sentry/components/events/interfaces/request/graphQlRequestBody';
@@ -24,6 +25,7 @@ import type {EntryRequest, Event} from 'sentry/types/event';
 import {EntryType} from 'sentry/types/event';
 import {defined} from 'sentry/utils/defined';
 import {isValidUrl} from 'sentry/utils/string/isValidUrl';
+import {copyToClipboard} from 'sentry/utils/useCopyToClipboard';
 import {SectionKey} from 'sentry/views/issueDetails/context';
 import {FoldSection} from 'sentry/views/issueDetails/foldSection';
 
@@ -146,21 +148,46 @@ export function Request({data, event}: RequestProps) {
     parsedUrl.href = fullUrl;
   }
 
-  let actions: React.ReactNode = null;
+  const canGenerateCurlCommand = !isPartial && fullUrl;
 
-  if (!isPartial && fullUrl) {
-    actions = (
-      <SegmentedControl aria-label={t('View')} size="xs" value={view} onChange={setView}>
-        <SegmentedControl.Item key="formatted">
-          {/* Translators: this means "formatted" rendering (fancy tables) */}
-          {t('Formatted')}
-        </SegmentedControl.Item>
-        <SegmentedControl.Item key="curl" textValue="curl">
-          <Text monospace>curl</Text>
-        </SegmentedControl.Item>
-      </SegmentedControl>
-    );
-  }
+  const actions = (
+    <Flex gap="sm" align="center">
+      {canGenerateCurlCommand && (
+        <SegmentedControl
+          aria-label={t('View')}
+          size="xs"
+          value={view}
+          onChange={setView}
+        >
+          <SegmentedControl.Item key="formatted">
+            {/* Translators: this means "formatted" rendering (fancy tables) */}
+            {t('Formatted')}
+          </SegmentedControl.Item>
+          <SegmentedControl.Item key="curl" textValue="curl">
+            <Text monospace>curl</Text>
+          </SegmentedControl.Item>
+        </SegmentedControl>
+      )}
+
+      <CopyAsDropdown
+        size="xs"
+        items={[
+          {
+            key: 'fullUrl',
+            label: t('Full URL'),
+            onAction: () => copyToClipboard(fullUrl ?? ''),
+            disabled: !fullUrl,
+          },
+          {
+            key: 'path',
+            label: t('Path'),
+            onAction: () => copyToClipboard(parsedUrl?.pathname ?? ''),
+            disabled: !parsedUrl,
+          },
+        ]}
+      />
+    </Flex>
+  );
 
   const title = (
     <TruncatedPathLink method={data.method} url={parsedUrl} fullUrl={fullUrl} />
