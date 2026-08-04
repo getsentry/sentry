bf746567c66b39254f2739278aa190590d5a2548
Malachi Willey <malwilley@gmail.com>
2026-08-04 14:04:06 -0700

    Without seer it should only show the count for fix proposed

--- static/app/views/issueList/queries/useInboxIssueCount.tsx
+++ static/app/views/issueList/queries/useInboxIssueCount.tsx
@@ -6,16 +6,25 @@ import {useOrganization} from 'sentry/utils/useOrganization';
 // Count all issues assigned to me/my teams which are assigned or further along
 const INBOX_COUNT_QUERY =
   'is:unresolved issue.progress:[fix_proposed,diagnosed,assigned] assignee:[me,my_teams]';
+const INBOX_COUNT_QUERY_NO_SEER =
+  'is:unresolved issue.progress:[fix_proposed] assignee:[me,my_teams]';
 
 export function useInboxIssueCount() {
   const organization = useOrganization();
+  const hasSeer =
+    !organization.hideAiFeatures &&
+    (organization.features.includes('seat-based-seer-enabled') ||
+      organization.features.includes('seer-added'));
 
   const {data} = useQuery({
     ...apiOptions.as<Record<string, number>>()(
       '/organizations/$organizationIdOrSlug/issues-count/',
       {
         path: {organizationIdOrSlug: organization.slug},
-        query: {query: [INBOX_COUNT_QUERY], project: [-1]},
+        query: {
+          query: [hasSeer ? INBOX_COUNT_QUERY : INBOX_COUNT_QUERY_NO_SEER],
+          project: [-1],
+        },
         staleTime: 180_000,
       }
     ),
