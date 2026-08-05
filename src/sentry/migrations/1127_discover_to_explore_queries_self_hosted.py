87d499e2d2b92aad983ccfca8e92dddd8ffdd335
Dan Fuller <dfuller@sentry.io>
2026-08-05 14:50:56 -0700

    fix(migrations): Fix broken migration dependency (#121369)
    After we deleted the old squashed migrations we missed this one
    dependency link

--- src/sentry/migrations/1127_discover_to_explore_queries_self_hosted.py
+++ src/sentry/migrations/1127_discover_to_explore_queries_self_hosted.py
@@ -81,7 +81,7 @@ class Migration(CheckedMigration):
 
     dependencies = [
         ("sentry", "1126_transactions_to_spans_widgets_self_hosted"),
-        ("discover", "0003_discover_json_field"),
+        ("discover", "0001_squashed_0003_discover_json_field"),
         ("explore", "0006_add_changed_reason_field_explore"),
     ]
 
