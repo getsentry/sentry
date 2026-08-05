4f76c0dc91a6a6f890c850155bc82e059a6a6839
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2026-08-05 11:53:10 -0400

    fix(inbox): Simplify view PR action label

--- static/app/views/issueList/pages/inbox.spec.tsx
+++ static/app/views/issueList/pages/inbox.spec.tsx
@@ -792,7 +792,7 @@ describe('InboxPage', () => {
 
     const preview = await openFixProposedPreview();
     const pullRequestButton = await within(preview).findByRole('button', {
-      name: 'View org/repository#10',
+      name: 'View PR',
     });
     expect(pullRequestButton).toHaveAttribute(
       'href',
@@ -840,7 +840,7 @@ describe('InboxPage', () => {
     const preview = await openFixProposedPreview();
     expect(
       await within(preview).findByRole('button', {
-        name: 'View org/repository#649',
+        name: 'View PR',
       })
     ).toHaveAttribute('href', 'https://github.com/org/repository/pull/649');
   });
