b7d4c3fd5141175b566d1d6f06ddb722fe9542ad
Kyle Consalus <kyle.consalus@sentry.io>
2026-08-05 12:55:58 -0700

    fix(derived data): Modernize the behavior of invalidate_group_derived_data (#121205)
    We now support and prefer soft invalidation, so we make that the
    default.
    In the soft case, we clear the GroupDerivedData's pipeline hash, making
    it explicitly in need of regen, then
    we schedule a regen.
    Since such scheduling isn't always necessary or desirable, regen
    triggering is guarded by a defaulted parameter. It's not as much that we
    expect this to be used, but that presenting the option makes the
    behavior more explicit.
    
    Additionally, since in some cases invalidation may be based on newer
    data than the cursor, we trigger incremental processing in that case to
    maintain the expectation of "if I insert stuff then invalidate, the GDD
    will be up-to-date".
    
    Fixes ISWF-3196.
    Fixes ISWF-3180.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -359,8 +359,6 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:issue-activity-feed-v2", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable new stack trace component for issue details
     manager.add("organizations:issue-details-new-stack-trace", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable invalidating group derived data when groups are merged
-    manager.add("organizations:hard-delete-derived-data-invalidation", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable double-reading from EAP for issue feed search queries
     manager.add("organizations:issue-feed.eap-search", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Fetch CI check status from the provider for pull requests linked to an issue
