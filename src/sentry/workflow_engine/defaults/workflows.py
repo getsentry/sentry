b9c08d8027acc838388657d1e950141733f30945
Leander Rodrigues <me@leander.xyz>
2026-08-06 15:29:41 -0400

    ✨ default workflow as well

--- src/sentry/workflow_engine/defaults/workflows.py
+++ src/sentry/workflow_engine/defaults/workflows.py
@@ -6,12 +6,19 @@ from sentry.models.organization import Organization
 from sentry.models.project import Project
 from sentry.notifications.models.notificationaction import ActionTarget
 from sentry.notifications.types import FallthroughChoiceType
-from sentry.workflow_engine.defaults.detectors import _ensure_detector
+from sentry.workflow_engine.defaults.detectors import (
+    _ensure_detector,
+    ensure_default_all_projects_detector,
+)
+from sentry.workflow_engine.handlers.condition.seer_activity_trigger_handler import (
+    SeerActivityTriggerStage,
+)
 from sentry.workflow_engine.models import (
     Action,
     DataCondition,
     DataConditionGroup,
     DataConditionGroupAction,
+    Detector,
     DetectorWorkflow,
     Workflow,
     WorkflowDataConditionGroup,
@@ -20,26 +27,15 @@ from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.typings.grouptype import IssueStreamGroupType
 
 DEFAULT_WORKFLOW_LABEL = "Send a notification for high priority issues"
+PULL_REQUEST_WORKFLOW_LABEL = "Send a notification when pull requests are ready"
 
 
-def connect_workflows_to_issue_stream(
-    project: Project,
+def connect_workflows_to_detector(
+    detector: Detector,
     workflows: list[Workflow],
 ) -> Sequence[DetectorWorkflow]:
-    # Because we don't know if this signal is handled already or not...
-    issue_stream_detector = _ensure_detector(project, IssueStreamGroupType.slug)
-
-    connections = [
-        DetectorWorkflow(
-            workflow=workflow,
-            detector=issue_stream_detector,
-        )
-        for workflow in workflows
-    ]
-    return DetectorWorkflow.objects.bulk_create(
-        connections,
-        ignore_conflicts=True,
-    )
+    connections = [DetectorWorkflow(workflow=workflow, detector=detector) for workflow in workflows]
+    return DetectorWorkflow.objects.bulk_create(connections, ignore_conflicts=True)
 
 
 def create_priority_workflow(org: Organization) -> Workflow:
@@ -105,8 +101,67 @@ def create_priority_workflow(org: Organization) -> Workflow:
     return workflow
 
 
+def create_pull_request_workflow(organization: Organization) -> Workflow:
+    with transaction.atomic(router.db_for_write(Workflow)):
+        when_condition_group = DataConditionGroup.objects.create(
+            logic_type=DataConditionGroup.Type.ANY_SHORT_CIRCUIT, organization=organization
+        )
+        workflow = Workflow.objects.create(
+            organization=organization,
+            name=PULL_REQUEST_WORKFLOW_LABEL,
+            when_condition_group=when_condition_group,
+            config={"frequency": 0},
+        )
+        DataCondition.objects.create(
+            type=Condition.SEER_ACTIVITY_TRIGGER,
+            condition_group=when_condition_group,
+            comparison=[SeerActivityTriggerStage.PR_CREATED.value],
+            condition_result=True,
+        )
+        action_filter = DataConditionGroup.objects.create(
+            logic_type=DataConditionGroup.Type.ANY_SHORT_CIRCUIT,
+            organization=organization,
+        )
+        action = Action.objects.create(
+            type=Action.Type.EMAIL,
+            config={
+                "target_type": ActionTarget.ISSUE_OWNERS,
+                "target_identifier": None,
+            },
+            data={
+                "fallthrough_type": FallthroughChoiceType.ACTIVE_MEMBERS.value,
+            },
+        )
+        DataConditionGroupAction.objects.create(action=action, condition_group=action_filter)
+        WorkflowDataConditionGroup.objects.create(workflow=workflow, condition_group=action_filter)
+
+    return workflow
+
+
 def ensure_default_workflows(project: Project) -> list[Workflow]:
     workflows = [create_priority_workflow(project.organization)]
-    connect_workflows_to_issue_stream(project, workflows)
+    # Because we don't know if this signal is handled already or not...
+    issue_stream_detector = _ensure_detector(project, IssueStreamGroupType.slug)
+    connect_workflows_to_detector(issue_stream_detector, workflows)
+    return workflows
 
+
+def ensure_pull_request_workflow(organization: Organization, detector: Detector) -> Workflow:
+    """
+    A primitive attempt to prevent duplicate workflows by checking if this label/detector combo
+    already exists. We don't intend to call `ensure_default_organization_workflows` twice regardless,
+    but this will guard if that does happen in quick succession (maybe from an RPC blip somehow).
+    """
+    existing = Workflow.objects.filter(
+        organization=organization,
+        name=PULL_REQUEST_WORKFLOW_LABEL,
+        detectorworkflow__detector=detector,
+    ).first()
+    return existing if existing else create_pull_request_workflow(organization)
+
+
+def ensure_default_organization_workflows(organization: Organization) -> list[Workflow]:
+    all_projects_detector = ensure_default_all_projects_detector(organization.id)
+    workflows = [ensure_pull_request_workflow(organization, all_projects_detector)]
+    connect_workflows_to_detector(all_projects_detector, workflows)
     return workflows
--- src/sentry/workflow_engine/receivers/organization_workflows.py
+++ src/sentry/workflow_engine/receivers/organization_workflows.py
@@ -7,21 +7,21 @@ from sentry.models.organization import Organization
 from sentry.signals import organization_created
 from sentry.workflow_engine.defaults.detectors import (
     UnableToAcquireLockApiError,
-    ensure_default_organization_detectors,
 )
+from sentry.workflow_engine.defaults.workflows import ensure_default_organization_workflows
 
 
-def create_organization_detectors(organization: Organization, **kwargs: Any) -> None:
+def create_organization_workflows(organization: Organization, **kwargs: Any) -> None:
     if not options.get("workflow_engine.all_projects_auto_creation_enabled"):
         return
     try:
-        ensure_default_organization_detectors(organization)
+        ensure_default_organization_workflows(organization)
     except UnableToAcquireLockApiError as e:
         sentry_sdk.capture_exception(e)
 
 
 organization_created.connect(
-    create_organization_detectors,
-    dispatch_uid="create_organization_detectors",
+    create_organization_workflows,
+    dispatch_uid="create_organization_workflows",
     weak=False,
 )
