9e687a449b8855da60a2d8360704392cb39017b9
Leander Rodrigues <me@leander.xyz>
2026-08-07 11:56:28 -0400

    ✏️ review comments

--- src/sentry/workflow_engine/receivers/organization_detectors.py
+++ src/sentry/workflow_engine/receivers/organization_detectors.py
@@ -9,15 +9,17 @@ from sentry.workflow_engine.defaults.detectors import (
     UnableToAcquireLockApiError,
     ensure_default_organization_detectors,
 )
+from sentry.workflow_engine.models import Detector
 
 
-def create_organization_detectors(organization: Organization, **kwargs: Any) -> None:
+def create_organization_detectors(organization: Organization, **kwargs: Any) -> dict[str, Detector]:
     if not options.get("workflow_engine.all_projects_auto_creation_enabled"):
-        return
+        return {}
     try:
-        ensure_default_organization_detectors(organization)
+        return ensure_default_organization_detectors(organization)
     except UnableToAcquireLockApiError as e:
         sentry_sdk.capture_exception(e)
+    return {}
 
 
 organization_created.connect(
