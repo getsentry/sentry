e54705fa35a54ba62fc6cc513251b448967e125a
Colleen O'Rourke <colleen@sentry.io>
2026-08-07 11:51:03 -0700

    Remove hasEAPAlerts, unused after the wizard removal

--- static/app/views/insights/common/utils/hasEAPAlerts.tsx
+++ static/app/views/insights/common/utils/hasEAPAlerts.tsx
@@ -1,10 +1,6 @@
 /* eslint-disable unicorn/filename-case */
 import type {Organization} from 'sentry/types/organization';
 
-export function hasEAPAlerts(organization: Organization): boolean {
-  return organization.features.includes('visibility-explore-view');
-}
-
 export function deprecateTransactionAlerts(organization: Organization): boolean {
   return organization.features.includes('discover-saved-queries-deprecation');
 }
