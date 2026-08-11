ac2f26993b56636790c30e15d4495618f2a7c463
elramen <elias.ram@sentry.io>
2026-08-11 15:01:51 +0200

    Add sync comment

--- src/sentry/models/artifactbundle.py
+++ src/sentry/models/artifactbundle.py
@@ -27,6 +27,7 @@ from sentry.utils.hashlib import sha1_text
 NULL_UUID = "00000000-00000000-00000000-00000000"
 NULL_STRING = ""
 
+# Must stay in sync with Symbolicator's `object_file_max_decompressed_source_size`
 MAX_SOURCE_FILE_SIZE = 190 * 1024 * 1024
 
 
