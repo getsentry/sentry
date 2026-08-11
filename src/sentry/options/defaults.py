16f49f46f6d9c92ffa8dd3c63bc3f2189b5eb17c
Charlie Luo <cvxluo@gmail.com>
2026-08-11 13:41:33 -0400

    chore(post-process): clean up rollout option (#121736)
    Since we have post process job level killswitches now, and since this
    has been rolled out fully, we can remove this rollout option added by
    https://github.com/getsentry/sentry/pull/121481. Basically I don't see a
    reason why we'd need a rollout that isn't 100% or 0% for the foreseeable
    future.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1087,12 +1087,6 @@ register(
     type=Bool,
     flags=FLAG_MODIFIABLE_BOOL | FLAG_AUTOMATOR_MODIFIABLE,
 )
-register(
-    "seer.post-process-issue-summary.rollout-rate",
-    type=Float,
-    default=0.0,
-    flags=FLAG_MODIFIABLE_RATE | FLAG_AUTOMATOR_MODIFIABLE,
-)
 
 register(
     "seer.similarity-killswitch.enabled",
