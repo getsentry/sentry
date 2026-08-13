288a1d5c9755e92b31aa7a044d0d1c182e076c23
TkDodo <office@dorfmeister.cc>
2026-08-13 17:03:51 +0200

    revert accidental removal

--- static/app/components/core/form/index.ts
+++ static/app/components/core/form/index.ts
@@ -11,5 +11,6 @@ export {AutoSaveForm} from './autoSaveForm';
 export {AutoSaveContextProvider} from './autoSaveContext';
 export {FieldGroup} from './layout/fieldGroup';
 export {FormSearch} from './FormSearch';
+export {FORM_FIELD_REGISTRY} from './generatedFieldRegistry';
 // eslint-disable-next-line no-restricted-imports
 export {useStore} from '@tanstack/react-form';
