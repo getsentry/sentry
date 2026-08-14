74d3981c83740d0f8f3593c7f78a24a61689a494
Pierre Massat <pierre.massat@sentry.io>
2026-08-13 21:00:57 -0700

    fix: Type empty custom measurements context default

--- static/app/utils/customMeasurements/customMeasurementsContext.tsx
+++ static/app/utils/customMeasurements/customMeasurementsContext.tsx
@@ -2,6 +2,8 @@ import {createContext} from 'react';
 
 import type {CustomMeasurementCollection} from 'sentry/utils/customMeasurements/customMeasurements';
 
+const emptyCustomMeasurements: CustomMeasurementCollection = {};
+
 export const CustomMeasurementsContext = createContext({
-  customMeasurements: {},
+  customMeasurements: emptyCustomMeasurements,
 });
