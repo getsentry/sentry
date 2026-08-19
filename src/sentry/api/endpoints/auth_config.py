8b589087371232bc2a0be63d011b17399004a018
Evan Purkhiser <evanpurkhiser@gmail.com>
2026-08-19 09:25:01 -0400

    fix(auth): Camelize auth config callback keys
    Normalize legacy template callback context before returning it from the auth config API so frontend consumers receive camelCase response keys.

--- src/sentry/api/endpoints/auth_config.py
+++ src/sentry/api/endpoints/auth_config.py
@@ -17,6 +17,7 @@ from sentry.api.serializers.models.auth import (
     AuthMfaRequiredSerializerResponse,
     serialize_auth_mfa_required,
 )
+from sentry.api.serializers.rest_framework import convert_dict_key_case, snake_to_camel_case
 from sentry.constants import WARN_SESSION_EXPIRED
 from sentry.http import get_server_hostname
 from sentry.models.organization import Organization
@@ -39,7 +40,11 @@ class AuthConfigResponse(TypedDict):
     pendingMfa: AuthMfaRequiredSerializerResponse | None
     mfaMethods: NotRequired[list[AuthMfaMethodSerializerResponse]]
     mfaRequired: NotRequired[bool]
+    githubLoginLink: NotRequired[str]
+    googleLoginLink: NotRequired[str]
+    vstsLoginLink: NotRequired[str]
     warning: NotRequired[str]
+    loginBanner: NotRequired[str]
 
 
 @control_silo_endpoint
@@ -125,6 +130,9 @@ class AuthConfigEndpoint(Endpoint, OrganizationMixin):
         if "session_expired" in request.COOKIES:
             context["warning"] = str(WARN_SESSION_EXPIRED)
 
-        context.update(additional_context.run_callbacks(request))
+        additional_login_context = convert_dict_key_case(
+            additional_context.run_callbacks(request), snake_to_camel_case
+        )
+        context.update(additional_login_context)
 
         return cast(AuthConfigResponse, context)
