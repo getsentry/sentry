3d93d2396c88e055e9000ba71b061addb53a6135
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-08-21 09:45:38 +0200

    ref(forms): Migrate consent forms to Scraps

--- static/app/views/beaconConsent/index.tsx
+++ static/app/views/beaconConsent/index.tsx
@@ -1,18 +1,45 @@
 import {useEffect} from 'react';
+import {useMutation} from '@tanstack/react-query';
+import {z} from 'zod';
 
+import {defaultFormOptions, useScrapsForm} from '@sentry/scraps/form';
+import {Flex, Stack} from '@sentry/scraps/layout';
 import {ExternalLink} from '@sentry/scraps/link';
+import {Text} from '@sentry/scraps/text';
 
-import {ApiForm} from 'sentry/components/forms/apiForm';
-import {FieldWrapper} from 'sentry/components/forms/fieldGroup/fieldWrapper';
-import {RadioField} from 'sentry/components/forms/fields/radioField';
 import {NarrowLayout} from 'sentry/components/narrowLayout';
 import {t, tct} from 'sentry/locale';
+import {fetchMutation} from 'sentry/utils/queryClient';
+
+const consentChoiceSchema = z.enum(['true', 'false']);
+const schema = z.object({
+  beacon: z.object({record_cpu_ram_usage: consentChoiceSchema}),
+});
+const defaultValues: z.infer<typeof schema> = {
+  beacon: {record_cpu_ram_usage: 'true'},
+};
 
 type Props = {
   onSubmitSuccess?: () => void;
 };
 
 function BeaconConsent({onSubmitSuccess}: Props) {
+  const mutation = useMutation({
+    mutationFn: (data: z.infer<typeof schema>) =>
+      fetchMutation({
+        url: '/internal/options/',
+        method: 'PUT',
+        data: {'beacon.record_cpu_ram_usage': data.beacon.record_cpu_ram_usage},
+      }),
+    onSuccess: onSubmitSuccess,
+  });
+  const form = useScrapsForm({
+    ...defaultFormOptions,
+    defaultValues,
+    validators: {onDynamic: schema},
+    onSubmit: ({value}) => mutation.mutateAsync(value).catch(() => {}),
+  });
+
   useEffect(() => {
     document.body.classList.add('auth');
 
@@ -21,39 +48,47 @@ function BeaconConsent({onSubmitSuccess}: Props) {
 
   return (
     <NarrowLayout>
-      <ApiForm
-        apiMethod="PUT"
-        apiEndpoint="/internal/options/"
-        onSubmitSuccess={onSubmitSuccess}
-        submitLabel={t('Continue')}
-      >
-        <FieldWrapper stacked={false} hasControlState={false}>
-          {t(
-            'We have made some updates to our self-hosted beacon broadcast system, and just need to get a quick answer from you.'
-          )}
-        </FieldWrapper>
-        <RadioField
-          name="beacon.record_cpu_ram_usage"
-          defaultValue={() => 'true'}
-          choices={[
-            [
-              'true',
-              t(
-                'Yes, I would love to help Sentry developers improve the experience of self-hosted by sending CPU/RAM usage'
-              ),
-            ],
-            ['false', t("No, I'd prefer to keep CPU/RAM usage private")],
-          ]}
-          label={t('CPU/RAM Usage')}
-          required
-          help={tct(
-            `Recording CPU/RAM usage will greatly help our development team understand how self-hosted sentry
-            is typically being used, and to keep track of improvements that we hope to bring you in the future.`,
-            {link: <ExternalLink href="https://sentry.io/privacy/" />}
-          )}
-          inline={false}
-        />
-      </ApiForm>
+      <form.AppForm form={form}>
+        <Stack gap="lg">
+          <Text>
+            {t(
+              'We have made some updates to our self-hosted beacon broadcast system, and just need to get a quick answer from you.'
+            )}
+          </Text>
+          <form.AppField name="beacon.record_cpu_ram_usage">
+            {field => (
+              <field.Layout.Stack
+                label={t('CPU/RAM Usage')}
+                hintText={tct(
+                  `Recording CPU/RAM usage will greatly help our development team understand how self-hosted sentry
+                  is typically being used, and to keep track of improvements that we hope to bring you in the future.`,
+                  {link: <ExternalLink href="https://sentry.io/privacy/" />}
+                )}
+                required
+              >
+                <field.Radio.Group
+                  value={field.state.value}
+                  onChange={value => field.handleChange(consentChoiceSchema.parse(value))}
+                >
+                  <Stack gap="sm">
+                    <field.Radio.Item value="true">
+                      {t(
+                        'Yes, I would love to help Sentry developers improve the experience of self-hosted by sending CPU/RAM usage'
+                      )}
+                    </field.Radio.Item>
+                    <field.Radio.Item value="false">
+                      {t("No, I'd prefer to keep CPU/RAM usage private")}
+                    </field.Radio.Item>
+                  </Stack>
+                </field.Radio.Group>
+              </field.Layout.Stack>
+            )}
+          </form.AppField>
+          <Flex justify="end">
+            <form.SubmitButton>{t('Continue')}</form.SubmitButton>
+          </Flex>
+        </Stack>
+      </form.AppForm>
     </NarrowLayout>
   );
 }
--- static/app/views/newsletterConsent/index.tsx
+++ static/app/views/newsletterConsent/index.tsx
@@ -1,18 +1,37 @@
 import {useEffect} from 'react';
+import {useMutation} from '@tanstack/react-query';
+import {z} from 'zod';
 
+import {defaultFormOptions, useScrapsForm} from '@sentry/scraps/form';
+import {Flex, Stack} from '@sentry/scraps/layout';
 import {ExternalLink} from '@sentry/scraps/link';
+import {Text} from '@sentry/scraps/text';
 
-import {ApiForm} from 'sentry/components/forms/apiForm';
-import {FieldWrapper} from 'sentry/components/forms/fieldGroup/fieldWrapper';
-import {RadioField} from 'sentry/components/forms/fields/radioField';
 import {NarrowLayout} from 'sentry/components/narrowLayout';
 import {t, tct} from 'sentry/locale';
+import {fetchMutation} from 'sentry/utils/queryClient';
+
+const consentChoiceSchema = z.enum(['true', 'false']);
+const schema = z.object({subscribed: consentChoiceSchema});
+const defaultValues: z.infer<typeof schema> = {subscribed: 'true'};
 
 type Props = {
   onSubmitSuccess?: () => void;
 };
 
 function NewsletterConsent({onSubmitSuccess}: Props) {
+  const mutation = useMutation({
+    mutationFn: (data: z.infer<typeof schema>) =>
+      fetchMutation({url: '/users/me/subscriptions/', method: 'POST', data}),
+    onSuccess: onSubmitSuccess,
+  });
+  const form = useScrapsForm({
+    ...defaultFormOptions,
+    defaultValues,
+    validators: {onDynamic: schema},
+    onSubmit: ({value}) => mutation.mutateAsync(value).catch(() => {}),
+  });
+
   useEffect(() => {
     document.body.classList.add('auth');
 
@@ -22,34 +41,45 @@ function NewsletterConsent({onSubmitSuccess}: Props) {
   // NOTE: the text here is duplicated within ``RegisterForm`` on the backend
   return (
     <NarrowLayout>
-      <ApiForm
-        apiMethod="POST"
-        apiEndpoint="/users/me/subscriptions/"
-        onSubmitSuccess={onSubmitSuccess}
-        submitLabel={t('Continue')}
-      >
-        <FieldWrapper stacked={false} hasControlState={false}>
-          {t('Pardon the interruption, we just need to get a quick answer from you.')}
-        </FieldWrapper>
-        <RadioField
-          name="subscribed"
-          choices={[
-            ['true', t('Yes, I would like to receive updates via email')],
-            ['false', t("No, I'd prefer not to receive these updates")],
-          ]}
-          label={t('Email Updates')}
-          required
-          inline={false}
-          help={tct(
-            `We'd love to keep you updated via email with product and feature
-               announcements, promotions, educational materials, and events. Our updates
-               focus on relevant information, and we'll never sell your data to third
-               parties. See our [link:Privacy Policy] for more details.
-               `,
-            {link: <ExternalLink href="https://sentry.io/privacy/" />}
-          )}
-        />
-      </ApiForm>
+      <form.AppForm form={form}>
+        <Stack gap="lg">
+          <Text>
+            {t('Pardon the interruption, we just need to get a quick answer from you.')}
+          </Text>
+          <form.AppField name="subscribed">
+            {field => (
+              <field.Layout.Stack
+                label={t('Email Updates')}
+                hintText={tct(
+                  `We'd love to keep you updated via email with product and feature
+                  announcements, promotions, educational materials, and events. Our updates
+                  focus on relevant information, and we'll never sell your data to third
+                  parties. See our [link:Privacy Policy] for more details.`,
+                  {link: <ExternalLink href="https://sentry.io/privacy/" />}
+                )}
+                required
+              >
+                <field.Radio.Group
+                  value={field.state.value}
+                  onChange={value => field.handleChange(consentChoiceSchema.parse(value))}
+                >
+                  <Stack gap="sm">
+                    <field.Radio.Item value="true">
+                      {t('Yes, I would like to receive updates via email')}
+                    </field.Radio.Item>
+                    <field.Radio.Item value="false">
+                      {t("No, I'd prefer not to receive these updates")}
+                    </field.Radio.Item>
+                  </Stack>
+                </field.Radio.Group>
+              </field.Layout.Stack>
+            )}
+          </form.AppField>
+          <Flex justify="end">
+            <form.SubmitButton>{t('Continue')}</form.SubmitButton>
+          </Flex>
+        </Stack>
+      </form.AppForm>
     </NarrowLayout>
   );
 }
