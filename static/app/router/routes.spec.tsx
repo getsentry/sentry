ceb6933406c5322c68688e366f19288cdcd99d5a
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-08-25 11:32:54 +0200

    ref(router): Inline the legacy stories redirect

--- static/app/router/routes.spec.tsx
+++ static/app/router/routes.spec.tsx
@@ -1,7 +1,9 @@
 import {matchRoutes, type RouteObject} from 'react-router-dom';
 
+import {render, waitFor} from 'sentry-test/reactTestingLibrary';
+
 import * as constants from 'sentry/constants';
-import {buildRoutes} from 'sentry/router/routes';
+import {buildRoutes, LegacyStoriesRedirect} from 'sentry/router/routes';
 import {replaceRouterParams} from 'sentry/utils/replaceRouterParams';
 import {normalizeUrl} from 'sentry/utils/url/normalizeUrl';
 
@@ -183,5 +185,25 @@ describe('buildRoutes()', () => {
       '/:orgId/:projectId/',
       'events/:eventId/',
     ]);
+  });
+});
+
+describe('LegacyStoriesRedirect', () => {
+  it('redirects stories deep links to scraps and preserves the query and hash', async () => {
+    const {router} = render(<LegacyStoriesRedirect />, {
+      initialRouterConfig: {
+        location: {
+          pathname: '/organizations/test-org/stories/core/button/?theme=dark#examples',
+        },
+      },
+    });
+
+    await waitFor(() => {
+      expect(router.location.pathname).toBe(
+        '/organizations/test-org/scraps/core/button/'
+      );
+    });
+    expect(router.location.query).toEqual({theme: 'dark'});
+    expect(router.location.hash).toBe('#examples');
   });
 });
--- static/app/router/routes.tsx
+++ static/app/router/routes.tsx
@@ -1,5 +1,5 @@
 import type {RouteObject} from 'react-router-dom';
-import {Outlet} from 'react-router-dom';
+import {Navigate, Outlet} from 'react-router-dom';
 import memoize from 'lodash/memoize';
 
 import {EXPERIMENTAL_SPA} from 'sentry/constants';
@@ -11,6 +11,7 @@ import type {OverrideName} from 'sentry/types/overrides';
 import {errorHandler} from 'sentry/utils/errorHandler';
 import {ProvideAriaRouter} from 'sentry/utils/provideAriaRouter';
 import {translateSentryRoute} from 'sentry/utils/reactRouter6Compat/router';
+import {useLocation} from 'sentry/utils/useLocation';
 import {withDomainRedirect} from 'sentry/utils/withDomainRedirect';
 import {withDomainRequired} from 'sentry/utils/withDomainRequired';
 import {App} from 'sentry/views/app';
@@ -51,6 +52,21 @@ import {type SentryRouteObject} from './types';
 const routeHook = (name: OverrideName): SentryRouteObject => {
   return getOverride(name)?.() ?? {};
 };
+
+export function LegacyStoriesRedirect() {
+  const location = useLocation();
+
+  return (
+    <Navigate
+      replace
+      to={{
+        pathname: location.pathname.replace(/\/stories(?=\/|$)/, '/scraps'),
+        search: location.search,
+        hash: location.hash,
+      }}
+    />
+  );
+}
 
 function buildRoutes(): RouteObject[] {
   // Read this to understand where to add new routes, how / why the routing
@@ -310,8 +326,7 @@ function buildRoutes(): RouteObject[] {
     {
       path: '/stories/*',
       withOrgPath: true,
-      // eslint-disable-next-line boundaries/dependencies -- storybook redirect
-      component: make(() => import('sentry/stories/view/legacyStoriesRedirect')),
+      component: LegacyStoriesRedirect,
     },
     {
       path: '/debug/notifications/:notificationSource?/',
