50f96f4a10604747bdc8bf661f670f54e3e5fc91
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-08-25 11:19:33 +0200

    test(web): Authenticate storybook route requests

--- tests/sentry/web/test_urls.py
+++ tests/sentry/web/test_urls.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from sentry.testutils.cases import TestCase
+from sentry.testutils.silo import control_silo_test
 
 
 class DocsRedirectTest(TestCase):
@@ -19,13 +20,16 @@ class ApiDocsRedirectTest(TestCase):
         assert resp.status_code == 302, resp.status_code
 
 
+@control_silo_test
 class StorybookRoutesTest(TestCase):
     def test_scraps_response(self) -> None:
+        self.login_as(self.user)
         path = reverse("scraps")
         resp = self.client.get(path)
         assert resp.status_code == 200, resp.status_code
 
     def test_legacy_stories_response(self) -> None:
+        self.login_as(self.user)
         path = reverse("stories")
         resp = self.client.get(path)
         assert resp.status_code == 200, resp.status_code
