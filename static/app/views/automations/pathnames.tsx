4ced7a4ca73d856107b1f022e20fd3c3a0a5d71e
Jonas Badalic <jbadalic@twitter.com>
2026-08-31 13:39:29 -0700

    ref(ui): Drop unused optional args in detectors, automations, and discover views
    Split from #122373 and #122624.

--- static/app/views/automations/pathnames.tsx
+++ static/app/views/automations/pathnames.tsx
@@ -1,20 +1,11 @@
-import * as qs from 'query-string';
-
 import {normalizeUrl} from 'sentry/utils/url/normalizeUrl';
 
 export const makeAutomationBasePathname = (orgSlug: string) => {
   return normalizeUrl(`/organizations/${orgSlug}/monitors/alerts/`);
 };
 
-export const makeAutomationCreatePathname = (
-  orgSlug: string,
-  query: {
-    connectedIds?: string[];
-  } = {}
-) => {
-  return normalizeUrl(
-    `${makeAutomationBasePathname(orgSlug)}new/?${qs.stringify(query)}`
-  );
+export const makeAutomationCreatePathname = (orgSlug: string) => {
+  return normalizeUrl(`${makeAutomationBasePathname(orgSlug)}new/`);
 };
 
 export const makeAutomationDetailsPathname = (orgSlug: string, automationId: string) => {
