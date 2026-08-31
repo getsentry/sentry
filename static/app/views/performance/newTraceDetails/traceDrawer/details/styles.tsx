3295880f0629f90227c72e4ab3b7a39b5feaa881
Jonas Badalic <jbadalic@twitter.com>
2026-08-31 13:40:47 -0700

    ref(ui): Drop unused optional args in trace details
    Split from #122373 and #122624.

--- static/app/views/performance/newTraceDetails/traceDrawer/details/styles.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/styles.tsx
@@ -2,7 +2,6 @@ import {Fragment, useMemo, useState, type PropsWithChildren} from 'react';
 import {css, useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 import {useHover} from '@react-aria/interactions';
-import type {LocationDescriptor} from 'history';
 
 import {Button, LinkButton} from '@sentry/scraps/button';
 import {Container, Flex, Stack} from '@sentry/scraps/layout';
@@ -321,38 +320,26 @@ function Duration(props: DurationProps) {
 
 function TableRow({
   title,
-  keep,
   children,
-  prefix,
-  extra = null,
-  toolTipText,
 }: {
   children: React.ReactNode;
   title: React.JSX.Element | string | null;
-  extra?: React.ReactNode;
-  keep?: boolean;
-  prefix?: React.JSX.Element;
-  toolTipText?: string;
 }) {
-  if (!keep && !children) {
+  if (!children) {
     return null;
   }
 
   return (
     <tr>
       <td className="key">
-        <Flex align="center">
-          {prefix}
-          {title}
-          {toolTipText ? <StyledQuestionTooltip size="xs" title={toolTipText} /> : null}
-        </Flex>
+        <Flex align="center">{title}</Flex>
       </td>
       <ValueTd className="value">
         <TableValueRow>
           <StyledPre>
             <span className="val-string">{children}</span>
           </StyledPre>
-          <TableRowButtonContainer>{extra}</TableRowButtonContainer>
+          <TableRowButtonContainer>{null}</TableRowButtonContainer>
         </TableValueRow>
       </ValueTd>
     </tr>
@@ -735,10 +722,6 @@ const TableValueRow = styled('div')`
   margin: 2px;
 `;
 
-const StyledQuestionTooltip = styled(QuestionTooltip)`
-  margin-left: ${p => p.theme.space.xs};
-`;
-
 const StyledPre = styled('pre')`
   margin: 0 !important;
   background-color: transparent !important;
@@ -1039,16 +1022,16 @@ function EventTags({projectSlug, event}: {event: Event; projectSlug: string}) {
 
 export type SectionCardKeyValueList = KeyValueListData;
 
+const SECTION_CARD_TRUNCATE_LENGTH = 5;
+
 function SectionCard({
   items,
   title,
-  disableTruncate,
   sortAlphabetically = false,
   itemProps = {},
 }: {
   items: SectionCardKeyValueList;
   title: React.ReactNode;
-  disableTruncate?: boolean;
   itemProps?: Partial<KeyValueDataContentProps>;
   sortAlphabetically?: boolean;
 }) {
@@ -1060,7 +1043,7 @@ function SectionCard({
         title={title}
         contentItems={contentItems}
         sortAlphabetically={sortAlphabetically}
-        truncateLength={disableTruncate ? Infinity : 5}
+        truncateLength={SECTION_CARD_TRUNCATE_LENGTH}
       />
     </CardWrapper>
   );
@@ -1091,17 +1074,7 @@ function SectionCardGroup({children}: {children: React.ReactNode}) {
   return <KeyValueData.Container>{children}</KeyValueData.Container>;
 }
 
-function CopyableCardValueWithLink({
-  value,
-  linkTarget,
-  linkText,
-  onClick,
-}: {
-  value: React.ReactNode;
-  linkTarget?: LocationDescriptor;
-  linkText?: string;
-  onClick?: () => void;
-}) {
+function CopyableCardValueWithLink({value}: {value: React.ReactNode}) {
   return (
     <CardValueContainer>
       <CardValueText>
@@ -1115,11 +1088,6 @@ function CopyableCardValueWithLink({
           />
         ) : null}
       </CardValueText>
-      {linkTarget && linkTarget ? (
-        <Link to={linkTarget} onClick={onClick}>
-          {linkText}
-        </Link>
-      ) : null}
     </CardValueContainer>
   );
 }
