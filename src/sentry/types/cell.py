e572bde43661002822f0820456ae5c2ed0d9b234
Lyn <1779792+lynnagara@users.noreply.github.com>
2026-09-02 11:04:40 -0700

    ref: Begin deprecating `category` from localities (#123250)
    No longer required, visibility is controlled via `visible` and
    `signup_visible` parameters
    
    This depends on the fixes in
    https://github.com/getsentry/getsentry/pull/21780 first

--- src/sentry/types/cell.py
+++ src/sentry/types/cell.py
@@ -34,8 +34,6 @@ class Locality:
     cells: frozenset[str]
     """The set of cell names that belong to this locality."""
 
-    category: RegionCategory
-
     new_org_cell: str
     """The cell within this locality where new organizations are provisioned."""
 
@@ -44,6 +42,9 @@ class Locality:
 
     signup_visible: bool = True
     """Whether or not a locality should be visible for org signup/relocation."""
+
+    category: RegionCategory = RegionCategory.MULTI_TENANT
+    """Deprecated. Visibility is defined via `visible` and `signup_visible`."""
 
     def to_url(self, path: str) -> str:
         """Resolve a path into a customer facing URL on this locality.
@@ -495,11 +496,7 @@ def find_all_multitenant_locality_names() -> list[str]:
     """
     Return all visible multi-tenant localities.
     """
-    return [
-        loc.name
-        for loc in get_global_directory().localities
-        if loc.category == RegionCategory.MULTI_TENANT and loc.visible
-    ]
+    return [loc.name for loc in get_global_directory().localities if loc.visible]
 
 
 def find_all_signup_locality_names() -> list[str]:
@@ -507,7 +504,5 @@ def find_all_signup_locality_names() -> list[str]:
     Return all locality names that are visible to org signup.
     """
     return [
-        loc.name
-        for loc in get_global_directory().localities
-        if loc.category == RegionCategory.MULTI_TENANT and loc.visible and loc.signup_visible
+        loc.name for loc in get_global_directory().localities if loc.visible and loc.signup_visible
     ]
