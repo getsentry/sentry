165592772a22286ec89b4524f298e0b23658d1e1
Rudra Patel <patelrudra2003@gmail.com>
2026-09-02 17:18:44 -0700

    feat: Scaffold a registry for detector settings

--- src/sentry/workflow_engine/typings/grouptype.py
+++ src/sentry/workflow_engine/typings/grouptype.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 
 from sentry.issues.grouptype import GroupCategory, GroupType
+from sentry.workflow_engine.registry import detector_settings_registry
 from sentry.workflow_engine.types import DetectorSettings
 
 
@@ -18,7 +19,10 @@ class IssueStreamGroupType(GroupType):
     enable_status_change_workflow_notifications = False
     enable_workflow_notifications = False
     enable_user_status_and_priority_changes = False
-    detector_settings = DetectorSettings(
+
+
+detector_settings_registry.register(IssueStreamGroupType.slug)(
+    DetectorSettings(
         config_schema={
             "$schema": "https://json-schema.org/draft/2020-12/schema",
             "type": "object",
@@ -26,3 +30,4 @@ class IssueStreamGroupType(GroupType):
             "additionalProperties": False,
         },
     )
+)
