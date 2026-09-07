2762a151c4a24bd0828555e1558da415a016dd60
Ivan Dlugos <dlugos.ivan@gmail.com>
2026-09-07 22:45:00 +0200

    chore(gitlab): Record version 3 in the webhook version changelog

--- src/sentry/integrations/gitlab/constants.py
+++ src/sentry/integrations/gitlab/constants.py
@@ -3,5 +3,6 @@ GITLAB_CLOUD_BASE_URL = "https://gitlab.com"
 # Increment this when webhook configuration changes (e.g., adding new event types)
 # Version 1: Added issues_events support for assignment and comment sync
 # Version 2: Added note_events support for merge request comments
+# Version 3: Re-push hook tokens, so hooks left behind by a reinstall get the current one
 GITLAB_WEBHOOK_VERSION_KEY = "gitlab_webhook_version"
 GITLAB_WEBHOOK_VERSION = 3
