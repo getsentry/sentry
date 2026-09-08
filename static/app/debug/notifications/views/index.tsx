6146652e6aaf7e8b836cf8764b44f4fb6f1b2292
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-09-08 10:44:21 +0200

    ref(ui): Scroll wide notification previews horizontally

--- static/app/debug/notifications/views/index.tsx
+++ static/app/debug/notifications/views/index.tsx
@@ -48,20 +48,16 @@ export default function DebugNotificationsIndex() {
           <SidebarContainer>
             <DebugNotificationsSidebar />
           </SidebarContainer>
-          <Stack area="body">
+          <Stack area="body" overflowX="auto">
             {selectedRegistration ? (
-              <Stack gap="xl" padding="2xl" maxWidth="2000px" containerType="inline-size">
+              <Stack gap="xl" padding="2xl" minWidth="900px" maxWidth="2000px">
                 <Heading as="h2" variant="success">
                   <Flex gap="md" align="center">
                     {selectedRegistration.source}
                     <Tag variant="success">{selectedRegistration.category}</Tag>
                   </Flex>
                 </Heading>
-                <Grid
-                  columns={{zero: '1fr', '3xl': '1fr auto'}}
-                  gap="2xl"
-                  position="relative"
-                >
+                <Grid columns="minmax(400px, 1fr) 450px" gap="2xl" position="relative">
                   <Stack position="relative" minWidth="400px" justify="start">
                     <EmailPreview registration={selectedRegistration} />
                     <SlackPreview registration={selectedRegistration} />
@@ -73,7 +69,7 @@ export default function DebugNotificationsIndex() {
                     top={`calc(${HEADER_HEIGHT}px + ${theme.space.xl})`}
                     alignSelf="flex-start"
                     minWidth={0}
-                    maxWidth={{zero: 'none', '3xl': '450px'}}
+                    maxWidth="450px"
                   >
                     <DebugNotificationsExample registration={selectedRegistration} />
                   </Container>
