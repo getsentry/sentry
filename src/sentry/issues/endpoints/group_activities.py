b70458428871caadf16aa2ed1896b0302ffab06c
Shashank Jarmale <shashank.jarmale@sentry.io>
2026-09-09 11:58:58 -0700

    Implement `get_serialized_activity_items`

--- src/sentry/issues/endpoints/group_activities.py
+++ src/sentry/issues/endpoints/group_activities.py
@@ -7,17 +7,10 @@ from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import cell_silo_endpoint
 from sentry.api.helpers.deprecation import deprecated
 from sentry.api.serializers import serialize
-from sentry.api.serializers.models.groupactionlogentry import serialize_first_seen_entry
+from sentry.api.serializers.models.groupactionlogentry import get_serialized_activity_items
 from sentry.constants import CELL_API_DEPRECATION_DATE
-from sentry.issues.action_log.read_metrics import (
-    ActivityReadReason,
-    ActivityReadResult,
-    activity_read_endpoint,
-    record_activity_read,
-)
-from sentry.issues.derived.gate import should_serve_action_log_activity
+from sentry.issues.action_log.read_metrics import activity_read_endpoint
 from sentry.issues.endpoints.bases.group import GroupEndpoint
-from sentry.issues.models.groupactionlogentry import GroupActionLogEntry
 from sentry.models.activity import Activity
 from sentry.models.group import Group
 
@@ -39,29 +32,11 @@ class GroupActivitiesEndpoint(GroupEndpoint):
         """
         Retrieve all the Activities for a Group
         """
-        endpoint = activity_read_endpoint(request)
-        if should_serve_action_log_activity(group.project, request.user, endpoint=endpoint):
-            action_log = GroupActionLogEntry.objects.get_actions_for_group(group, 99)
-            if action_log:
-                record_activity_read(
-                    endpoint,
-                    ActivityReadResult.GALE,
-                )
-                serialized = serialize(action_log, request.user)
-                serialized.append(serialize_first_seen_entry(group))
-                return Response(
-                    {
-                        "activity": serialized,
-                    }
-                )
-            record_activity_read(
-                endpoint,
-                ActivityReadResult.FELL_BACK,
-                ActivityReadReason.EMPTY_LOG,
-            )
-            logger.info(
-                "group_activities.groupactionlogentry.not_found", extra={"group_id": group.id}
-            )
+        activity_items = get_serialized_activity_items(
+            group, request.user, endpoint=activity_read_endpoint(request)
+        )
+        if activity_items is not None:
+            return Response({"activity": activity_items})
 
         activity = Activity.objects.get_activities_for_group(group, num=100)
         return Response(
