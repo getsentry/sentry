5ada608b182e24ec86a346ed53697ce51be8525a
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-09-10 06:50:04 +0200

    fix(ui): Reset key value pre spacing

--- static/app/components/events/interfaces/keyValueList/index.tsx
+++ static/app/components/events/interfaces/keyValueList/index.tsx
@@ -130,6 +130,10 @@ function MultiValueContainer({values}: {values: string[]}): React.JSX.Element {
 }
 
 const Tablevalue = styled('div')`
+  > pre {
+    margin: 0;
+    padding: 0;
+  }
   pre {
     && {
       word-break: break-all;
