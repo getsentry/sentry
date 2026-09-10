5168c959853b9016dab12bb08af30208db884cd6
Simon Hellmayr <shellmayr@users.noreply.github.com>
2026-09-10 17:00:11 +0200

    chore(sdk): fix name of sample rate constant entry from sentry.tasks.post_process.post_process_group to sentry.issues.tasks.post_process.post_process_group (#124031)
    - This does not work anymore due to a rename of the task
    - Rename to make it work

--- src/sentry/utils/sdk.py
+++ src/sentry/utils/sdk.py
@@ -72,7 +72,7 @@ SAMPLED_TASKS = {
     ),
     "sentry.tasks.process_suspect_commits": settings.SENTRY_SUSPECT_COMMITS_APM_SAMPLING,
     "sentry.tasks.process_commit_context": settings.SENTRY_SUSPECT_COMMITS_APM_SAMPLING,
-    "sentry.tasks.post_process.post_process_group": settings.SENTRY_POST_PROCESS_GROUP_APM_SAMPLING,
+    "sentry.issues.tasks.post_process.post_process_group": settings.SENTRY_POST_PROCESS_GROUP_APM_SAMPLING,
     "sentry.tasks.reprocessing2.handle_remaining_events": settings.SENTRY_REPROCESSING_APM_SAMPLING,
     "sentry.tasks.reprocessing2.reprocess_group": settings.SENTRY_REPROCESSING_APM_SAMPLING,
     "sentry.tasks.reprocessing2.finish_reprocessing": settings.SENTRY_REPROCESSING_APM_SAMPLING,
