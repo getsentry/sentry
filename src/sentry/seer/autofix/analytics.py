61c8e699d9c52b831e5819d025cf0791d6f6489e
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2026-09-10 14:22:59 -0700

    ref(analytics): Record analytics events to Datadog+Sentry (#124129)
    I've been unable to find good real-time analytics for autofix related
    events. This forwards autofix events to Datadog & Sentry so we can have
    some real-time monitoring in place.
    
    ---
    
    <sub>Stack created with <a
    href="https://github.com/github/gh-stack">GitHub Stacks CLI</a> • <a
    href="https://gh.io/stacks-feedback">Give Feedback 💬</a></sub>

---
+++ src/sentry/seer/autofix/analytics.py
@@ -0,0 +1,13 @@
+from sentry_sdk import metrics as sentry_metrics
+
+from sentry import analytics
+from sentry.analytics.events.autofix_events import AiAutofixPhaseEvent
+from sentry.utils import metrics
+
+
+def record_autofix_event(event: AiAutofixPhaseEvent) -> None:
+    """Record an Autofix funnel event in BigQuery, Datadog, and Sentry Metrics."""
+    analytics.record(event)
+    if event.type is not None:
+        metrics.incr(event.type)
+        sentry_metrics.count(event.type, 1)
