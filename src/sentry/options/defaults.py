fbe6a351ce7b5462d8f19f9d8d57ff24a234a11c
Leander Rodrigues <leander.rodrigues@sentry.io>
2026-09-11 16:21:18 -0400

    chore(workflow-engine): Create scheduled repair task for missing all projects detectors (#123942)
    Creates a task that will be scheduled to chip away at any organizations
    that don't receive all project detectors after they're created.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3648,6 +3648,12 @@ register(
     default=False,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+register(
+    "workflow_engine.tasks.health_check_organization.enabled",
+    type=Bool,
+    default=False,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
 
 register(
     "workflow_engine.group.type_id.open_periods_type_denylist",
