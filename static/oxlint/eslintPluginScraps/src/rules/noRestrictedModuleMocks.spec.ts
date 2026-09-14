c5525694cab6212a16fce38b7f0d06378325a3fa
Malachi Willey <malwilley@gmail.com>
2026-09-14 13:29:39 -0700

    feat(frontend): Disallow mocking supported test hooks

---
+++ static/oxlint/eslintPluginScraps/src/rules/noRestrictedModuleMocks.spec.ts
@@ -0,0 +1,60 @@
+import {RuleTester} from '@typescript-eslint/rule-tester';
+
+import {noRestrictedModuleMocks} from './noRestrictedModuleMocks';
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-restricted-module-mocks', noRestrictedModuleMocks, {
+  valid: [
+    "jest.mock('sentry/utils/analytics');",
+    "jest.mock('sentry/utils/useLocationExtra');",
+    "jest.requireActual('sentry/utils/useLocation');",
+    "other.mock('sentry/utils/useLocation');",
+    'jest.mock(moduleName);',
+    "const hooks = {useLocation() {}}; jest.spyOn(hooks, 'useLocation');",
+    "import * as location from 'sentry/utils/useLocation'; jest.spyOn(location, 'parseLocation');",
+  ],
+  invalid: [
+    ...[
+      'useLocation',
+      'useNavigate',
+      'usePageFilters',
+      'useOrganization',
+      'useProjects',
+    ].flatMap(hook =>
+      ['mock', 'doMock'].map(method => ({
+        code: `jest.${method}('sentry/utils/${hook}', () => ({}));`,
+        errors: [{messageId: 'forbidden' as const}],
+      }))
+    ),
+    {
+      code: "jest.mock('sentry/utils/useLocation');",
+      errors: [
+        {
+          messageId: 'forbidden',
+          data: {
+            hook: 'useLocation',
+            replacement: 'Set initialRouterConfig.location in the render options.',
+          },
+        },
+      ],
+    },
+    {
+      code: "import * as router from 'sentry/utils/useNavigate'; jest.spyOn(router, 'useNavigate');",
+      errors: [
+        {
+          messageId: 'forbidden',
+          data: {
+            hook: 'useNavigate',
+            replacement:
+              'Use the router provided by render and assert on router.location after interacting.',
+          },
+        },
+      ],
+    },
+    {
+      code: "jest.spyOn(require('sentry/utils/useOrganization'), 'useOrganization');",
+      errors: [{messageId: 'forbidden'}],
+    },
+  ],
+});
---
+++ static/oxlint/eslintPluginScraps/src/rules/noRestrictedModuleMocks.ts
@@ -0,0 +1,118 @@
+import {ESLintUtils} from '@typescript-eslint/utils';
+
+import {createImportTracker} from '../ast/tracker/imports.ts';
+
+const replacements = new Map([
+  [
+    'sentry/utils/useOrganization',
+    'Pass organization: OrganizationFixture({...}) to the render options.',
+  ],
+  ['sentry/utils/useProjects', 'Use ProjectsStore.loadInitialData([ProjectFixture()]).'],
+  ['sentry/utils/useLocation', 'Set initialRouterConfig.location in the render options.'],
+  [
+    'sentry/utils/useNavigate',
+    'Use the router provided by render and assert on router.location after interacting.',
+  ],
+  [
+    'sentry/utils/usePageFilters',
+    'Use PageFiltersStore.onInitializeUrlState(PageFiltersFixture({...})).',
+  ],
+]);
+
+export const noRestrictedModuleMocks = ESLintUtils.RuleCreator.withoutDocs({
+  meta: {
+    type: 'problem',
+    docs: {
+      description:
+        'Disallow module mocking of hooks with supported test providers or stores.',
+    },
+    schema: [],
+    messages: {
+      forbidden: 'Do not mock {{hook}}. {{replacement}}',
+    },
+  },
+  create(context) {
+    const importTracker = createImportTracker();
+
+    return {
+      ...importTracker.visitors,
+
+      CallExpression(node) {
+        const {callee} = node;
+        if (
+          callee.type !== 'MemberExpression' ||
+          callee.object.type !== 'Identifier' ||
+          callee.object.name !== 'jest' ||
+          callee.property.type !== 'Identifier'
+        ) {
+          return;
+        }
+
+        if (callee.property.name === 'mock' || callee.property.name === 'doMock') {
+          const source = node.arguments[0];
+          if (source?.type !== 'Literal' || typeof source.value !== 'string') {
+            return;
+          }
+
+          const replacement = replacements.get(source.value);
+          if (replacement) {
+            context.report({
+              node: source,
+              messageId: 'forbidden',
+              data: {hook: source.value.split('/').at(-1), replacement},
+            });
+          }
+          return;
+        }
+
+        if (callee.property.name !== 'spyOn') {
+          return;
+        }
+
+        const moduleObject = node.arguments[0];
+        const property = node.arguments[1];
+        if (
+          !moduleObject ||
+          moduleObject.type === 'SpreadElement' ||
+          property?.type !== 'Literal' ||
+          typeof property.value !== 'string'
+        ) {
+          return;
+        }
+
+        let moduleSource: string | undefined;
+        if (moduleObject.type === 'Identifier') {
+          const importedModule = importTracker.resolve(moduleObject.name);
+          if (importedModule?.imported === '*') {
+            moduleSource = importedModule.source;
+          }
+        } else if (
+          moduleObject.type === 'CallExpression' &&
+          moduleObject.callee.type === 'Identifier' &&
+          moduleObject.callee.name === 'require'
+        ) {
+          const source = moduleObject.arguments[0];
+          if (source?.type === 'Literal' && typeof source.value === 'string') {
+            moduleSource = source.value;
+          }
+        }
+
+        if (!moduleSource) {
+          return;
+        }
+
+        const replacement = replacements.get(moduleSource);
+        const hook = moduleSource.split('/').at(-1);
+        if (!replacement || property.value !== hook) {
+          return;
+        }
+
+        context.report({
+          node: property,
+          messageId: 'forbidden',
+          data: {hook, replacement},
+        });
+      },
+    };
+  },
+});
