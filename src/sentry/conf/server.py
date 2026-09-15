2ee2ef5efcff8650316726449c866b0f36bfd7e9
Alex Alderman Webb <alexander.webb@sentry.io>
2026-09-15 14:28:49 +0200

    chore(sdk): Decrease `SAMPLED_DEFAULT_RATE` to 0.006 (#124386)
    Adjust constants passed to the SDK's traces sampler.
    We intend to turn off dynamic sampling for the monolith and are moving the average server-side sampling rate to the client.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -2911,7 +2911,7 @@ SENTRY_PROJECT_COUNTER_STATEMENT_TIMEOUT = 1000
 # Implemented in getsentry to run additional devserver workers.
 SENTRY_EXTRA_WORKERS: MutableSequence[str] = []
 
-SAMPLED_DEFAULT_RATE = 0.0125
+SAMPLED_DEFAULT_RATE = 0.006
 
 # A set of extra URLs to sample
 ADDITIONAL_SAMPLED_URLS: dict[str, float] = {}
