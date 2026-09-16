65b74c09a97e3baf7d5062cdc0c871547b6a1dd5
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-09-16 09:13:03 +0200

    chore(sdk): Decrease SAMPLED_DEFAULT_RATE to 0.003

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -2906,7 +2906,7 @@ SENTRY_PROJECT_COUNTER_STATEMENT_TIMEOUT = 1000
 # Implemented in getsentry to run additional devserver workers.
 SENTRY_EXTRA_WORKERS: MutableSequence[str] = []
 
-SAMPLED_DEFAULT_RATE = 0.006
+SAMPLED_DEFAULT_RATE = 0.003
 
 # A set of extra URLs to sample
 ADDITIONAL_SAMPLED_URLS: dict[str, float] = {}
