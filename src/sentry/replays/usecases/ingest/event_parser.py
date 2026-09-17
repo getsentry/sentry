e78e9bdc64d18ba63212d0be904efeb0ab02f339
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-09-17 14:06:23 +0200

    chore: Inline span-streaming branch of tracing shims in replays

--- src/sentry/replays/usecases/ingest/event_parser.py
+++ src/sentry/replays/usecases/ingest/event_parser.py
@@ -10,12 +10,12 @@ from enum import Enum
 from typing import Any, Literal, TypedDict, TypeVar
 
 from sentry_protos.snuba.v1.trace_item_pb2 import TraceItem
+from sentry_sdk import traces
 
 from sentry import options
 from sentry.logging.handlers import SamplingFilter
 from sentry.replays.lib.eap.write import Value, new_trace_item
 from sentry.utils import json
-from sentry.utils.tracing import trace
 
 logger = logging.getLogger("sentry.replays.event_parser")
 logger.addFilter(SamplingFilter(0.001))
@@ -96,7 +96,7 @@ class EventContext(TypedDict):
     user_geo_subdivision: str | None
 
 
-@trace
+@traces.trace
 def parse_events(
     context: EventContext, events: list[dict[str, Any]]
 ) -> tuple[ParsedEventMeta, list[TraceItem]]:
