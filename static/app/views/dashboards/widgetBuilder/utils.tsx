a07cc0354ce28d3bf31ea14bd8e6fea9cc82889f
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-09-17 11:12:19 -0400

    ref(dashboards): Remove transactions dataset mapping from widget builder

--- static/app/views/dashboards/widgetBuilder/utils.tsx
+++ static/app/views/dashboards/widgetBuilder/utils.tsx
@@ -33,8 +33,6 @@ export const sortDirections = {
 
 export function getDiscoverDatasetFromWidgetType(widgetType: WidgetType) {
   switch (widgetType) {
-    case WidgetType.TRANSACTIONS:
-      return DiscoverDatasets.SPANS;
     case WidgetType.ERRORS:
       return DiscoverDatasets.ERRORS;
     default:
