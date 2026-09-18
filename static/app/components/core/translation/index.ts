2ac84a658f17e7064cd17c5de5e11f464bfdcb64
TkDodo <office@dorfmeister.cc>
2026-09-18 15:49:12 +0200

    fix: knip

--- static/app/components/core/translation/index.ts
+++ static/app/components/core/translation/index.ts
@@ -1,5 +1,6 @@
 import {TranslationContext} from './translationContext';
 
+/** @public */
 export type {TranslationContextValue} from './translationContext';
 
 export const TranslationContextProvider = TranslationContext.Provider;
