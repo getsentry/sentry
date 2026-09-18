a6c1b4ab7dfc28d4f55fc59a74f28fa974b93c94
Nar Saynorath <nar.saynorath@sentry.io>
2026-09-18 14:01:20 -0400

    fix missing negation

--- static/app/components/searchBar/utils.tsx
+++ static/app/components/searchBar/utils.tsx
@@ -1,7 +1,7 @@
 export function escapeTagValue(value: string): string {
   // Wrap in quotes if there is a space
   const isArrayTag = value.startsWith('[') && value.endsWith(']') && value.includes(',');
-  return isArrayTag && (value.includes(' ') || value.includes('"'))
+  return !isArrayTag && (value.includes(' ') || value.includes('"'))
     ? `"${value.replace(/"/g, '\\"')}"`
     : value;
 }
