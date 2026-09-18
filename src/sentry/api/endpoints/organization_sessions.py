6cc8da8fbca08b79417976c5200219005594e854
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-09-18 09:49:47 +0200

    chore: Inline span-streaming branch of tracing shis in ai_monitoring

--- src/sentry/api/endpoints/organization_sessions.py
+++ src/sentry/api/endpoints/organization_sessions.py
@@ -4,6 +4,7 @@ from drf_spectacular.utils import extend_schema
 from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
+from sentry_sdk import traces
 
 from sentry import release_health
 from sentry.api.api_owners import ApiOwner
@@ -27,7 +28,6 @@ from sentry.models.organization import Organization
 from sentry.release_health.base import SessionsQueryResult
 from sentry.snuba.sessions_v2 import SNUBA_LIMIT, InvalidField, QueryDefinition
 from sentry.utils.cursors import Cursor, CursorResult
-from sentry.utils.tracing import start_span
 
 
 @extend_schema(tags=["Releases"])
@@ -82,7 +82,9 @@ class OrganizationSessionsEndpoint(OrganizationEndpoint):
 
         def data_fn(offset: int, limit: int) -> SessionsQueryResult:
             with self.handle_query_errors():
-                with start_span(op="sessions.endpoint", name="build_sessions_query"):
+                with traces.start_span(
+                    name="build_sessions_query", attributes={"sentry.op": "sessions.endpoint"}
+                ):
                     request_limit = None
                     if request.GET.get("per_page") is not None:
                         request_limit = limit
