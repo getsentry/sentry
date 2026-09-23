0255222affe218ff4365aaa1ca3ae0bc12510ade
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-09-23 05:06:16 -0700

    ref(debug-meta): Use inherited query container

--- static/app/components/events/interfaces/debugMeta/debugImageDetails/candidates.tsx
+++ static/app/components/events/interfaces/debugMeta/debugImageDetails/candidates.tsx
@@ -316,7 +316,7 @@ export class Candidates extends Component<Props, State> {
     const {emptyMessage, emptyAction} = this.getEmptyMessage();
 
     return (
-      <Grid gap="lg" containerType="inline-size">
+      <Grid gap="lg">
         <Flex
           direction={{zero: 'column', xl: 'row'}}
           gap={{zero: 'sm', xl: '0'}}
