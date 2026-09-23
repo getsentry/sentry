99cb3727fbf66e0715720e5a3fd6752aee15687a
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-09-23 14:39:51 -0700

    do not invoke emit_evaluations_to_eap, add a little more flavor to that method as well

--- src/sentry/workflow_engine/processors/evaluations/eap.py
+++ src/sentry/workflow_engine/processors/evaluations/eap.py
@@ -1,6 +1,5 @@
 from typing import TYPE_CHECKING
 
-from sentry import features
 from sentry.workflow_engine.processors.evaluations.logging import WorkflowEngineResult
 
 if TYPE_CHECKING:
@@ -11,9 +10,11 @@ def emit_evaluations_to_eap(
     organization: Organization,
     result: WorkflowEngineResult,
 ) -> None:
+    """
     if not features.has("organizations:workflow-engine-evaluation-artifacts-eap", organization):
         # The feature is not enabled for the org
         return
 
-    # TODO - emit the artifacts to EAP
+    artifacts = result.evaluation_artifacts()
+    """
     return
--- src/sentry/workflow_engine/processors/evaluations/tracking.py
+++ src/sentry/workflow_engine/processors/evaluations/tracking.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-from sentry.workflow_engine.processors.evaluations.eap import emit_evaluations_to_eap
+# from sentry.workflow_engine.processors.evaluations.eap import emit_evaluations_to_eap
 from sentry.workflow_engine.processors.evaluations.logging import (
     WorkflowEngineResult,
     emit_evaluation_logs,
@@ -17,4 +17,4 @@ def emit_evaluations(
     organization: Organization,
 ) -> None:
     emit_evaluation_logs(organization, result)
-    emit_evaluations_to_eap(organization, result)
+    # emit_evaluations_to_eap(organization, result)
