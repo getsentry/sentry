c8a450a7eb337e613e8ee081df49079396e4a1c4
Nate Moore <nate.moore@sentry.io>
2026-09-23 13:34:24 -0400

    fix(stories): Set heading link fragments safely

--- static/app/stories/view/storyTableOfContents.tsx
+++ static/app/stories/view/storyTableOfContents.tsx
@@ -60,7 +60,7 @@ function getContentEntries(main: HTMLElement): Entry[] {
       ? entry.nextElementSibling
       : null;
     if (headingLink instanceof HTMLAnchorElement) {
-      headingLink.href = `#${entry.id}`;
+      headingLink.hash = entry.id;
     }
 
     entries.push({
