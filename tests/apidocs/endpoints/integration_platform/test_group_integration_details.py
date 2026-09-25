7dfa846c91881cfb41f04b1b420b21616102f1e2
betegon <miguelbetegongarcia@gmail.com>
2026-09-25 21:49:30 +0200

    test(api): Remove obsolete issue link changed assertion

--- tests/apidocs/endpoints/integration_platform/test_group_integration_details.py
+++ tests/apidocs/endpoints/integration_platform/test_group_integration_details.py
@@ -88,7 +88,6 @@ class GroupIntegrationDetailsDocs(APIDocsTestCase):
         request = RequestFactory().put(self.base_url, data=data)
 
         assert response.status_code == 200
-        assert response.data["changed"] is False
         self.validate_schema(request, response)
 
     def test_delete(self) -> None:
