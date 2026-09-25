a2adcba9958c824720623c1a84b8f7558f4485f8
Leander Rodrigues <leander.rodrigues@sentry.io>
2026-09-25 09:55:33 -0400

    fix(workflow-engine): Prevent falling back for connected detectors (#124617)
    Bit of a behaviour change here, if we ever can't load the detectors,
    don't fall back to a different option, instead fail loudly.
    
    This is important because `getInitialMonitorMode` falling back without
    warning in the UI could lead users to unintentionally changing the
    connected detectors when they might only be wanting to update a slack
    channel or something.
    
    This was originally pointed out by cursor here:
    https://github.com/getsentry/sentry/pull/120938#discussion_r3737127542,
    but finally following up

--- static/app/views/automations/hooks/useConnectedDetectors.tsx
+++ static/app/views/automations/hooks/useConnectedDetectors.tsx
@@ -4,11 +4,17 @@ import {useFormField} from 'sentry/components/workflowEngine/form/useFormField';
 import {useOrganization} from 'sentry/utils/useOrganization';
 import {detectorListApiOptions} from 'sentry/views/detectors/hooks';
 
-export function useConnectedDetectors() {
-  const detectorIds = useFormField<string[]>('detectorIds') ?? [];
+export function useConnectedDetectors(detectorIdsOverride?: string[]) {
+  const formDetectorIds = useFormField<string[]>('detectorIds') ?? [];
+  const detectorIds = detectorIdsOverride ?? formDetectorIds;
   const organization = useOrganization();
 
-  const {data: connectedDetectors = [], isLoading} = useQuery({
+  const {
+    data: connectedDetectors = [],
+    isError,
+    isLoading,
+    refetch,
+  } = useQuery({
     ...detectorListApiOptions(organization, {
       ids: detectorIds,
       includeIssueStreamDetectors: true,
@@ -16,5 +22,5 @@ export function useConnectedDetectors() {
     enabled: detectorIds.length > 0,
   });
 
-  return {connectedDetectors, isLoading};
+  return {connectedDetectors, isError, isLoading, refetch};
 }
